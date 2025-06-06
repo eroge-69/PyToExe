5f5296575db931869b51ed05fdaddbd3c17088b4
eroge-69 <erogexunamed@gmail.com>
2025-06-06 13:44:53 +0700

    Upload clase 2-(23-5-2025).py via PY to EXE tool

---
+++ python-files/clase 2-(23-5-2025).py
@@ -0,0 +1,7 @@
+200"""ingresar 5 datos por teclado e imprimir el mayor de los  numeros"""
+a=float(input("Introduce la resistencia R1 : "))
+b=float(input("Introduce la resistencia R2 : "))
+c=float(input("Introduce la resistencia R3 : "))
+d=float(input("Introduce la resistencia R4 : "))
+e=float(input("Introduce la resistencia R5 : "))
+print("El mayor de las resistencias es :",max(a,b,c,d,e))
