436eced136ade9cee34303d4728f433a2f36701a
eroge-69 <erogexunamed@gmail.com>
2025-06-09 01:41:10 +0700

    Upload NotTrojan.py via PY to EXE tool

---
+++ python-files/NotTrojan.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Replace this with your desired YouTube URL
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+# Open the URL in the default web browser
+webbrowser.open(url)
