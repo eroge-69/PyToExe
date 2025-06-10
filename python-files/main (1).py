80ae0225b56de2284567a290f69c21b0f66e9830
eroge-69 <erogexunamed@gmail.com>
2025-06-10 20:21:18 +0700

    Upload main (1).py via PY to EXE tool

---
+++ python-files/main (1).py
@@ -0,0 +1,36 @@
+#calculator
+print("      WELCOME")
+
+
+num1=float(input("enter frist number\n:"))
+oper=input("choose the operation(+,-,*,/,)\n:")
+num2=float(input("enter secound number\n:"))
+if oper=="+":
+        result=(num1+num2)
+        print(num1,"+",num2,"= ",result) 
+
+elif oper=="-":
+        result=num1-num2
+        print(num1,"-",num2,"= ",result)
+        
+elif oper=="*":
+        result=num1*num2
+        print(num1,"*",num2,"= ",result)
+        
+
+elif oper=="/":
+        result=num1/num2
+        print(num1,"/",num2,"= ",result)
+       
+print(" THANKS FOR USING MY APP \n PLEASE RATE MY APP FROM 10 ")
+       
+input ("       rating : ")
+      
+print("*******THANKS*******    ")
+        
+        
+        
+        
+        
+        
+        
