f0511ca55e647c2b8a27b11684b3220a537fedc2
eroge-69 <erogexunamed@gmail.com>
2025-06-14 22:42:31 +0700

    Upload surprise.py via PY to EXE tool

---
+++ python-files/surprise.py
@@ -0,0 +1,23 @@
+Enter_name= str(input("Enter your name: "))
+print(f"Hello {Enter_name}")
+print("Loading files...........")
+Question=str(input("Are you Subal Ghosh's father? (Y/N): "))
+
+if Question== "Y" or "y" or "Yes" or "yes":
+	print("Loading files................ \n Downloading packages............")
+	print("Happy father's day dad!! ")
+	print("You are the best dad in the entire universe!!!!")
+	print("Your love is is unconditioned: can't be measured.")
+	print("With lotssss of love : Subal Ghosh, Isha Ghosh  and dear Zoro Ghosh")
+
+else:
+	print("This surprise is not for you :)")
+
+Question2=str(input("Anything you would like to say......?"))
+if Question2=="":
+	print("Bruh")
+else:
+	print("Thankuuuuuuuuuuuuuuuuuu")
+
+
+
