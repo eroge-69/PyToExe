26740ae22c80d9a79d1f7c5ae7ed147515aac852
eroge-69 <erogexunamed@gmail.com>
2025-06-19 18:51:51 +0700

    Upload calcolo_quota.py via PY to EXE tool

---
+++ python-files/calcolo_quota.py
@@ -0,0 +1,15 @@
+import time
+
+def calcola_nuovo_valore():
+    quota_desiderata = float(input("Valore a cui deve essere la quota: "))
+    quota_rilevata = float(input("Valore della quota rilevata: "))
+    valore_pratico_attuale = float(input("Valore pratico attuale: "))
+
+    differenza = quota_desiderata - quota_rilevata
+    nuovo_valore = valore_pratico_attuale + differenza
+
+    print(f"\nIl nuovo valore pratico corretto è: {nuovo_valore}")
+    print("\nIl programma si chiuderà tra 10 secondi...")
+    time.sleep(10)
+
+calcola_nuovo_valore()
