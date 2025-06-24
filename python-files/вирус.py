fcc55c12cedfb1522a152c45f5452ea824a28718
eroge-69 <erogexunamed@gmail.com>
2025-06-24 21:27:12 +0700

    Upload вирус.py via PY to EXE tool

---
+++ python-files/вирус.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import pygame   
+
+def wertyuiop():
+    root = tk.Tk("qwerthjhg")
+    root.title("asdfghj")
+    root.geometry("250x250")
+    root.mainloop() 
+    wertyuiop()
+ 
+wertyuiop()
+
+
+
+
+
+pygame.init()  
+pygame.mixer.Sound("sound.wav")  
+sound.play()  
