017f5838ea06bd8b2abd21d8707ae7aa84dd28ea
eroge-69 <erogexunamed@gmail.com>
2025-06-24 16:46:25 +0700

    Upload chatbot.py via PY to EXE tool

---
+++ python-files/chatbot.py
@@ -0,0 +1,20 @@
+while True:
+    frage = input("Du:")
+
+    if frage.lower() == "hi":
+        print("Bot: Was willst du??")
+    elif frage.lower() == "sieg":
+        print("Bot: Heil!!!")
+    elif frage.lower() == "wie geht es dir":
+        print("Bot: Scheiße ich bin nur ein Programm du opfer")
+    elif frage.lower() == "was machst du mit juden":
+        print("Bot: Adolf holen und ab nach Auschwitz!")
+    elif frage.lower() == "was sagt adolf dazu":
+        print("Adolf: VERRRGASENN")
+    elif frage.lower() == "kannst du eine rede halten":
+        print("Adolf: DU, MEINE ARRRBEIT FÜR RICHTIG HÄLLST, OB DU GLAUBST DAS CH FLEIßIG GEWESEN BIN, DAS ICH GEARRBEITET HABE, DAS ICH MICH IN DIESEM JAHR FÜR DICH EINGESETZT HABE, IM DIENSTE MEINES VOLKES, GEB DU JETZT DEINE STIMME ABB, WENN JAA DANN TRRRITTT FÜR MICH EIN SOWIE ICH FÜR DICH EINGETRETEN BINN!!!!")
+    elif frage.lower() == "exit":
+        print("Bot: Auf Wieder sehen!")
+        break
+    else:
+        print("Bot: Entschuldigung, das habe ich leider nicht verstanden")
