9981be30414e4632cd71a6df174d92d650cff63b
eroge-69 <erogexunamed@gmail.com>
2025-06-26 14:53:40 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,34 +1,13 @@
-import tkinter as tk
-
+import tkinter  as tk
 from tkinter import messagebox
-from login import Login
-from carrusel import Carrusel
+import os
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Inicio")
-        self.geometry("750x500")
+root = tk.Tk()
+root.withdraw()
 
-        contenedor = tk.Frame(self)
-        contenedor.pack(side="top", fill="both", expand=True)
-        contenedor.grid_rowconfigure(0, weight=1)
-        contenedor.grid_columnconfigure(0, weight=1)
+cevap = messagebox.askyesno("Uyarı!","Devam etmek istiyormusunuz?")
 
-        self.frames = {}
-        for F in (Login, Carrusel):
-            nombrePagina = F.__name__
-            frame = F(parent=contenedor, controller=self)
-            self.frames[nombrePagina] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.cambiarPagina("Login")
-
-    def cambiarPagina(self, nombrePagina):
-        frame = self.frames[nombrePagina]
-        frame.tkraise()
-
-
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+if  cevap:
+    os.remove("C:\Kullanıcılar\demir\Belgeler\deneme")
+    else
+    print("İşlem iptal edildi.")
