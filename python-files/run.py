c03e1edb420268ad9c1cd5bc7cb51aa0c53f3a28
eroge-69 <erogexunamed@gmail.com>
2025-06-26 23:06:31 +0700

    Upload run.py via PY to EXE tool

---
+++ python-files/run.py
@@ -0,0 +1,7 @@
+from copilot_engine.core_logic import CoPilotPlusEngine
+from utils.parser import load_rules
+
+rules = load_rules("example_rules.yaml")
+engine = CoPilotPlusEngine(rules)
+input_data = {"type": "greeting"}
+print(engine.process(input_data))
