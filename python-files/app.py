f61d28a79d2b2acca3b3e959eaddecf313d9586a
eroge-69 <erogexunamed@gmail.com>
2025-06-30 00:14:53 +0700

    Upload app.py via PY to EXE tool

--- python-files/app.py
+++ python-files/app.py
@@ -1,94 +1,15 @@
-import json
-import json.decoder
-import os
-import sys
-import threading
+import ctypes import tkinter as tk
 
-import psutil
-import win32gui
-from backend import *
-from frontend import *
+ES_CONTINUOUS = 0x80000000 ES_SYSTEM_REQUIRED = 0x00000001 ES_DISPLAY_REQUIRED = 0x00000002
 
+Start keeping PC awake
 
-def main():
-    # If the user trys to open another instance, do not open
-    count = 0
-    for process in psutil.process_iter(["name"]):
-        if process.info["name"] == "BeatBind.exe":
-            count += 1
-    if count > 1:
-        sys.exit()
+ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED)
 
-    try:
-        backend = Backend()
-        frontend = Frontend(backend)
+def close(): # Restore normal behavior ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS) root.destroy()
 
-        # Check if there is a config file
-        if os.path.exists(backend.config_path):
-            with open(backend.config_path, "r", encoding="utf-8") as f:
-                config = json.load(f)
-                hotkeys = config["hotkeys"]
+root = tk.Tk() root.title("Stay Awake")
 
-                # Load the config settings into the backend
-                for key, default_value in [
-                    ("client_id", ""),
-                    ("client_secret", ""),
-                    ("port", 8888),
-                    ("device_id", ""),
-                    ("volume", 5),
-                    ("seek", 5000),
-                    ("rewind_instead_prev", False),
-                ]:
-                    setattr(backend, key, config.get(key, default_value))
+btn = tk.Button(root, text="Close", command=close, width=30, height=3) btn.pack(padx=20, pady=20)
 
-                for key, default_value in [
-                    ("play/pause", "control+alt+shift+p"),
-                    ("play", "control+alt+shift+z"),
-                    ("pause", "control+alt+shift+x"),
-                    ("prev_track", "control+alt+shift+left"),
-                    ("next_track", "control+alt+shift+right"),
-                    ("volume_up", "control+alt+shift+up"),
-                    ("volume_down", "control+alt+shift+down"),
-                    ("mute", "control+alt+shift+space"),
-                    ("seek_forward", "control+alt+shift+f"),
-                    ("seek_backward", "control+alt+shift+b"),
-                    ("save_track", "control+alt+shift+s"),
-                    ("remove_track", "control+alt+shift+r"),
-                ]:
-                    backend.hotkeys[key] = hotkeys.get(key, default_value)
-
-            # Refresh token after successfully loading config
-            backend.StartupTokenRefresh()
-
-            # Check minimize flag and start frontend accordingly
-            if config.get("minimize", False) and not frontend.menu.visible:
-                frontend.run()
-            else:
-                frontend.SettingsWindow()
-        else:
-            # If config file does not exist, open the settings window
-            logging.warning("Config file not found, opening Settings window")
-            frontend.SettingsWindow()
-
-        # Start message pump to process Windows messages
-        def start_message_pump():
-            win32gui.PumpMessages()
-
-        message_pump_thread = threading.Thread(target=start_message_pump)
-        message_pump_thread.daemon = True
-        message_pump_thread.start()
-        logging.info("Beatbind initialized")
-    except json.JSONDecodeError as e:
-        logging.error(f"Error decoding JSON in config file: {e}")
-        backend.ErrorMessage(f"Error decoding JSON in config file: {e}")
-        logging.info("Exiting app")
-        sys.exit()
-    except Exception as e:
-        logging.info(f"Error initializing app: {e}")
-        backend.ErrorMessage(e)
-        logging.info("Exiting app")
-        sys.exit()
-
-
-if __name__ == "__main__":
-    main()
+root.mainloop()
