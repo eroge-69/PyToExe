4c1bda2e8f834650628988497bd1cbb946570545
eroge-69 <erogexunamed@gmail.com>
2025-07-01 06:51:40 +0700

    Upload download.py via PY to EXE tool

--- python-files/download.py
+++ python-files/download.py
@@ -1,6 +1,13 @@
-x = input("введи своё имя:")
-print("приветик," + x)
-if x == "dana":
-    print("я люблю тебя больше жизни")
-else:
-    print("а ну кыш отсюда, это только для даны!")
+import json
+import random
+from typing import Dict, List, Set
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog, simpledialog
+import pandas as pd
+import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+
+class SchoolScheduleApp:
+    def __init__(self, root):
+        self.root = root
+        self.style = tb.Style(theme="minty")  
