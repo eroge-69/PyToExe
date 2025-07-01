3c1f7a60a35b1cd1b4b961f95a1be1a2efe3ac10
eroge-69 <erogexunamed@gmail.com>
2025-07-01 22:10:30 +0700

    Upload joker.py via PY to EXE tool

---
+++ python-files/joker.py
@@ -0,0 +1,8 @@
+print("Приветики!!!")
+name = input("Ваше имя?")
+ansver = input("Прекрасное имя, хотите шутку?")
+age = int(input("Ваш возраст?"))
+if ansver == "Да" and age > 10:
+ print("Нажмите alt+f4, чтобы продолжить,", name)
+else:
+ print("Пока,", name)
