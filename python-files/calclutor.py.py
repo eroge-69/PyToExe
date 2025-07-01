585c8b2d9d84b6ae2f5bfc5964aeff69f2097dfa
eroge-69 <erogexunamed@gmail.com>
2025-07-02 00:09:38 +0700

    Upload calclutor.py.py via PY to EXE tool

---
+++ python-files/calclutor.py.py
@@ -0,0 +1,15 @@
+print("|Welcome is Calclutor| \n")
+num1=float(input("First number  : " ) )
+num2=float(input("Second number : " ) )
+print("--------------------------------------")
+
+print("+ : " + str(num1+num2) )
+print("- : " + str(num1-num2) )
+print("% : " + str(num1%num2) )
+print("* : " + str(num1*num2) )
+print("/ : " + str(num1/num2) )
+
+print("---------------------------------------")
+print("Thank you\n")
+
+
