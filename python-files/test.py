435f5df70487f3ccf4675f46c2237a81c4f77675
eroge-69 <erogexunamed@gmail.com>
2025-07-02 09:29:24 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -1,8 +1,13 @@
-user_name=input("Hello gorgeous,What's ur name?")
-print(user_name,"? Sounds hot")
-user_year=int(input(f"Which year did u born {user_name}?"))
-user_age=2025-user_year
-print("So u r",user_age,"? U look younger!")
-distance_to_30=30-user_age
-print("u r actually",distance_to_30,"years to 30")
-print("But it doesnt matter, The whole point of showing u this is ke man toro kheili dus daramo mibinamet taghriban 70-80 deighe dige")
+          response_file = open (basePath+'response_msg/work/'+ShipmentReference+'_response_'+date+'.txt', 'w', newline = '\n' ) #this creates the response file
+            response_file.write (response.text)
+            response_file.close()
+            request_file.close()
+ 
+            response_str = json.loads(response.content)
+            AWBNumber = response_str.get('shipmentTrackingNumber')# this extract the shipment ID from the json response
+            documents = response_str.get('documents') # this extracts the list - documents
+            LabelImage = [i["content"] for i in documents] # this extracts the dictionary from the list - documents
+            label = LabelImage[0] # this remove the [ ] from the extracted base64 codes
+            bytes = b64decode(label, validate=True) # this decode the base64 image
+            f = open(basePath+'label/'+ShipmentReference+'_'+str(AWBNumber)+'.pdf', 'wb') #this save the decoded image as PDF file
+            f.write(bytes)
