50ba829f3bcffa492e65ce89f4f3e6dbbc3460f9
eroge-69 <erogexunamed@gmail.com>
2025-07-02 14:11:21 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -1,13 +1,2 @@
-          response_file = open (basePath+'response_msg/work/'+ShipmentReference+'_response_'+date+'.txt', 'w', newline = '\n' ) #this creates the response file
-            response_file.write (response.text)
-            response_file.close()
-            request_file.close()
- 
-            response_str = json.loads(response.content)
-            AWBNumber = response_str.get('shipmentTrackingNumber')# this extract the shipment ID from the json response
-            documents = response_str.get('documents') # this extracts the list - documents
-            LabelImage = [i["content"] for i in documents] # this extracts the dictionary from the list - documents
-            label = LabelImage[0] # this remove the [ ] from the extracted base64 codes
-            bytes = b64decode(label, validate=True) # this decode the base64 image
-            f = open(basePath+'label/'+ShipmentReference+'_'+str(AWBNumber)+'.pdf', 'wb') #this save the decoded image as PDF file
-            f.write(bytes)
+for i in range(20):
+    print(i)
