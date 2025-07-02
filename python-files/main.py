09d21b14e34a23914b31e00d159288d0909ad570
eroge-69 <erogexunamed@gmail.com>
2025-07-02 07:46:45 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,44 +1,17 @@
-import re
-import random
-import logging
-import sys
-import time
-from colorama import init, Fore, Style
+#calculator by L1mzy но он для RemiiitCompany
 
-init()
+print ("Привет это простой калькулятор на Python.")
+print ("Удачи в использование!")
 
-# Настройка логирования
-logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+#Отступ для красоты1
+print("")
 
-# Варианты для выбора с нужным соотношением
-choice_weights = ['ТОТР'] * 35 + ['НЕТОТР'] * 65
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
 
-# Ввод имени файла
-input_filename = 'doc.txt'  # Замените на ваш файл
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
 
-try:
-    with open(input_filename, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-except FileNotFoundError:
-    print("Файл не найден.")
-    sys.exit(1)
-
-pattern = re.compile(r'^\d+:\d+$')
-
-for line in lines:
-    line = line.strip()
-    # Задержка между выводами (0.5-1 секунды)
-    interval = random.uniform(0.5, 1)
-    time.sleep(interval)
-
-    if pattern.match(line):
-        choice = random.choice(choice_weights)
-        if choice == 'ТОТР':
-            print(Fore.RED + 'ТОТР' + Style.RESET_ALL)
-            logging.info('ТОТР')
-        else:
-            print(Fore.GREEN + 'НЕТОТР' + Style.RESET_ALL)
-            logging.info('НЕТОТР')
-    else:
-        print(Fore.YELLOW + 'Ошибка формата: ' + line + Style.RESET_ALL)
-        logging.info('Ошибка формата: ' + line)
+input ("для продолжения нажмите enter: ")
