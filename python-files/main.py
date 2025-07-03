bed204d5a94c78d4e0aabd96622738c4357977a4
eroge-69 <erogexunamed@gmail.com>
2025-07-03 23:49:07 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,42 +1,41 @@
-from PIL import ImageGrab
-import pyautogui as pag
-import keyboard
-import time
+import random
+from turtle import Turtle, Screen
+screen = Screen()
 
-# Центр экрана
-x = pag.size().width / 2
-y = pag.size().height / 2
+nigers = ["nigg", "nogg", "nagg", "negg", "nugg"]
+colors = ["black", "red", "blue", "green", "yellow"]
+screen.setup(width=400, height=400)
+
+# setting up nigers
+for niger in nigers:
+    globals()[niger] = Turtle(shape="turtle",)
+    globals()[niger].color(colors[nigers.index(niger)])
+    globals()[niger].penup()
+    globals()[niger].goto(x= -180, y= -100 + nigers.index(niger)*50)
+
+bet = screen.textinput("nigers race!", "which niger will win?")
+
+#racing
+race = True
+winner = "none"
+while race:
+    globals()[niger].speed("fast")
+    for niger in nigers:
+        globals()[niger].forward(random.randint(1, 5))
+        if globals()[niger].pos()[0] > 170:
+            race = False
+            winner = niger
+
+if bet == winner:
+    print(f"Your niger {bet} won!")
+else:
+    print(f"your niger did not win. The winner is {winner}")
 
 
-# Получение цвета пикселей
-def get_pixel_color(x, y):
-    screenshot = ImageGrab.grab()
-    pixel1 = screenshot.getpixel((x-2, y-2))
-    pixel2 = screenshot.getpixel((x-2, y+2))``
-    pixel3 = screenshot.getpixel((x+2, y-2))
-    pixel4 = screenshot.getpixel((x+2, y+2))
-    return pixel1, pixel2, pixel3, pixel4
 
-def main():
-    while True:
-        if keyboard.is_pressed("alt"):
-            p1, p2, p3, p4 = get_pixel_color(x,y)
 
-            newp1, newp2, newp3, newp4 = get_pixel_color(x,y)
-            if newp1 != p1 and newp1 != (255, 255, 255):
-                time.sleep(0.2)
-                pag.leftClick()
-            elif newp2 != p2 and newp2 != (255, 255, 255):
-                time.sleep(0.2)
-                pag.leftClick()
-            elif newp3 != p3 and newp3 != (255, 255, 255):
-                time.sleep(0.2)
-                pag.leftClick()
-            elif newp4 != p4 and newp4 != (255, 255, 255):
-                time.sleep(0.2)
-                pag.leftClick()
 
-        if keyboard.is_pressed("l"):
-            break
-if __name__ == "__main__":
-    main()
+
+
+
+screen.exitonclick()
