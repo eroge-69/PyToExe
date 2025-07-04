07f4e9caa96f6217b2300d90952f4258db6e397b
eroge-69 <erogexunamed@gmail.com>
2025-07-04 18:30:59 +0700

    Upload Calculator.py via PY to EXE tool

--- python-files/Calculator.py
+++ python-files/Calculator.py
@@ -1,70 +1,30 @@
-from tkinter import *
-root = Tk()
-root.title("Caculator")
-root.geometry("300x400")
-root.resizable(width=0,height=0)
-root.configure(bg="black")
-e=Entry(root,bd=10,width=30,font="Arial 12",bg="LightGrey")
-e.pack()
-def click(number):
-    e.insert(index=32,string=number)
-def add():
-    e.insert(index=32,string="+")
-def clear():
-    e.delete(0,END)
-def equal():
-    current= e.get()
-    List = current.split("+")
-    result = int(List[0])+int(List[1])
-    clear()
-    e.insert(index=32,string=result)
-btn1 = Button(root,text="7",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(7))
-btn1.place(x=10,y=60)               
-btn2 = Button(root,text="8",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(8))
-btn2.place(x=85,y=60)               
-btn3 = Button(root,text="9",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(9))
-btn3.place(x=160,y=60)               
-btn4 = Button(root,text="4",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(4))
-btn4.place(x=10,y=145)               
-btn5 = Button(root,text="5",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(5))
-btn5.place(x=85,y=145)               
-btn6 = Button(root,text="6",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(6))
-btn6.place(x=160,y=145)               
-btn7 = Button(root,text="1",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(1))
-btn7.place(x=10,y=230)               
-btn8 = Button(root,text="2",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(2))
-btn8.place(x=85,y=230)               
-btn9 = Button(root,text="3",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(3))
-btn9.place(x=160,y=230)               
-btn10 = Button(root,text="0",font="Arial 19 bold",bg="Grey",bd=10,padx=10,pady=5,command=lambda: click(0))
-btn10.place(x=10,y=320)
-btn11 = Button(root,text="+",font="Arial 19 bold",bg="skyBlue",bd=10,padx=10,pady=5,height=4,command=add)
-btn11.place(x=235,y=60)               
-btn12 = Button(root,text="C",font="Arial 19 bold",bg="crimson",bd=10,padx=10,pady=5,width=7,command=clear)
-btn12.place(x=85,y=320)               
-btn13 = Button(root,text="=",font="Arial 19 bold",bg="khaki",bd=10,padx=10,pady=5,height=4,command=equal)
-btn13.place(x=235,y=230)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-              
-              
-
-
-
-
-root.mainloop()
+#Taking input of what to do
+F = input("Welcome to Deva's calculator please type in which operation you would like to do (+,-,/,*): ")
+fl = ["+","-","/","*"]
+while F not in fl:
+    F = input("Please enter one of the given operations (+,-,/,*): ")
+#Taking first number
+N1 = (input("Enter the first number: "))
+while not N1.isdigit():
+    N1 = (input("Please enter a number: "))
+N1=int(N1)
+#Taking second number
+N2 = input("Enter the second number: ")
+while not N2.isdigit():
+    N2 = (input("Please enter a number"))
+N2=int(N2)
+#Solving part
+if F == "+" :
+    R = N1 + N2
+elif F == "-" :
+    R = N1-N2
+elif N1==0 and N2==0 and F=="/":
+    R ="Undefined:)"
+elif N1>N2 and N2==0 :
+    R = "Undefined:)"
+elif  F == "/" :
+    R = N1/N2
+elif F == "*" :
+    R = N1 * N2
+#Result
+print(f"The answer of {N1}{F}{N2} is : {R}")
