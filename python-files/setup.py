db6db36f5b9301c622c8b1ddbcf6668c98c59432
eroge-69 <erogexunamed@gmail.com>
2025-07-05 00:53:54 +0700

    Upload setup.py via PY to EXE tool

--- python-files/setup.py
+++ python-files/setup.py
@@ -1,25 +1,64 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'face_recognition_models>=0.3.0',
+    'Click>=6.0',
+    'dlib>=19.7',
+    'numpy',
+    'Pillow'
+]
+
+test_requirements = [
+    'tox',
+    'flake8'
+]
 
 setup(
-    name="pocketoptionapi",
-    version="0.1.1",
-    author="Mastaaa1987",
-    description="API for integration with PocketOption",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Mastaaa1987/PocketOptionAPI-v2",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    name='face_recognition',
+    version='1.4.0',
+    description="Recognize faces from Python or from the command line",
+    long_description=readme + '\n\n' + history,
+    author="Adam Geitgey",
+    author_email='ageitgey@gmail.com',
+    url='https://github.com/ageitgey/face_recognition',
+    packages=[
+        'face_recognition',
     ],
-    python_requires=">=3.6",
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_points={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
+        ]
+    },
     install_requires=requirements,
-) 
+    license="MIT license",
+    zip_safe=False,
+    keywords='face_recognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
