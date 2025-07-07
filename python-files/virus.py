dde5ff02f100975fe236e5044c336417313f6561
eroge-69 <erogexunamed@gmail.com>
2025-07-07 22:55:14 +0700

    Upload virus.py via PY to EXE tool

--- python-files/virus.py
+++ python-files/virus.py
@@ -1,23 +1,7 @@
-from pydub import AudioSegment
-from pydub.playback import play
-import os
+import ctypes
 
-def play_at_max_volume(file):
-    # Загрузка аудиофайла
-    sound = AudioSegment.from_file(file)
-    
-    # Увеличение громкости до максимума (без искажений)
-    # Вы можете регулировать коэффициент усиления (в dB)
-    sound = sound + 20  # Увеличение на 20 dB
-    
-    print(f"Воспроизведение {file} на максимальной громкости")
-    play(sound)
+def main():
+    ctypes.windll.user32.MessageBoxW(0, "Hello World!", "Test", 1)
 
-music_file = "music.mp3"
-
-try:
-    play_at_max_volume(music_file)
-except FileNotFoundError:
-    print(f"Ошибка: файл {music_file} не найден!")
-except Exception as e:
-    print(f"Ошибка воспроизведения: {e}")
+if __name__ == "__main__":
+    main()
