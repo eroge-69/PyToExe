79e86620635cb64fcb40ef099d260a0c640991f1
eroge-69 <erogexunamed@gmail.com>
2025-07-09 03:35:45 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,28 +1,76 @@
-import keyboard
-import pyautogui
-import threading
-import time
+from math import cos, sin, tan, radians
 
-clicking = False
-delay = 1 / 30  # 30 CPS
+while True:
+    command = input(f'введите команду >> ')
+    
+    if command == 'команды':
+        print(f'чет, кос, син, тан, радиан, калькулятор, стоп')
 
+    elif command == 'чет':
+        a = input(f'>> ')
+        try:
+            a = int(a)
+            if a % 2 == 0:
+                print(f'чет >> {a} <<')
+            else:
+                print(f'нечет >> {a} <<')
+        except ValueError:
+            print(f'>> {a} << не число!')
 
-def click_loop():
-    while clicking:
-        pyautogui.click()
-        time.sleep(delay)
+    elif command == 'кос':
+        try:
+            coss = int(input(f'>> '))
+            print(cos(coss))
+        except ValueError:
+            print(f'>> {coss} << не число!')
 
+    elif command == 'син':
+        try:
+            sinn = int(input('>> '))
+            print(sin(sinn))
+        except ValueError:
+            print(f'>> {sinn} << не число!')
 
-def toggle_clicking():
-    global clicking
-    clicking = not clicking
-    if clicking:
-        threading.Thread(target=click_loop).start()
-    print("CPS BOOSTER:", "ON" if clicking else "OFF")
+    elif command == 'тан':
+        try:
+            tann = int(input(f'>> '))
+            print(tan(tann))
+        except ValueError:
+            print(f'>> {tann} << не число!')
 
+    elif command == 'радиан':
+        try:
+            radian = int(input(f'>> '))
+            print(radians(radian))
+        except ValueError:
+            print(f'>> {radian} << не число!')
 
-keyboard.add_hotkey("f6", toggle_clicking)
+    elif command == 'калькулятор':
+        znak = input(f'какой знак использовать?\n 1) +\n 2) -\n 3) *\n 4) /\n>> ')
+        if znak not in ['1', '2', '3', '4']:
+            print(f'>> {znak} << не является цифрой из заданных! выберите цифру 1, 2, 3 или 4!')
+        elif znak == '1':
+            chislo = float(input(f'>> '))
+            chislo1 = float(input(f'>> '))
+            print(f'>> {chislo + chislo1} <<')
 
-print("Press F6 to toggle CPS booster")
+        elif znak == '2':
+            chislo2 = float(input(f'>> '))
+            chislo3 = float(input(f'>> '))
+            print(f'>> {chislo2 - chislo3} <<')
 
-keyboard.wait("esc")
+        elif znak == '3':
+            chislo4 = float(input(f'>> '))
+            chislo5 = float(input(f'>> '))
+            print(f'>> {chislo4 * chislo5} <<')
+
+        elif znak == '4':
+            chislo6 = float(input(f'>> '))
+            chislo7 = float(input(f'>> '))
+            print(f'>> {chislo6 / chislo7} <<')
+
+    elif command == 'стоп':
+        print(f'>> {command} << выход!')
+        break
+    else:
+        print(f'нет такой команды!')
