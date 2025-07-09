b78e3dd05d9c58aeab686b2b4d2d62e15abd00e8
eroge-69 <erogexunamed@gmail.com>
2025-07-09 17:08:40 +0700

    Upload code.py via PY to EXE tool

--- python-files/code.py
+++ python-files/code.py
@@ -1,38 +1,179 @@
-# Returns the minor of the matrix after removing the i-th row and j-th column
-def get_minor(matrix, i, j):
-    return [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+print("1:Encrypt/2:Decrypt")
+option=input("Enter 1 or 2:")
 
-# Computes the determinant of a square matrix (recursive for n > 2)
-def determinant(matrix):
-    n = len(matrix)
-    # Base case for 1x1 matrix
-    if n == 1:
-        return matrix[0][0]
-    # Base case for 2x2 matrix
-    if n == 2:
-        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+while option != "1" and option != "2":
+    print("Invalid option! Please enter 1 or 2.")
+    print("1:Encrypt/2:Decrypt")
+    option=input("Enter 1 or 2:")
     
-    det = 0
-    # Expand along the first row
-    for col in range(n):
-        minor = get_minor(matrix, 0, col)
-        det += ((-1) ** col) * matrix[0][col] * determinant(minor)
-    return det
-
-# Get matrix size from user
-n = int(input("Enter the size of the square matrix: "))
-
-# Get matrix elements from user
-print(f"Enter the {n}x{n} matrix row by row:")
-matrix = []
-for i in range(n):
-    row = []
-    for j in range(n):
-        element = float(input(f"Enter element [{i}][{j}]: "))
-        row.append(element)
-    matrix.append(row)
-
-print("Input matrix:")
-for row in matrix:
-    print(row)
-print("Determinant:", determinant(matrix))
+if option=="1":
+     print("Enter the message for encryption:")
+     message=input()
+     print("Enter the length of the message:")
+     length = input()
+     print("Each letter of the normal alphabet will be displayed.Then you must enter the coresponding letter in the depryption alphabet:")
+     A = input("A")
+     B = input("B")
+     C = input("C")
+     D = input("D")
+     E = input("E")
+     F = input("F")
+     G = input("G")
+     H = input("H")
+     I = input("I")
+     J = input("J")
+     K = input("K")
+     L = input("L")
+     M = input("M")
+     N = input("N")
+     O = input("O")
+     P = input("P")
+     Q = input("Q")
+     R = input("R")
+     S = input("S")
+     T = input("T")
+     U = input("U")
+     V = input("V")
+     W = input("W")
+     X = input("X")
+     Y = input("Y")
+     Z = input("Z")
+     llist = list(message)
+     for letter in llist:
+        if letter == "A":
+            print(A)
+        elif letter == "B":
+            print(B)
+        elif letter == "C": 
+            print(C)
+        elif letter == "D": 
+            print(D)
+        elif letter == "E": 
+            print(E)
+        elif letter == "F": 
+            print(F)
+        elif letter == "G": 
+            print(G)
+        elif letter == "H": 
+            print(H)
+        elif letter == "I": 
+            print(I)
+        elif letter == "J": 
+            print(J)
+        elif letter == "K": 
+            print(K)
+        elif letter == "L": 
+            print(L)
+        elif letter == "M": 
+            print(M)
+        elif letter == "N": 
+            print(N)
+        elif letter == "O": 
+            print(O)
+        elif letter == "P": 
+            print(P)
+        elif letter == "Q": 
+            print(Q)
+        elif letter == "R":
+            print(R)
+        elif letter == "S": 
+            print(S)
+        elif letter == "T": 
+            print(T)
+        elif letter == "U": 
+            print(U)
+        elif letter == "V": 
+            print(V)
+        elif letter == "W": 
+            print(W)
+        elif letter == "X": 
+            print(X)
+        elif letter == "Y":
+            print(Y)
+        elif letter == "Z": 
+            print(Z)
+            
+elif option=="2":
+    print("Enter the coced message:")
+    message=input()
+    print("Each letter of the depryption alphabet will be displayed.Then you must enter the coresponding letter in the normal alphabet:")
+    A = input("A")
+    B = input("B")
+    C = input("C")
+    D = input("D")
+    E = input("E")
+    F = input("F")
+    G = input("G")
+    H = input("H")
+    I = input("I")
+    J = input("J")
+    K = input("K")
+    L = input("L")
+    M = input("M")
+    N = input("N")
+    O = input("O")
+    P = input("P")
+    Q = input("Q")
+    R = input("R")
+    S = input("S")
+    T = input("T")
+    U = input("U")
+    V = input("V")
+    W = input("W")
+    X = input("X")
+    Y = input("Y")
+    Z = input("Z")
+    llist = list(message)
+    for letter in llist:
+       if letter == "A":
+           print(A)
+       elif letter == "B":
+           print(B)
+       elif letter == "C": 
+           print(C)
+       elif letter == "D": 
+           print(D)
+       elif letter == "E": 
+           print(E)
+       elif letter == "F": 
+           print(F)
+       elif letter == "G": 
+           print(G)
+       elif letter == "H": 
+           print(H)
+       elif letter == "I": 
+           print(I)
+       elif letter == "J": 
+           print(J)
+       elif letter == "K": 
+           print(K)
+       elif letter == "L": 
+           print(L)
+       elif letter == "M": 
+           print(M)
+       elif letter == "N": 
+           print(N)
+       elif letter == "O": 
+           print(O)
+       elif letter == "P": 
+           print(P)
+       elif letter == "Q": 
+           print(Q)
+       elif letter == "R":
+           print(R)
+       elif letter == "S": 
+           print(S)
+       elif letter == "T": 
+           print(T)
+       elif letter == "U": 
+           print(U)
+       elif letter == "V": 
+           print(V)
+       elif letter == "W": 
+           print(W)
+       elif letter == "X": 
+           print(X)
+       elif letter == "Y":
+           print(Y)
+       elif letter == "Z": 
+           print(Z)
