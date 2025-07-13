566da69781b305b0e26465b16e7b62c016f856e6
eroge-69 <erogexunamed@gmail.com>
2025-07-13 21:20:38 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -1,27 +1,17 @@
-
-import pyautogui
+import Adeept
 import time
-import pygetwindow as gw
-from pynput import keyboard
 
-# Configuration variables
-DELAY = 0.05  # Sleep time in seconds between clicks
-WINDOW_TITLE = "Diablo IV"  # Exact or partial window title to check
-KEY_TRIGGER = 'z'  # The key to trigger the action (lowercase for simplicity)
+global thread_flag
+thread_flag = 1
 
-def on_press(key):
-    try:
-        if key.char.lower() == KEY_TRIGGER:
-            active_window = gw.getActiveWindow()
-            if active_window and WINDOW_TITLE in active_window.title:
-                pyautogui.click(340, 818)
-                time.sleep(DELAY)
-                pyautogui.click(330, 500)
-                time.sleep(DELAY)
-                pyautogui.click(338, 956)
-    except AttributeError:
-        pass  # Ignore non-char keys
+Adeept.com_init('COM13',115200,1)
+Adeept.wiat_connect()
 
-# Start the keyboard listener
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+############cut-off rule#################
+
+Adeept.three_function("'pinmode'",8,1)
+while thread_flag:
+  Adeept.three_function("'digitalWrite'",8,1)
+  time.sleep(1)
+  Adeept.three_function("'digitalWrite'",8,0)
+  time.sleep(1)
