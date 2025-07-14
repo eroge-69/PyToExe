9068dc8a511c0f5c8d7f49706a07ec1a3ee955c2
eroge-69 <erogexunamed@gmail.com>
2025-07-14 20:56:40 +0700

    Upload code.py via PY to EXE tool

--- python-files/code.py
+++ python-files/code.py
@@ -1,179 +1,35 @@
-print("1:Encrypt/2:Decrypt")
-option=input("Enter 1 or 2:")
+from tkinter import Tk, Label
 
-while option != "1" and option != "2":
-    print("Invalid option! Please enter 1 or 2.")
-    print("1:Encrypt/2:Decrypt")
-    option=input("Enter 1 or 2:")
-    
-if option=="1":
-     print("Enter the message for encryption:")
-     message=input()
-     print("Enter the length of the message:")
-     length = input()
-     print("Each letter of the normal alphabet will be displayed.Then you must enter the coresponding letter in the depryption alphabet:")
-     A = input("A")
-     B = input("B")
-     C = input("C")
-     D = input("D")
-     E = input("E")
-     F = input("F")
-     G = input("G")
-     H = input("H")
-     I = input("I")
-     J = input("J")
-     K = input("K")
-     L = input("L")
-     M = input("M")
-     N = input("N")
-     O = input("O")
-     P = input("P")
-     Q = input("Q")
-     R = input("R")
-     S = input("S")
-     T = input("T")
-     U = input("U")
-     V = input("V")
-     W = input("W")
-     X = input("X")
-     Y = input("Y")
-     Z = input("Z")
-     llist = list(message)
-     for letter in llist:
-        if letter == "A":
-            print(A)
-        elif letter == "B":
-            print(B)
-        elif letter == "C": 
-            print(C)
-        elif letter == "D": 
-            print(D)
-        elif letter == "E": 
-            print(E)
-        elif letter == "F": 
-            print(F)
-        elif letter == "G": 
-            print(G)
-        elif letter == "H": 
-            print(H)
-        elif letter == "I": 
-            print(I)
-        elif letter == "J": 
-            print(J)
-        elif letter == "K": 
-            print(K)
-        elif letter == "L": 
-            print(L)
-        elif letter == "M": 
-            print(M)
-        elif letter == "N": 
-            print(N)
-        elif letter == "O": 
-            print(O)
-        elif letter == "P": 
-            print(P)
-        elif letter == "Q": 
-            print(Q)
-        elif letter == "R":
-            print(R)
-        elif letter == "S": 
-            print(S)
-        elif letter == "T": 
-            print(T)
-        elif letter == "U": 
-            print(U)
-        elif letter == "V": 
-            print(V)
-        elif letter == "W": 
-            print(W)
-        elif letter == "X": 
-            print(X)
-        elif letter == "Y":
-            print(Y)
-        elif letter == "Z": 
-            print(Z)
-            
-elif option=="2":
-    print("Enter the coced message:")
-    message=input()
-    print("Each letter of the depryption alphabet will be displayed.Then you must enter the coresponding letter in the normal alphabet:")
-    A = input("A")
-    B = input("B")
-    C = input("C")
-    D = input("D")
-    E = input("E")
-    F = input("F")
-    G = input("G")
-    H = input("H")
-    I = input("I")
-    J = input("J")
-    K = input("K")
-    L = input("L")
-    M = input("M")
-    N = input("N")
-    O = input("O")
-    P = input("P")
-    Q = input("Q")
-    R = input("R")
-    S = input("S")
-    T = input("T")
-    U = input("U")
-    V = input("V")
-    W = input("W")
-    X = input("X")
-    Y = input("Y")
-    Z = input("Z")
-    llist = list(message)
-    for letter in llist:
-       if letter == "A":
-           print(A)
-       elif letter == "B":
-           print(B)
-       elif letter == "C": 
-           print(C)
-       elif letter == "D": 
-           print(D)
-       elif letter == "E": 
-           print(E)
-       elif letter == "F": 
-           print(F)
-       elif letter == "G": 
-           print(G)
-       elif letter == "H": 
-           print(H)
-       elif letter == "I": 
-           print(I)
-       elif letter == "J": 
-           print(J)
-       elif letter == "K": 
-           print(K)
-       elif letter == "L": 
-           print(L)
-       elif letter == "M": 
-           print(M)
-       elif letter == "N": 
-           print(N)
-       elif letter == "O": 
-           print(O)
-       elif letter == "P": 
-           print(P)
-       elif letter == "Q": 
-           print(Q)
-       elif letter == "R":
-           print(R)
-       elif letter == "S": 
-           print(S)
-       elif letter == "T": 
-           print(T)
-       elif letter == "U": 
-           print(U)
-       elif letter == "V": 
-           print(V)
-       elif letter == "W": 
-           print(W)
-       elif letter == "X": 
-           print(X)
-       elif letter == "Y":
-           print(Y)
-       elif letter == "Z": 
-           print(Z)
+# Create main overlay window
+root = Tk()
+
+# Remove window decorations and keep it always on top
+root.overrideredirect(True)
+root.attributes("-topmost", True)
+
+# Make a specific color (white) fully transparent
+root.attributes("-transparentcolor", "white")
+
+# Get screen width and define overlay dimensions
+screen_width = root.winfo_screenwidth()
+overlay_height = 40  # ~1 cm in height
+x_pos = 0
+y_pos = 30  # A few pixels below the top edge
+
+# Set window geometry and transparent background
+root.geometry(f"{screen_width}x{overlay_height}+{x_pos}+{y_pos}")
+root.configure(bg="white")  # This white becomes transparent
+
+# Create a full-width black label with white text
+label = Label(
+    root,
+    text="Hello World",
+    font=("Helvetica", 20, "bold"),
+    fg="white",
+    bg="black",             # Black background strip behind text
+    anchor="center",
+)
+label.pack(fill="both", expand=True)
+
+# Run the application
+root.mainloop()
