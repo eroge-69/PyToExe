b3e70f433b0b37e124239d6403b92a8df99c1189
eroge-69 <erogexunamed@gmail.com>
2025-07-19 17:23:35 +0700

    Upload 1st.py via PY to EXE tool

--- python-files/1st.py
+++ python-files/1st.py
@@ -1,9 +1,43 @@
-rows = 6
-k = 2 * rows - 2
-for i in range(rows, -1, -1):
-    for j in range(k, 0, -1):
-        print(end=" ")
-    k = k + 1
-    for j in range(0, i + 2):
-        print("*", end=" ")
-    print("")
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import pywhatkit
+
+listener = sr.Recognizer()
+alexa =pyttsx3.init()
+voices =alexa.getProperty('voices')
+alexa.setProperty('voice', voices[1].id)
+
+def talk(text):
+    alexa.say(text)
+    alexa.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            listener.adjust_for_ambient_noise(source)
+            voice = listener.listen(source)
+            command =listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa','')
+
+    except:
+        pass
+    return command
+
+def run_alxa():
+    command = take_command()
+    if 'time' in command:
+        time = datetime.datetime.now().strftime("%I:%M %p")
+        print(time)
+        talk('time is'+time)
+    elif 'play' in command:
+        song = command.replace('play','')
+        talk('playing ' +song)
+        pywhatkit.playonyt(song)
+
+while True:
+    run_alxa()
