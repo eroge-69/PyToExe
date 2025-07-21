79ae4b0c0a559a2c9a86cfd522a74087eb42fc37
eroge-69 <erogexunamed@gmail.com>
2025-07-21 20:58:20 +0700

    Upload DOS.py via PY to EXE tool

---
+++ python-files/DOS.py
@@ -0,0 +1,25 @@
+import time
+
+time.sleep(1)
+print("Microsoft <c> 2025 MS-DOS 2.0")
+time.sleep(2)
+print("Okay")
+time.sleep(0.5)
+a = input('C:>')
+
+if a == ('help'):
+	print("пошел нахуй")
+
+else:
+	print(a + ' не является командой или исполняемым файлом  ')
+
+	time.sleep(0.2)
+	a = input('C:>')
+
+
+
+
+
+
+
+
