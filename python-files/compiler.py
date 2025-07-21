bc28398422cc7c8c8474266659bc3877667818ce
eroge-69 <erogexunamed@gmail.com>
2025-07-21 07:37:02 +0700

    Upload compiler.py via PY to EXE tool

---
+++ python-files/compiler.py
@@ -0,0 +1,8 @@
+📦 [MythSeal_Module]
+Inputs:
+- Project: EchoTemple.mbp
+- Config: EngineConfig.ini
+- Persona: GarrickCrayton.persona
+- Commentary: Finalcall_Maddon.cue
+Output:
+- EchoTemple_Installer.exe
