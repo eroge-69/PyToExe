30ed70951483e84d84d77a8a8a08d006e3d5d430
eroge-69 <erogexunamed@gmail.com>
2025-07-23 16:52:58 +0700

    Upload he.py via PY to EXE tool

--- python-files/he.py
+++ python-files/he.py
@@ -1,16 +1,9 @@
-/******************************************************************************
+'''
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
-*******************************************************************************/
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello World");
-
-    return 0;
-}
+'''
+print ('Hello World')
