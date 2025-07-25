15561a49db3a934c835b08f00e488df1673e8dca
eroge-69 <erogexunamed@gmail.com>
2025-07-26 00:59:22 +0700

    Upload SimpleDOS .py via PY to EXE tool

---
+++ python-files/SimpleDOS .py
@@ -0,0 +1,21 @@
+import time 
+
+print("starting DOS")
+time.sleep(0.75)
+print("loading...")
+time.sleep(0.75)
+ROM = "32mb"
+RAM = "32mb"
+print ("ROM:" + ROM)
+print ("RAM:" + RAM)
+time.sleep(0.75)
+print("Welcome to SimpleDOS")
+print("Version Alpha 1.2.0")
+while True:
+    command0 = imput("Enter Command:")
+    if command0 == "Sysin":
+    print("SimpleDOS x16bit")
+    print("Alpha test")
+    print("ROM = 32mb")
+    print("RAM = 32mb")    
+      
