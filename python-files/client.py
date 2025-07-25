6fec9f55b13c826692340b1841765bf38b912e1e
eroge-69 <erogexunamed@gmail.com>
2025-07-25 07:05:15 +0700

    Upload client.py via PY to EXE tool

--- python-files/client.py
+++ python-files/client.py
@@ -1,45 +1,73 @@
-import subprocess
-from scapy.all import sniff, DNSQR
+"""
+    <--------REMOTE DESKTOP-------->
+         Created: Artem Davydov
+         Coded: Artem Davydov
+         Language: Python
+    <------------------------------>
+"""
+
+# Socket
 import socket
-import requests
-import threading
+# Work with Image
+from PIL import ImageGrab
+import io
+import numpy as np
+from random import randint
+import pyautogui
+# Thread
+from threading import Thread
+# PyQt5
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QPushButton, QAction, QMessageBox, QLineEdit
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QRect, Qt
 
-hostname = socket.gethostname()
-server_url = "http://localhost/report.php"
-visited_domains = set()
+class Dekstop(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-def install_npcap_silently():
-    try:
-        result = subprocess.run(["sc query npcap"], shell=True, capture_output=True, text=True)
-        if "FAILED" in result.stdout or "does not exist" in result.stdout or "not recognized" in result.stdout:
-            subprocess.run(["npcap.exe", "/S"], shell=True)
-    except:
-        pass
+    def StartThread(self):
+        self.start.start()
 
-def send_to_server(domain):
-    payload = {
-        "hostname": hostname,
-        "domain": domain
-    }
-    try:
-        requests.post(server_url, json=payload)
-    except:
-        pass
+    def ChangeImage(self):
+        try:
+            if len(self.ip.text()) != 0 and len(self.port.text()):
+                sock = socket.socket()
+                sock.connect((self.ip.text(), int(self.port.text())))
+                while True:
+                    img = ImageGrab.grab()
+                    img_bytes = io.BytesIO()
+                    img.save(img_bytes, format='PNG')
+                    sock.send(img_bytes.getvalue())
+                sock.close()
+        except:
+            print("DISCONNECTED")
 
-def packet_callback(packet):
-    if packet.haslayer(DNSQR):
-        domain = packet[DNSQR].qname.decode().rstrip('.')
-        if domain not in visited_domains:
-            visited_domains.add(domain)
-            send_to_server(domain)
-
-def start_sniffing():
-    sniff(filter="udp port 53", prn=packet_callback, store=0)
-
-if __name__ == "__main__":
-    install_npcap_silently()
-    t = threading.Thread(target=start_sniffing)
-    t.daemon = True
-    t.start()
-    while True:
-        pass
+    def initUI(self):
+        self.pixmap = QPixmap()
+        self.label = QLabel(self)
+        self.label.resize(self.width(), self.height())
+        self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, 400, 90))
+        self.setFixedSize(self.width(), self.height())
+        self.setWindowTitle("[CLIENT] Remote Desktop: " + str(randint(99999, 999999)))
+        self.start = Thread(target=self.ChangeImage, daemon=True)
+        self.btn = QPushButton(self)
+        self.btn.move(5, 55)
+        self.btn.resize(390, 30)
+        self.btn.setText("Start Demo")
+        self.btn.clicked.connect(self.StartThread)
+        self.ip = QLineEdit(self)
+        self.ip.move(5, 5)
+        self.ip.resize(390, 20)
+        self.ip.setPlaceholderText("IP")
+        self.port = QLineEdit(self)
+        self.port.move(5, 30)
+        self.port.resize(390, 20)
+        self.port.setPlaceholderText("PORT")
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Dekstop()
+    ex.show()
+    sys.exit(app.exec())
