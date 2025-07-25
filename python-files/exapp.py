a3e7931f815563a6ff744a467d889fc6d69b1b89
eroge-69 <erogexunamed@gmail.com>
2025-07-25 21:09:04 +0700

    Upload exapp.py via PY to EXE tool

---
+++ python-files/exapp.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("app")
+root.attributes("-fullscreen", True)
+
+l1 = tk.Label(text="hallo", bg="red")
+l1.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
