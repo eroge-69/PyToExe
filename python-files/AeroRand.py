675efd8af4ea107e0b7c63d71070675c1dabb7aa
eroge-69 <erogexunamed@gmail.com>
2025-07-27 05:28:03 +0700

    Upload AeroRand.py via PY to EXE tool

---
+++ python-files/AeroRand.py
@@ -0,0 +1,26 @@
+num1 = int(input("ВВедите любую цифру 1: "))
+#String
+num2 = int(input("Введите любую цифру 2: "))
+num3 = num1 + num2 -num1//num2
+if num3== 1: print("Yes")
+if num3== 2: print("No")
+if num3== 3: print("No")
+if num3== 4: print("Maybe")
+if num3== 5: print("Yes")
+if num3== 6: print("Yes")
+if num3== 7: print("No")
+if num3== 8: print("Yes")
+if num3== 9: print("No")
+if num3== 10: print("Maybe")
+if num3== 11: print("Yes")
+if num3== 12: print("Yes")
+if num3== 13: print("No")
+if num3== 14: print("No")
+if num3== 15: print("Maybe")
+if num3== 16: print("Yes")
+if num3== 17: print("No")
+if num3 > 17: print("You didn't enter a number. / Вы ввели не цифру.")
+
+
+
+
