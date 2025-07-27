b405e0f0c3e293eca920332322e958fe5f2bc2d6
eroge-69 <erogexunamed@gmail.com>
2025-07-27 19:00:49 +0700

    Upload randomnumber.py via PY to EXE tool

---
+++ python-files/randomnumber.py
@@ -0,0 +1,13 @@
+import random
+
+alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+a = random.choice(alphabet)
+b = random.choice(alphabet)
+c = random.choice(alphabet)
+d = random.choice(alphabet)
+e = random.choice(alphabet)
+f = random.choice(alphabet)
+g = random.choice(alphabet)
+h = random.choice(alphabet)
+s = a + b + c + d + e + f + g + h
+print('Рандомный номер:', s)
