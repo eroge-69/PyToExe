afd2721348dd34ca1c8a93062a87aa215e53ddbf
eroge-69 <erogexunamed@gmail.com>
2025-07-28 08:52:23 +0700

    Upload pluh.py via PY to EXE tool

---
+++ python-files/pluh.py
@@ -0,0 +1,23 @@
+    import tkinter as tk
+# Main Win
+
+    inf = "none"
+        root = tk.tk()
+        root.title ("zenning on web")
+        root.geometry ("500x400")
+        
+        #Label
+        label = tk.Label(root, text= "gg ez on school computer no ai")
+        label.pack()
+        # button
+        button = tk.Button (root, text="proxy" command=on_button_click)
+        button.pack()
+
+        def on_button_click():
+           while inf = "none"
+            print"fucked by rxiled"
+            
+
+
+
+    root.mainloop()
