f97c0f1c2b9e20ef477fc229598d2f39a97c3821
eroge-69 <erogexunamed@gmail.com>
2025-07-29 01:17:13 +0700

    Upload кошка1. мурка.py via PY to EXE tool

---
+++ python-files/кошка1. мурка.py
@@ -0,0 +1,76 @@
+$ pip install pytest
+$ pip install -e .
+$ pytest
+
+a = input("привет")
+
+if (a == "привет кошка"):
+    print(" ___")
+    print("|_._|")
+    print("мяу")
+else:
+    print("ничего не получится")
+    b = input()
+    if (b == "почему"):
+        print("потому")
+        print(" ___")
+        print("|_._|")
+    else:
+        print("уходи!")
+
+c = input()
+if (c == "давай играть"):
+    print("давай")
+    print(" _____")
+    print("|_0.0_|")
+m = int(input("сколько будет 5+5"))
+if (m == 10):
+    print("правильно. мяу")
+else:
+    print("неправильно")
+
+m = int(input("сколько будет 3+4"))
+if (m == 7):
+    print("правильно. мяу")
+else:
+    print("неправильно")
+
+m = int(input("сколько будет 20+50"))
+if (m == 70):
+    print("правильно. мяу")
+else:
+    print("неправильно")
+
+m = int(input("сколько будет 200+100"))
+if (m == 300):
+    print("правильно. мяу")
+else:
+    print("неправильно")
+
+m = int(input("сколько будет 1250+345"))
+if (m == 1595):
+    print("правильно. мяу")
+else:
+    print("неправильно")
+print("закончили.")
+x = input("ну как?")
+if (x == "хорошо"):
+    print("хорошо")
+    print(" ___")
+    print("|_._|")
+elif (x == "плохо"):
+    print("плохо")
+    print(" ___")
+    print("|_._|")
+else:
+    print("чего")
+    print(" _____")
+    print("|_0.0_|")
+print("мяу. пока")
+print(" _____")
+print("|_0.0_|")
+x = input()
+if (x == "пока"):
+    print("пока")
+else:
+    print( )
