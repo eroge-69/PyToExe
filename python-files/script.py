35e9afa290cc48be928d0d8c796e5cba728c27c8
eroge-69 <erogexunamed@gmail.com>
2025-07-28 08:25:50 +0700

    Upload script.py via PY to EXE tool

--- python-files/script.py
+++ python-files/script.py
@@ -1,10 +1,12 @@
-import pyautogui as gui
-import time
+import random
+import scratchattach as sa
+import webbrowser
 
-number = 8
-
-while True:
-	gui.typewrite(str(number))
-	gui.press("enter")
-	number += 1
-	time.sleep(1.5)
+hortini = sa.get_user("Hortini")
+lilhort = sa.get_user("LittleHort")
+hortina = sa.get_user("Hortina")
+hortmini = sa.get_user("Hortmini")
+joert = sa.get_user("Joert")
+messagenum = hortini.message_count() + lilhort.message_count() + hortina.message_count() + hortmini.message_count() + joert.message_count()
+print(messagenum)
+input("press enter to leave ")
