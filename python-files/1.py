595abbab91a9fb5a5c96a71c3f84d7821984b714
eroge-69 <erogexunamed@gmail.com>
2025-07-29 15:05:15 +0700

    Upload 1.py via PY to EXE tool

--- python-files/1.py
+++ python-files/1.py
@@ -1,14 +1,22 @@
-import tkinter as tk
 
+import win32gui
+import win32con
+import ctypes
+import math
 
-root = tk.Tk()
-
-
-root.title('Привет!')
-
-
-label = tk.Label(root, text='Привет, Коля!', font=('Arial', 16))
-label.pack(pady=20)
-
-
-root.mainloop()
+user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
+[sw, sh] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)] 
+hdc = win32gui.GetDC(0)
+dx = dy = 1
+angle = 0
+size = 1
+speed = 1
+while True:
+    
+    win32gui.BitBlt(hdc, 0, 0, sw, sh, hdc, dx, dy, win32con.SRCCOPY)
+    dx = math.ceil(math.sin(angle) * size * 10)
+    dy = math.ceil(math.cos(angle) * size * 10)
+    angle += speed / 10
+    if angle > math.pi :
+        angle = math.pi * -1
