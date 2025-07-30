e52bf6460fd8efc3be2309f2c7f7552129a1be59
eroge-69 <erogexunamed@gmail.com>
2025-07-30 18:01:24 +0700

    Upload Yeni Metin Belgesi.py via PY to EXE tool

--- python-files/Yeni Metin Belgesi.py
+++ python-files/Yeni Metin Belgesi.py
@@ -1,41 +1,36 @@
-import os
+import tkinter as tk
 
+def buton():
+ etiket.delete(0, tk.END) 
+ etiket.insert(0, "1")
+  
 
+def qbuton():
+ etiket.delete(0, tk.END) 
+ etiket.insert(0, "2")
 
-def main():
+def abuton():
+ etiket.delete(0, tk.END) 
+ etiket.insert(0, "3")
+def pbuton():
+ etiket.delete(0, tk.END) 
+ etiket.insert(0, "4")
+pencere = tk.Tk()
+pencere.geometry("300x400")
+pencere.title("Boş Alan")
 
-  inp, out = "input.txt", "output.txt"
+# Kullanıcının yazacağı giriş kutusu
+etiket = tk.Entry(pencere, font=("Arial", 16))
+etiket.pack(pady=10)
 
-  if not os.path.exists(inp):
+buton = tk.Button(pencere, text= "1", command=buton)
+buton.pack(pady=10)
 
-    print("HATA: input.txt bulunamadı.")
+qbuton = tk.Button(pencere, text= "2", command=qbuton)
+qbuton.pack(pady=10)
 
-    input("Devam etmek için tuşa basın...")
-
-    return
-
-  with open(inp, "r", encoding="utf-8", errors="ignore") as f:
-
-    lines = f.readlines()
-
-  res = []
-
-  for i, l in enumerate(lines):
-
-    if l.rstrip().endswith("0"):
-
-      res += lines[i+1:i+7]
-
-  with open(out, "w", encoding="utf-8") as f:
-
-    f.writelines(res)
-
-  print("Tamamlandı! output.txt oluşturuldu.")
-
-  input("Çıkmak için tuşa basın...")
-
-
-
-if __name__ == "__main__":
-
-  main()
+abuton = tk.Button(pencere, text= "3", command=abuton)
+abuton.pack(pady=10)
+pbuton = tk.Button(pencere, text= "4", command=pbuton)
+pbuton.pack(pady=10)
+pencere.mainloop()
