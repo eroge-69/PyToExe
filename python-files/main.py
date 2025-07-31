07eb9baaaf6e3478ca4da8c6f503844fcae442c5
eroge-69 <erogexunamed@gmail.com>
2025-07-31 22:38:09 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,2 +1,24 @@
-import webbrowser
-webbrowser.open("index.html")
+import os 
+import re 
+import pandas as pd 
+
+def extract_names(text):
+    return re.findall(r'\\b[A-Z])[a-z]*\\b',text)
+    
+    def 
+    extract_names_from_files_and_write_to_excel(directory,excel_path):
+        all_names = []
+        for filename in
+        os.listdir(directory):
+            with
+            open(os.path.join(directory,filename),'r') as file:
+                text=
+                file.read()
+                names=
+                extract_names(text)
+                for name
+                in names:
+                    all_names.append({'filename':filename,'name':})
+                    df=pd.dataframe(all_names)
+                    df.to_excel(excel_path,index=False)
+                    return df
