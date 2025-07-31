d2a4913a59c1fa09a9d447c0d1bdaa07d4e9f246
eroge-69 <erogexunamed@gmail.com>
2025-07-31 13:53:24 +0700

    Upload testcode.py via PY to EXE tool

---
+++ python-files/testcode.py
@@ -0,0 +1,9 @@
+#برنامه بنویسید  که اسم و سن طرف را بگیرد و اگر سن بالای  18 بود باید بره سربازی 
+name = input("name to chist : ")
+age = int(input("age to chist : "))
+
+
+if age >18 and name !="sara":
+    print("hello",(name),"bro sarbazi")
+else:
+    print("naro sasbazi  mr" (name))
