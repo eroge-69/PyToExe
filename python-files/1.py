26291b07b02e7a147778c9d4971210a2aac49fb4
eroge-69 <erogexunamed@gmail.com>
2025-08-02 05:19:27 +0700

    Upload 1.py via PY to EXE tool

--- python-files/1.py
+++ python-files/1.py
@@ -1,1 +1,16 @@
-print("Michalinka jest fajna")
+x = int (input('пиши пароль'))
+if x==558:
+    print("TRUE")
+else:
+  print("FALSE")
+y = int (input('122+769='))
+if y==891:
+    print("TRUE")
+else:
+    print("FALSE")
+i = int (input('ДЕДИНСАЙД'))
+if i==993:
+    print("TRUE")
+else:
+    print("FALSE")
+print("каким образом ответил на последний вопрос сука я знал что ты шизик (:")
