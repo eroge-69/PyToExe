b50535c6c9af0b3429919259220b811d2d216b18
eroge-69 <erogexunamed@gmail.com>
2025-08-03 19:45:37 +0700

    Upload battlefield6_fake.py via PY to EXE tool

---
+++ python-files/battlefield6_fake.py
@@ -0,0 +1,10 @@
+
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Battlefield 6 - BETA 0.17.0")
+root.geometry("500x300")
+label = tk.Label(root, text="Connecting to EA servers...", font=("Arial", 16))
+label.pack(expand=True)
+
+root.mainloop()
