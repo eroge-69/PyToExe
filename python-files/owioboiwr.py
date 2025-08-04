f8931149807aeeb6be34552fb5e1cbff09835e70
eroge-69 <erogexunamed@gmail.com>
2025-08-05 04:26:21 +0700

    Upload owioboiwr.py via PY to EXE tool

---
+++ python-files/owioboiwr.py
@@ -0,0 +1,6 @@
+time = int(input("What time are you going to go to sleep? "))
+while time != 1:
+    print("1")
+    time = int(input("What time are you going to go to sleep? "))
+print("Excellent")
+        
