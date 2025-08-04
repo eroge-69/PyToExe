c24b05dec02e60c071686a4a758c52d761e05b91
eroge-69 <erogexunamed@gmail.com>
2025-08-05 03:28:19 +0700

    Upload Future.py via PY to EXE tool

---
+++ python-files/Future.py
@@ -0,0 +1,14 @@
+def Future() :
+    S = str(input ("Did you got a job in game industry? (N/Y) "))
+    if S ==("Y"):
+        S = True
+    else :
+        S = False
+    
+    if S :
+        print ("Leave medicine.")
+    else :
+        print ("Just contine this miesrable life.")
+        
+Future()
+        
