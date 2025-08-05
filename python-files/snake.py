5c6ec7a42d549e1f25e0c99d74ec702d714a69d0
eroge-69 <erogexunamed@gmail.com>
2025-08-05 18:47:24 +0700

    Upload snake.py via PY to EXE tool

--- python-files/snake.py
+++ python-files/snake.py
@@ -1,161 +1,57 @@
-import pygame
 import random
-pygame.init()
-pygame.mixer.init()
+import curses
 
+# Инициализация экрана
+screen = curses.initscr()
+curses.curs_set(0)  # Скрыть курсор
+height, width = 20, 60
+win = curses.newwin(height, width, 0, 0)
+win.keypad(1)
+win.timeout(100)
 
+# Начальная позиция змейки и еды
+snake = [[10, 30], [10, 29], [10, 28]]
+food = [random.randint(1, height-2), random.randint(1, width-2)]
+win.addch(food[0], food[1], '*')
 
-white = (255,255,255)
-red = (255,0,0)
-black = (0,0,0)
-r = (0,0,55)
+# Начальное направление
+key = curses.KEY_RIGHT
 
-screen_width = 800
-screen_height = 600
-game_window = pygame.display.set_mode((screen_width,screen_height))
+while True:
+    next_key = win.getch()
+    key = key if next_key == -1 else next_key
 
-sound = pygame.mixer.Sound('eat.mp3')
-bgimg = pygame.image.load('img.jpg')
-bgimg = pygame.transform.scale(bgimg,(screen_width,screen_height)).convert_alpha()
+    # Вычисляем новое положение головы
+    head = [snake[0][0], snake[0][1]]
 
-pygame.display.set_caption("Snakes like AS")
-pygame.display.update()
-#Game Control variablee
+    if key == curses.KEY_DOWN:
+        head[0] += 1
+    elif key == curses.KEY_UP:
+        head[0] -= 1
+    elif key == curses.KEY_LEFT:
+        head[1] -= 1
+    elif key == curses.KEY_RIGHT:
+        head[1] += 1
 
-font = pygame.font.SysFont("jokerman",52)
-clock = pygame.time.Clock()
-fps = 30
+    # Проверка на столкновение со стеной или собой
+    if (
+        head in snake or
+        head[0] in [0, height - 1] or
+        head[1] in [0, width - 1]
+    ):
+        curses.endwin()
+        quit()
 
-def plot_snake(game_window,color,snake_list,snake_size):
-    for x,y in snake_list:
-        pygame.draw.rect(game_window,color,[x,y,snake_size,snake_size])
-    
+    # Добавляем новую голову
+    snake.insert(0, head)
 
-def text_s(text,color,x,y):
-    s_text = font.render(text,True,color)
-    game_window.blit(s_text,[x,y])
-def welcome():
-    exit_game = False
-    while not exit_game:
-        game_window.fill(white)
-        game_window.blit(bgimg,(0,0))
-        text_s("Welcome to the World of Nags",red,2,240)
-        text_s("       Press Space",red,2,300)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit_game == True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    pygame.mixer.music.load('back.mp3')
-                    pygame.mixer.music.play()
-                    gameloop()
+    # Проверка на еду
+    if head == food:
+        food = [random.randint(1, height - 2), random.randint(1, width - 2)]
+        win.addch(food[0], food[1], '*')
+    else:
+        tail = snake.pop()
+        win.addch(tail[0], tail[1], ' ')
 
-        pygame.display.update()
-        clock.tick(30)        
-def gameloop():
-    game_over = False
-    exit_game = False
-    snake_x = 69
-    snake_y = 76
-    snake_size = 16
-    food_size = 16
-    clock = pygame.time.Clock()
-    fps = 30
-    velocity_x = 0
-    velocity_y = 0
-    e_velo = 5
-    food_x = random.randint(20,screen_width/2)
-    food_y = random.randint(20,screen_height/2)
-    score = 0
-    font = pygame.font.SysFont("jokerman",55)
-    snake_list = []
-    snake_lenght = 1
-        
-    while not exit_game:
-        if game_over:
-            game_window.fill(white)
-            game_window.blit(bgimg,(0,0))
-            text_s("Game Over , Press Enter",red,100,200)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit_game = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        welcome()
-        else:   
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit_game = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        velocity_x = e_velo
-                        velocity_y = 0
-                    if event.key == pygame.K_LEFT:
-                        velocity_x = -e_velo
-                        velocity_y = 0
-                    if event.key == pygame.K_UP:
-                        velocity_y = -e_velo
-                        velocity_x = 0
-                    if event.key == pygame.K_DOWN:
-                        velocity_y = e_velo
-                        velocity_x = 0
-
-            
-        
-            snake_x += velocity_x
-            snake_y += velocity_y
-
-            if abs(snake_x - food_x)<9 and abs(snake_y - food_y)<9:
-                score += 1
-                food_x = random.randint(60,screen_width/2)
-                food_y = random.randint(60,screen_height/2)
-                snake_lenght += 5
-                '''
-                f =  open('hiscore.txt','w')
-                h = f.write(str(score))
-                a = open('hiscore.txt','r')
-                b = a.read()
-                
-                print(b)
-                '''
-                sound.play()
-                
-            if snake_x < 0 or snake_x > screen_width or snake_y > screen_height or snake_y < 0:
-                game_over = True
-                pygame.mixer.music.load('game over.mp3')
-                pygame.mixer.music.play()
-            
-                
-                    
-            game_window.fill(white)
-            game_window.blit(bgimg,(0,0))
-            text_s("Score:- "+ str(score),red,30,5)
-            pygame.draw.rect(game_window,red,[food_x,food_y,snake_size,snake_size])   
-            pygame.draw.rect(game_window,black,[snake_x,snake_y,snake_size,snake_size])
-            plot_snake(game_window,black,snake_list,snake_size)
-
-            head = []
-            head.append(snake_x)
-            head.append(snake_y)
-            snake_list.append(head)
-            
-                
-            if len(snake_list) > snake_lenght:
-                del snake_list[0]
-                
-            if head in snake_list[:-1]:
-                game_over = True
-                pygame.mixer.music.load('game over.mp3')
-                pygame.mixer.music.play()
-                
-        pygame.display.update()
-        clock.tick(fps)#Frames per second
-
-        
-     
-    
-    pygame.quit()
-    quit()
-welcome()    
-
-            
+    # Отображаем голову
+    win.addch(snake[0][0], snake[0][1], '#')
