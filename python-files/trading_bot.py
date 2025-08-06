b84c303e90732f096912487ec36cbcee70107414
eroge-69 <erogexunamed@gmail.com>
2025-08-07 01:22:47 +0700

    Upload trading_bot.py via PY to EXE tool

---
+++ python-files/trading_bot.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import ta
+import matplotlib.pyplot as plt
+from scipy.signal import argrelextrema
+
+print("✅ برنامج تحليل التداول جاهز!")
+input("اضغط Enter لإغلاق النافذة...")
