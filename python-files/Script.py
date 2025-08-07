1cf812568ec9630fef658f23b56ccfce434511ef
eroge-69 <erogexunamed@gmail.com>
2025-08-07 16:31:57 +0700

    Upload Script.py via PY to EXE tool

--- python-files/Script.py
+++ python-files/Script.py
@@ -8,12 +8,19 @@ df = df.drop(["CVR%","AOV","Days since campaign start","Sessions","Transactions"
 ##remove all values which are 0 
 df = df[(df!=0).all(1)]
 ##check prerequesities
-df = df[df['Campaign'].str.count('_') == 3]
-df = df[~df['Campaign'].str.contains('test',case=False,na=False)]
-df = df[~df['Campaign'].str.contains('aut',case=False,na=False)]
+df = df[df['Campaign'].str.count('_') >= 3]
+
+#df = df[~df['Campaign'].str.contains('test',case=False,na=False)]
+
+#df = df[~df['Campaign'].str.contains(r'\bAUT\b', case=False, na=False)]
 
 
-df[['Date','Region','type','Name']] = df['Campaign'].str.split('_',expand=True)
+
+#sort the values by date
+#df = df.sort_values(by=['Campaign'], ascending=True)
+
+df[['Date','Region','type','Name','test']] = df['Campaign'].str.split('_',n=4,expand=True)
+
 
 #drop aggregated col
 df.drop('Campaign', axis=1, inplace=True)
@@ -22,12 +29,19 @@ df.drop('Campaign', axis=1, inplace=True)
 col = 'GBP Revenue'
 df = df[[c for c in df.columns if c != col] + [col]]
 
+
+print(df.head)
+
+
 #aggregation sum of revenue by name
 df = df.groupby('Name',as_index=False)['GBP Revenue'].sum()
 
 
-#sort the values by date
-#df = df.sort_values(by=['Campaign'], ascending=True)
-print(df.head)
+df['GBP Revenue'] = df['GBP Revenue'].astype(int)
 
-df.to_csv('output.csv', index=False)
+df['GBP Revenue'] = df['GBP Revenue'].astype(int).apply(lambda x: f"{x:,}")
+df['GBP Revenue'] = '£'+ df['GBP Revenue'].astype(str)
+
+print(df.head)
+#save to output file
+df.to_csv('output.csv', encoding='utf-8-sig',index=False)
