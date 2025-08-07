da8109945af1c073c2d36ddcb86dc61f059ac3b3
eroge-69 <erogexunamed@gmail.com>
2025-08-07 16:07:10 +0700

    Upload virus.py via PY to EXE tool

--- python-files/virus.py
+++ python-files/virus.py
@@ -1,18 +1,1 @@
-import time
-from datetime import datetime
-
-target = datetime(2025, 8, 6, 0, 0, 0)
-
-while True:
-    now = datetime.now()
-    remaining = target - now
-
-    if remaining.total_seconds() <= 0:
-        print("Time's up!")
-        break
-
-    hours, remainder = divmod(int(remaining.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    print(f"{hours}h {minutes}m {seconds}s left until the deadline")
-    time.sleep(1)
+print("Virus")
