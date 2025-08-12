656621da7dd1f26d26a98f7338aab7220fd9ef23
eroge-69 <erogexunamed@gmail.com>
2025-08-12 07:03:17 +0700

    Upload client.py via PY to EXE tool

--- python-files/client.py
+++ python-files/client.py
@@ -1,36 +1,11 @@
 import socket
-import pyautogui
-import io
 
-SERVER_IP = "127.0.0.1"  # غيّر هذا إلى IP جهاز الـ Mac
+SERVER_ADDRESS = '192.168.1.6'
 SERVER_PORT = 5000
-PASSWORD = "Bmw_m3"  # كلمة مرور بسيطة للمصادقة
 
-def send_screenshot():
-    screenshot = pyautogui.screenshot()
-    img_bytes = io.BytesIO()
-    screenshot.save(img_bytes, format='PNG')
-    img_data = img_bytes.getvalue()
-    return img_data
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((SERVER_ADDRESS, SERVER_PORT))
+    data = s.recv(1024)
+    print('Received', repr(data))
 
-def main():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((SERVER_IP, SERVER_PORT))
-        s.send(PASSWORD.encode())  # تحقق كلمة المرور
-
-        while True:
-            command = s.recv(1024).decode()
-            if command == "screenshot":
-                img_data = send_screenshot()
-                s.sendall(len(img_data).to_bytes(4, 'big'))
-                s.sendall(img_data)
-            elif command == "exit":
-                break
-    except Exception as e:
-        pass
-    finally:
-        s.close()
-
-if __name__ == "__main__":
-    main()
+input()
