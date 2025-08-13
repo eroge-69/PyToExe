b41947c99136f915582421caac248aebbb5fe32c
eroge-69 <erogexunamed@gmail.com>
2025-08-14 02:26:27 +0700

    Upload Clock.py via PY to EXE tool

--- python-files/Clock.py
+++ python-files/Clock.py
@@ -1,16 +1,28 @@
-import time
 import tkinter as tk
-def a():
-    current_time = time.strftime('%H:%M:%S')
-    label.config(text=current_time)
-    label.after(1000, a)
-t = tk.Tk()
-t.title('Clock')
-t.geometry('250x100')
-t.configure(bg='black')
-label = tk.Label(t, font=('irancell', 40), bg='black', fg='green')
-label.pack()
-a()
+from time import strftime
+import ctypes
 
+# Show message box using ctypes
+ctypes.windll.user32.MessageBoxW(
+    0,
+    "This collects Date But Dosen't Sell to Cybercriminals/Hackers, "
+    "But Tracks your location and Tells the time So don't worry "
+    "Your address, age, ID, Credit card, or Location Won't be found.",
+    "Privacy Info",
+    0
+)
 
+# Clock GUI
+root = tk.Tk()
+root.title("Clock")
 
+def time():
+    string = strftime('%H:%M:%S %p\n%A\n%B %d, %Y')
+    label.config(text=string)
+    label.after(1000, time)
+
+label = tk.Label(root, font=('calibri', 40, 'bold'), background='black', foreground='white')
+label.pack(anchor='center')
+
+time()
+root.mainloop()
