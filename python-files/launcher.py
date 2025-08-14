6c95be518533617f3ddde97e2fddb8acfa335d4f
eroge-69 <erogexunamed@gmail.com>
2025-08-14 23:48:14 +0700

    Upload launcher.py via PY to EXE tool

--- python-files/launcher.py
+++ python-files/launcher.py
@@ -1,9 +1,29 @@
-import subprocess
+#!/usr/bin/env python3
 import os
+import subprocess
+import stat
+import sys
 
-def launch_dashboard():
-    dashboard_path = os.path.expanduser("~/AdvisorOne/FluidDashboard.py")
-    subprocess.run(["python", dashboard_path])
+scriptdir = os.path.dirname(os.path.realpath(__file__))
 
-if __name__ == "__main__":
-    launch_dashboard()
+# Go to game folder
+game_dir = os.path.join(scriptdir, "game")
+try:
+    os.chdir(game_dir)
+except FileNotFoundError:
+    sys.exit(1)
+
+# Set permissions to 0700
+for root, dirs, files in os.walk(game_dir):
+    for d in dirs:
+        os.chmod(os.path.join(root, d), 0o700)
+    for f in files:
+        os.chmod(os.path.join(root, f), 0o700)
+
+# Run setup if pinned_libs_64 is missing
+pinned_libs = os.path.join(game_dir, "steam-runtime", "pinned_libs_64")
+if not os.path.isdir(pinned_libs):
+    subprocess.run([os.path.join(game_dir, "steam-runtime", "setup.sh")])
+
+# Run the game
+subprocess.run([os.path.join(game_dir, "steam-runtime", "run.sh"), "./dowser"])
