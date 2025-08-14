6a4f092c0dfcc1345f49b68d261066a2e280c280
eroge-69 <erogexunamed@gmail.com>
2025-08-14 20:04:40 +0700

    Upload Hello.py via PY to EXE tool

--- python-files/Hello.py
+++ python-files/Hello.py
@@ -1,1 +1,31 @@
-print('Hello')
+import turtle
+
+turtle.pensize(3)
+
+def draw_heart_curve():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+        
+turtle.color("pink", "pink")
+
+turtle.begin_fill()
+
+turtle.left(140)
+turtle.forward(111.65)
+draw_heart_curve()
+
+turtle.left(120)
+draw_heart_curve()
+turtle.forward(111.65)
+
+turtle.end_fill()
+
+turtle.color('deep pink')
+style = ('Comic Sans MS', 35, 'italic')
+turtle.write('Я люблю тебя Настюш!', font=style, align='center')
+
+
+turtle.hideturtle()
+turtle.done()
+
