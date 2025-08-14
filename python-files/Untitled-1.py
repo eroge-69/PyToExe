ebc9bbeabded021e238120594e3c844cfbc8cdf0
eroge-69 <erogexunamed@gmail.com>
2025-08-14 15:40:16 +0700

    Upload Untitled-1.py via PY to EXE tool

--- python-files/Untitled-1.py
+++ python-files/Untitled-1.py
@@ -1,2 +1,9 @@
-x = input("number : ")
-print(x)
+print("Berechne deine Taxikosten dein Programmierer Finn Gräbner")
+start_price = 4.80
+km = int(input("Bitte Kilometer eingeben: "))
+if km > 5:
+    costs = 1.90
+ else:  
+    costs = 2.10
+total_expenses = start_price + costs * km
+print("Deine kosten im Taxi sind:", total_expenses)
