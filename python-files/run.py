3e69b077df56cc8d956186c893c5f26227fc0efb
eroge-69 <erogexunamed@gmail.com>
2025-08-15 13:28:00 +0700

    Upload run.py via PY to EXE tool

--- python-files/run.py
+++ python-files/run.py
@@ -1,31 +1,73 @@
-import turtle
+import http.server
+import ssl
+import os
+import shutil
 
-turtle.pensize(3)
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # serve index.html for root
+        if self.path == "/":
+            self.path = "/index.html"
 
-def draw_heart_curve():
-    for i in range(200):
-        turtle.right(1)
-        turtle.forward(1)
-        
-turtle.color("pink", "pink")
+        # normalize request path (remove query/hash)
+        req_path = self.path.split('?',1)[0].split('#',1)[0]
+        accept_encoding = self.headers.get('Accept-Encoding', '')
 
-turtle.begin_fill()
+        # translate to local filesystem path using built-in helper
+        fs_path = self.translate_path(req_path)
 
-turtle.left(140)
-turtle.forward(111.65)
-draw_heart_curve()
+        # If client explicitly requested a .br file -> serve it with Content-Encoding: br
+        if req_path.endswith('.br') and os.path.exists(fs_path):
+            orig_path = req_path[:-3]  # used to detect Content-Type
+            ctype = self.guess_type(orig_path)
+            try:
+                f = open(fs_path, 'rb')
+            except OSError:
+                return super().do_GET()
+            try:
+                self.send_response(200)
+                self.send_header("Content-Type", ctype)
+                self.send_header("Content-Encoding", "br")
+                self.send_header("Vary", "Accept-Encoding")
+                fs = os.fstat(f.fileno())
+                self.send_header("Content-Length", str(fs.st_size))
+                self.end_headers()
+                shutil.copyfileobj(f, self.wfile)
+            finally:
+                f.close()
+            return
 
-turtle.left(120)
-draw_heart_curve()
-turtle.forward(111.65)
+        # If client accepts brotli and precompressed .br exists -> serve that with Content-Encoding: br
+        if 'br' in accept_encoding and os.path.exists(fs_path + '.br'):
+            br_fs_path = fs_path + '.br'
+            ctype = self.guess_type(req_path)
+            try:
+                f = open(br_fs_path, 'rb')
+            except OSError:
+                return super().do_GET()
+            try:
+                self.send_response(200)
+                self.send_header("Content-Type", ctype)
+                self.send_header("Content-Encoding", "br")
+                self.send_header("Vary", "Accept-Encoding")
+                fs = os.fstat(f.fileno())
+                self.send_header("Content-Length", str(fs.st_size))
+                self.end_headers()
+                shutil.copyfileobj(f, self.wfile)
+            finally:
+                f.close()
+            return
 
-turtle.end_fill()
+        # otherwise fall back to default behavior
+        return super().do_GET()
 
-turtle.color('deep pink')
-style = ('Comic Sans MS', 35, 'italic')
-turtle.write('Я люблю тебя Настюш!', font=style, align='center')
+host = "localhost"
+port = 8080
 
+httpd = http.server.HTTPServer((host, port), MyHandler)
+# SSL: key.pem и cert.pem должны быть в той же папке
+httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="key.pem", certfile="cert.pem", server_side=True)
 
-turtle.hideturtle()
-turtle.done()
-
+print(f"HTTPS сервер запущен на https://{host}:{port}")
+print("Файлы в текущей папке:", os.listdir("."))
+httpd.serve_forever()
