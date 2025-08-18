9d4d645ee44afc1661d6fbae67dee6c7acb7fae0
eroge-69 <erogexunamed@gmail.com>
2025-08-19 01:05:02 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,91 +1,169 @@
-import re
+import sys
+import time
+import subprocess
+import os
+import requests
+import zipfile
+import hashlib
+import inspect
+import functools
+from tqdm import tqdm
+from time import sleep
+from keyauth import api
+import keyauth
 
-stroka = """38088  10  100  0
-115709  0  100  0
-144  -1  100  0
-59  -3  100  0
-3171  10  300  0
-1  2  300  0
-2  1  300  0
-9060  0  300  0
-600  -1  300  0
-52  1  400  0
-161  0  400  0
-2  -1  400  0
-2  1  402  0"""
+# ======================== Настройки ========================
+username = "Test"
+build = "Alpha 1.16.5"
+loader_version = "1.0.3"
+cheat_version = "1.5.0"
 
-def get_info(text):
-    
-    my_dict = {
-        "Pallet": [0,0,0], # 1,402,VIRTUAL
-        "Case": [0,0], # 1,10
-        "Bottle": [0,0,0] # 1,10,-1
-    }
+def getchecksum():
+    md5_hash = hashlib.md5()
+    file = open(''.join(sys.argv), "rb")
+    md5_hash.update(file.read())
+    digest = md5_hash.hexdigest()
+    return digest
 
-    mas = stroka.splitlines()
-    mas_ad = []
-    otvet = ''
-    for i in mas:
-        mas_ad.append(i.split())
-    
-    for i in mas_ad:
-        Counter = int(i[0])
-        Status = int(i[1])
-        Type = int(i[2])
-        NotificationStatus = int(i[3])
-        
-        if Type == 100 and Status != 0:
-            if Status == 1:
-                my_dict['Bottle'][0] += Counter
-            if Status == 10:
-                my_dict['Bottle'][1] += Counter
-                
-        if Type == 300 and Status != 0:
-            if Status == 1:
-                my_dict['Case'][0] += Counter
-            if Status == 10:
-                my_dict['Case'][1] += Counter
-        
-        if Type == 400 and Status != 0:
-            if Status == 1:
-                my_dict['Pallet'][0] += Counter
-        
-        if Type == 402 and Status != 0:
-            if Status == 1:
-                my_dict['Pallet'][1] += Counter
-        
-        if Type == 405 and Status != 0:
-            my_dict['Pallet'][2] += Counter
-                
-    for i in my_dict:
-        num = 0
-        if i == 'Pallet':
-            num = my_dict[i][0] + my_dict[i][1] + my_dict[i][2]
-            otvet += 'Паллеты - Всего(' + str(num) + '): '
-            if int(my_dict[i][0]) != 0 :
-                otvet +=  str(my_dict[i][0]) + ' полные'
-            if int(my_dict[i][1]) != 0 :
-                otvet +=  ' + ' + str(my_dict[i][1]) + ' неполные '
-            if int(my_dict[i][2]) != 0 :
-                otvet += ' + ' + str(my_dict[i][2]) + ' виртуальные '
-            
-        if i == 'Case':
-            num = my_dict[i][0] + my_dict[i][1]
-            otvet += '\nКейсы: Всего(' + str(num) + '): '
-            if int(my_dict[i][0]) != 0 :
-                otvet +=  str(my_dict[i][0]) + ' упакованные'
-            if int(my_dict[i][1]) != 0 :
-                otvet +=  ' + ' + str(my_dict[i][1]) + ' не агрег. в паллеты '
-        
-        if i == 'Bottle':
-            num = my_dict[i][0] + my_dict[i][1]
-            otvet += '\nБутылки: Всего(' + str(num) + '): '
-            if int(my_dict[i][0]) != 0 :
-                otvet +=  ' + ' + str(my_dict[i][0]) + ' упакованные '
-            if int(my_dict[i][1]) != 0 :
-                otvet +=  str(my_dict[i][1]) + ' не упакованные '
-                
-    return otvet
-    
-            
-    
+keyauthapp = api(
+    name = "LoaderObs", # App name 
+    ownerid = "oSzq4Vxwvr", # Account ID
+    version = "1.0", # Application version. Used for automatic downloads see video here https://www.youtube.com/watch?v=kW195PLCBKs
+    hash_to_check = getchecksum()
+)
+# ======================== Авторизация ========================
+def auth():
+    print("██╗░░░██╗████████╗██╗  ████████╗██╗██████╗░███████╗██╗░░██╗░██████╗")
+    print("╚██╗░██╔╝╚══██╔══╝╚═╝  ╚══██╔══╝██║██╔══██╗██╔════╝╚██╗██╔╝██╔════╝")
+    print("░╚████╔╝░░░░██║░░░░░░  ░░░██║░░░██║██║░░██║█████╗░░░╚███╔╝░╚█████╗░")
+    print("░░╚██╔╝░░░░░██║░░░░░░  ░░░██║░░░██║██║░░██║██╔══╝░░░██╔██╗░░╚═══██╗")
+    print("░░░██║░░░░░░██║░░░██╗  ░░░██║░░░██║██████╔╝███████╗██╔╝╚██╗██████╔╝")
+    print("░░░╚═╝░░░░░░╚═╝░░░╚═╝  ░░░╚═╝░░░╚═╝╚═════╝░╚══════╝╚═╝░░╚═╝╚═════╝░")
+    print("Лоадер сделан Tidexs, YT: Tidexs")
+    sleep(2)
+    print("Добро пожаловать в систему авторизации...")
+    key = input("Введите ключ:")
+    keyauthapp.license(key)
+    sleep(1)
+    print("Авторизация прошла успешно!\n")
+
+# ======================== Загрузка клиента ========================
+def download_and_extract(url: str, extract_dir: str):
+    target_folder = os.path.join(extract_dir)
+    filename = url.split('/')[-1].split(".zip")[0] + ".zip"
+    target_path = os.path.join(target_folder, filename)
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    print("[Загрузка] Начинается загрузка клиента...")
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024
+    t = tqdm(total=total_size, unit='B', unit_scale=True, desc=filename)
+
+    with open(target_path, 'wb') as file:
+        for data in response.iter_content(block_size):
+            t.update(len(data))
+            file.write(data)
+    t.close()
+
+    print("[Распаковка] Извлечение клиента...")
+    with zipfile.ZipFile(target_path, mode='r') as zip_ref:
+        zip_ref.extractall(target_folder)
+
+    try:
+        os.remove(target_path)
+    except OSError:
+        pass
+
+# ======================== Запуск клиента ========================
+def run_cheat():
+    print("\n[Чит] Подготовка к запуску...")
+
+    client_path = "C:\\ObsClient"
+    if not os.path.isdir(client_path):
+        os.mkdir(client_path)
+
+    download_and_extract(
+        url="https://www.dropbox.com/scl/fi/fxhjl9n6sw3rv4hgqi9iw/client.zip?rlkey=f9aq3ew0j51260zs4jor2nmof&st=5vjvwnyn&dl=1",
+        extract_dir=client_path
+    )
+
+    while True:
+        memory_input = input("Введите объём оперативной памяти (в гигабайтах): ")
+        try:
+            memory = int(memory_input)
+            break
+        except ValueError:
+            print("Ошибка: введите корректное число.")
+
+    local_java = os.path.join(client_path, "Client\\jre-21.0.2\\bin\\java.exe")
+    java_exe = local_java if os.path.exists(local_java) else "java"
+    if java_exe == local_java:
+        print("[Java] Используется встроенная Java")
+    else:
+        print("[Java] Встроенная Java не найдена, используется системная")
+
+    print("[Запуск] Запускаю клиент...\n")
+
+    launch_command = [
+        java_exe,
+        f"-Xmx{memory}G",
+        f"-Djava.library.path={client_path}\\Client\\natives",
+        "-cp",
+        f"{client_path}\\Client\\libraries;{client_path}\\Client\\client.jar",
+        "net.minecraft.client.main.Main",
+        "--username", username,
+        "--width", "854",
+        "--height", "480",
+        "--version", "1.16.5",
+        "--gameDir", f"{client_path}\\game",
+        "--assetsDir", f"{client_path}\\Client\\assets",
+        "--assetIndex", "1.16",
+        "--accessToken", "0"
+    ]
+
+    try:
+        subprocess.run(launch_command)
+    except subprocess.CalledProcessError as e:
+        print(f"[Ошибка запуска]: {e}")
+    except FileNotFoundError:
+        print("[Ошибка]: Java не найдена.")
+
+# ======================== Меню ========================
+def menu():
+    global username
+    while True:
+        print("\n=== Главное меню ===")
+        print("1. Изменить ник")
+        print("2. Показать версию лоадера")
+        print("3. Показать версию чита")
+        print("4. Запустить чит")
+        print("5. Выход")
+
+        choice = input("Выберите пункт (1-5): ")
+
+        if choice == '1':
+            new_nick = input("Введите новый ник: ")
+            if new_nick.strip():
+                username = new_nick.strip()
+                print(f"Ник изменён на: {username}")
+            else:
+                print("Ник не может быть пустым.")
+        elif choice == '2':
+            print(f"Версия лоадера: {loader_version}")
+        elif choice == '3':
+            print(f"Версия чита: {cheat_version}")
+        elif choice == '4':
+            run_cheat()
+        elif choice == '5':
+            print("Выход из программы...")
+            break
+        else:
+            print("Неверный выбор. Попробуйте снова.")
+
+# ======================== Запуск ========================
+if __name__ == "__main__":
+    auth()
+    menu()
