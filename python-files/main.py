c1476694e1687f551dce44638cd8d635bdea1d42
eroge-69 <erogexunamed@gmail.com>
2025-08-19 01:17:06 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,29 +1,8 @@
-import keyboard
-import pyautogui
-import time
+import tkinter
+from ventanas import Login
 
-# Define text expansions
-expansions = {
-    "sg": "signin/google"
-}
-
-buffer = ""
-
-def on_key(e):
-    global buffer
-    if e.event_type == "down":
-        if e.name == "space":
-            if buffer in expansions:
-                # Erase the trigger word
-                for _ in range(len(buffer)):
-                    pyautogui.press('backspace')
-                # Type replacement
-                pyautogui.write(expansions[buffer])
-            buffer = ""
-        elif len(e.name) == 1:  # normal character
-            buffer += e.name
-        else:
-            buffer = ""
-
-keyboard.hook(on_key)
-keyboard.wait()
+if __name__ == "__main__":
+    app = tkinter.Tk()
+    app.withdraw()
+    Login()
+    app.mainloop()
