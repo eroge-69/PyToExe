b2ab8051f92176f1c08a471387a052dbba766e68
eroge-69 <erogexunamed@gmail.com>
2025-08-19 23:19:20 +0700

    Upload تبدیل سن به ثانیه و .............................py via PY to EXE tool

---
+++ python-files/تبدیل سن به ثانیه و .............................py
@@ -0,0 +1,11 @@
+name=str(input("نام خود را وارد کن"))
+age=int(input("سن خود را وارد کنيد"))
+day=age*365
+hours=day*24
+min=hours*60
+print("نام کاربر=",name)
+print("تعداد روز ها=",day)
+print("تعداد ساعت ها =",hours)
+print("تعداد دقايق=",min)
+print("OK")
+print("Thank you for coming")
