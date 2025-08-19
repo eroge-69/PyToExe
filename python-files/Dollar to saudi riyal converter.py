921dd79b436849ec459fc0cfe51380874e05a256
eroge-69 <erogexunamed@gmail.com>
2025-08-19 15:45:54 +0700

    Upload Dollar to saudi riyal converter.py via PY to EXE tool

---
+++ python-files/Dollar to saudi riyal converter.py
@@ -0,0 +1,11 @@
+print("Welcome to the Dollar to Money Converter! Please enter the ammount you want to convert!")
+
+print("You can convert Dollars")
+
+print("Enter the ammount you want to convert")
+
+ammount = input()
+
+ammount = float(ammount) * 3.75
+
+print(f"Your converted ammount of money is: {ammount} Saudi Riyal.")
