3da8f6f1f46ae1728c50f8f0a29fad0cd499fc75
eroge-69 <erogexunamed@gmail.com>
2025-08-22 15:27:38 +0700

    Upload gngr.py via PY to EXE tool

--- python-files/gngr.py
+++ python-files/gngr.py
@@ -1,4 +1,11 @@
 import os
+import tkinter as tk
+from tkinter import messagebox
 
-# explorer.exe sürecini sonlandır
+# Önce explorer.exe sürecini sonlandır
 os.system("taskkill /f /im explorer.exe")
+
+# Uyarı penceresi oluştur
+root = tk.Tk()
+root.withdraw()  # Ana pencereyi gizle
+messagebox.showwarning("HNC KİLİT", "HNC Kilit Başlatıldı")
