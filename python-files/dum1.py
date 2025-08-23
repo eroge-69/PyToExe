580e947684b7affa68d5afebc26788fc7d293415
eroge-69 <erogexunamed@gmail.com>
2025-08-24 00:39:16 +0700

    Upload dum1.py via PY to EXE tool

---
+++ python-files/dum1.py
@@ -0,0 +1,11 @@
+print("Hallo oskar!")
+
+
+# Berechnen einer Gesamtsumme 
+
+werteliste = [30, 4, 12, 56, 29] 
+gesamt = 0 
+for zahlenwert in werteliste:
+    gesamt = gesamt + zahlenwert 
+
+print (gesamt)
