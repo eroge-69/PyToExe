499bc590fadb1da76d413c6551e02e885737fee5
eroge-69 <erogexunamed@gmail.com>
2025-08-24 22:46:00 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -2,23 +2,20 @@ import cv2
 import os
 import time
 
-while 1:
-    img_path = r'C:\Users\Admin\Documents\hello\captured.jpg'
-    img = cv2.imread(img_path)
 
-    cascade_path = r'C:\Users\Admin\Documents\hello\haarcascade_frontalface_default.xml'
-    human_cascade = cv2.CascadeClassifier(cascade_path)
+img_path = r'C:\Users\Admin\Documents\hello\captured.jpg'
+img = cv2.imread(img_path)
 
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    humans = human_cascade.detectMultiScale(img_gray, 1.1, 4)
+cascade_path = r'C:\Users\Admin\Documents\hello\haarcascade_frontalface_default.xml'
+human_cascade = cv2.CascadeClassifier(cascade_path)
 
-    for (x, y, w, h) in humans:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+humans = human_cascade.detectMultiScale(img_gray, 1.1, 4)
 
-    save_path = r'C:\Users\Admin\Documents\hello'
-    os.chdir(save_path)
+for (x, y, w, h) in humans:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imwrite('output.jpg', img)
+save_path = r'C:\Users\Admin\Documents\hello'
+os.chdir(save_path)
 
-    print('.')
-    time.sleep(0.5)
+cv2.imwrite('output.jpg', img)
