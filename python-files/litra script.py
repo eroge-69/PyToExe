fe95d4c6f44f0b5f27a13c4ab51bd744ba08839b
eroge-69 <erogexunamed@gmail.com>
2025-08-26 10:06:32 +0700

    Upload litra script.py via PY to EXE tool

--- python-files/litra script.py
+++ python-files/litra script.py
@@ -5,7 +5,8 @@ import ctypes
 import win32con
 import win32gui
 import win32api
-from infi.systray import SysTrayIcon
+import pystray
+from PIL import Image
 
 # Programs to run
 PROGRAM_ON = r".\litra.exe on"
@@ -22,11 +23,10 @@ ICON_ON = "bulb_on.ico"
 
 # Globals
 camera_active = False
-systray = None
+tray_icon = None
 
 
 def run_program(command):
-    """Run a command string like '.\\litra.exe on'."""
     try:
         subprocess.Popen(command, shell=True)
     except Exception as e:
@@ -34,7 +34,7 @@ def run_program(command):
 
 
 def wnd_proc(hwnd, msg, wparam, lparam):
-    global camera_active, systray
+    global camera_active, tray_icon
 
     if msg == WM_DEVICECHANGE:
         if wparam == DBT_DEVICEARRIVAL:  # Camera ON
@@ -42,19 +42,17 @@ def wnd_proc(hwnd, msg, wparam, lparam):
                 print("📷 Camera ON")
                 run_program(PROGRAM_ON)
                 camera_active = True
-                if systray:
-                    systray.icon = ICON_ON
-                    systray.hover_text = "Camera ON"
-                    systray.update()
+                if tray_icon:
+                    tray_icon.icon = Image.open(ICON_ON)
+                    tray_icon.title = "Camera ON"
         elif wparam == DBT_DEVICEREMOVECOMPLETE:  # Camera OFF
             if camera_active:
                 print("📷 Camera OFF")
                 run_program(PROGRAM_OFF)
                 camera_active = False
-                if systray:
-                    systray.icon = ICON_OFF
-                    systray.hover_text = "Camera OFF"
-                    systray.update()
+                if tray_icon:
+                    tray_icon.icon = Image.open(ICON_OFF)
+                    tray_icon.title = "Camera OFF"
 
     return win32gui.DefWindowProc(hwnd, msg, wparam, lparam)
 
@@ -67,4 +65,31 @@ def start_message_loop():
     wndClass.lpfnWndProc = wnd_proc
     wndClass.hInstance = hInstance
     wndClass.lpszClassName = className
-    atom = win32gui.RegisterClass(wndC
+    atom = win32gui.RegisterClass(wndClass)
+
+    hwnd = win32gui.CreateWindow(
+        atom,
+        className,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        hInstance,
+        None,
+    )
+
+    win32gui.PumpMessages()
+
+
+def on_quit(icon, item):
+    print("Exiting Camera Watcher...")
+    os._exit(0)
+
+
+def start_tray():
+    global tray_icon
+    tray_icon = pystray.Icon("camera_watcher", Image.open(ICON_OFF), "Camera OFF", menu=pystray.Menu(
+        pystray.MenuItem("Exit",
