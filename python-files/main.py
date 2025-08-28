151c47135a30e29865ce2f8b016e54404c2a1044
eroge-69 <erogexunamed@gmail.com>
2025-08-29 03:57:38 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,45 +1,25 @@
+while True:
+    q1 = input("Do you want to a. Add a new student or b. Search for a student? (enter a or b) ")
 
+    if q1.lower() == "a":
+        student = input("Enter Student Full Name: ")
+        agreement = input("Do they consent to the Marketing Agreement? (enter y or n): ")
+        with open("MarketingAgreement.txt", 'a') as file:  # open in append mode only when adding
+            file.write(student + ": " + agreement + "\n")
 
-print("Selecione o tipo de dispositivo (1 ou 2):\n1 - MDVR\n2 - Dashcam")
-device = int(input())
+    elif q1.lower() == "b":
+        student = input("Enter Student Full Name: ")
+        with open("MarketingAgreement.txt", 'r') as file:  # open in read mode when searching
+            found = False
+            for line in file:
+                curr = line.strip().split(":")
+                if curr[0] == student:
+                    print(curr[1].strip())
+                    found = True
+                    break
+            if not found:
+                print("Student not found.")
 
-print("Insira a quantidade de canais de gravação (de 1 a 8 canais):")
-channels = int(input())
-
-print("Insira o tempo total (em horas) de gravação por dia:")
-record_time = int(input())
-
-print("Selecione a qualidade desejada:\n1 - D1 (10 FPS)\n2 - D2 (25 FPS)\n3 - 720p (10 FPS)\n4 - 720p (25FPS)")
-quality = int(input())
-
-if device == 1:
-    # Armazenamento em megabytes
-    storage = 110000
-elif device == 2:
-    storage = 230000
-
-if quality == 1:
-    # Armazenamento por minuto em MB/min
-    storage_per_minute = 2
-elif quality == 2:
-    storage_per_minute = 5
-elif quality == 3:
-    storage_per_minute = 6
-elif quality == 4:
-    storage_per_minute = 14
-
-storage_per_minute = storage_per_minute * channels
-
-storage_time_in_hours = (storage/storage_per_minute)/60
-hours1 = int(storage_time_in_hours)
-fraction1 = storage_time_in_hours - hours1
-
-storage_time_in_days = storage_time_in_hours/record_time
-days = int(storage_time_in_days)
-fraction2 = storage_time_in_days - days
-
-minutes = round(fraction1 * 60)
-hours2 = round(fraction2 * 24)
-
-print(f"Tempo estimado de armazenamento: {hours1} horas e {minutes} minutos")
-print(f"Tempo estimado de armazenamento: {days} dias e {hours2} horas")
+    q2 = input("Run again (y or n)? ")
+    if q2.lower() != "y":
+        break
