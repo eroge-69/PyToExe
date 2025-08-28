9b28b8b5143c53b5dbd60a67f91ba6f1aadf183b
eroge-69 <erogexunamed@gmail.com>
2025-08-28 23:21:40 +0700

    Upload sector_scraper.py via PY to EXE tool

---
+++ python-files/sector_scraper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Google/SERP tabanlı arama yapıp herkese açık sitelerden e‑posta ve telefon
+bilgilerini toplayan, KVKK/GDPR ve robots.txt'ye saygılı, asenkron Python aracı.
+[... KODUN TAMAMI BURAYA GELECEK ...]
+"""
