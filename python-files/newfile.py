425ba6099b8ad5297a6faa810ef97098f227532f
eroge-69 <erogexunamed@gmail.com>
2025-09-01 07:51:36 +0700

    Upload newfile.py via PY to EXE tool

--- python-files/newfile.py
+++ python-files/newfile.py
@@ -1,10 +1,8 @@
-bebdjxjs
-fjckdk
-	jrjfjsejt
-		fjdisjrjr
-		
-		
-jdjcjrjs
-	djjfrj
-	jdjdjfrj
-	dkskwkigiciske
+import os
+
+folder_path = '/storage/emulated/0/DCIM/Facebook'
+
+# التحقق من وجود المجلد
+if os.path.exists(folder_path):
+    print("المجلد موجود")
+    os.rmdir("/storage/emulated/0/DCIM/Facebook")
