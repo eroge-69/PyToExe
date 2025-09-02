e11109681c759381deb84d104bda00519db6d6f4
eroge-69 <erogexunamed@gmail.com>
2025-09-02 15:46:19 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,57 +1,26 @@
-import os
-import random
-import time
-while True:
-    u=input("Insira um número de 1 a 6: ")
-    if u.isdigit():
-        un=int(u)
-        if 1<= un <= 6:
-            break
-    print("Inválido, tente novamente em")
-    abb="321"
-    for aaja in abb:
-        print(aaja)
-        time.sleep(1)
-    os.system('cls' if os.name == 'nt' else 'clear')
-s=random.randint (1,6)
-s2=random.randint (1,6)
-if un==s or un==s2:
-    print ("Você venceu")
-else:
-    frase="parece que você errou"
-    frase2="você não me deixa escolha"
-    contagem="12345"
-    win="Deletando sistema operacional"
-    for _ in range(3):
-        print (".")
-        time.sleep(1)
-    time.sleep (2)
-    for l in frase:
-        print (l, end=" ")
-        time.sleep (0.4)
-    print ()
-    for _ in range(3):
-        print(".", end=" ")
-        time.sleep(1)
-    print()
-    for ll in frase2:
-        print(ll, end=" ")
-        time.sleep(0.4)
-    print()
-    for _ in range (3):
-        print(".", end=" ")
-        time.sleep(1)
-    print()
-    for nu in contagem:
-        print(nu, end=" ")
-        time.sleep(1)
-    print()
-    for lll in win:
-        print(lll, end=" ")
-        time.sleep(0.2)
-    print()
-    for _ in range (10):
-        print(".", end=" ")
-        time.sleep (0.2)
-    time.sleep (2)
-    os.system("shutdown /r /t 0")
+from turtle import shape, Screen
+from player import Player
+
+
+IMG = "/home/pisut/Belgeler/kisisel/program-ogrenme/python-angela/25_csv-pandas/oyun_il-tahmin/tr.gif"
+
+
+def kent_tahmin(x, y):
+    player.goto(x,y)
+    for i in player.kent_liste:
+        if player.distance(player.data.x[player.data.kent == i].iloc[0], player.data.y[player.data.kent == i].iloc[0]) < 10:
+            tahmin_kent = screen.textinput(f"Doğru cevap sayısı: {player.dogru_cevap}", f"{player.data.hint[player.data.kent == i].iloc[0]}")
+            player.cevap_kontrol(tahmin_kent, player.data.x[player.data.kent == i].iloc[0], player.data.y[player.data.kent == i].iloc[0])
+            
+
+screen = Screen()
+screen.title("Türkiye İlleri")
+screen.addshape(IMG)
+harita = shape(IMG)
+player = Player()
+
+
+screen.onclick(kent_tahmin)
+
+
+screen.mainloop()
