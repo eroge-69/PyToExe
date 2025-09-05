adb00b440e19e5360825da0f8fe61c096ad952c2
eroge-69 <erogexunamed@gmail.com>
2025-09-05 17:33:51 +0700

    Upload gui.py via PY to EXE tool

--- python-files/gui.py
+++ python-files/gui.py
@@ -1,75 +1,114 @@
-import tkinter as tk
-from openpyxl import load_workbook
-from datetime import datetime
+from tkinter import *
+from tkinter import ttk
+## Creating the student class
 
-file_name = "registery_with_sheets.xlsx"  
-wb = load_workbook(file_name)
+class Student :
 
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
 
-# Create the main window
-root = tk.Tk()
-root.title("Registery")
-root.geometry("400x300")
+    @classmethod
+    def calculateAvg(self, grades) :
+        n = len(grades)
 
-# Function to run when button is clicked
-def search_code():
-    result="test"
-    code = entry.get()
-    if code in wb.sheetnames:
-        ws = wb[code]
+        res = 0
+
+        for i in range(n) :
+            res = res + int(grades[i])
+
+        ans = res / n
+
+        return ans, res, n
     
-    # Get the last row with data
-        last_row = ws.max_row
+students = {}
+std = []
+avg = []
+grading = []
 
-    # Select desired columns
-        selected_columns = [1, 8, 9, 10, 12, 14]
-        values_list = []
+def turn2List(str) :
+    arr = []
+    for x in str.split() :
+        arr.append(int(x))
+    return arr
+
+
+def addNew() :
+        name = StringVar()
+        getName = ttk.Entry(mainframe, textvariable=name)
+        getName.grid(column=1, row=3)
+        grades = StringVar()
+        getGrades = ttk.Entry(mainframe, textvariable=grades )
+        getGrades.grid(column=1, row=4)
+        def set2Student() :
+            s = Student(name.get(), grades.get())
+            nameText.config()
+            std.append(s.name)
+            students[name.get()] = turn2List(grades.get())
+            avg.append(s.calculateAvg(turn2List(grades.get())))
+            grading.append(grades.get())
+            print(name.get(), grades.get(), std, students, avg, grading)
+        nameText = ttk.Label(mainframe, text='Enter the name of the student')
+        nameText.grid(column=2, row=3)
+        gradeText = ttk.Label(mainframe, text='Enter the grades of the student')
+        gradeText.grid(column=2, row=4)
+        button5 = ttk.Button(mainframe, text='Submit', command=set2Student)
+        button5.grid(column=1, row=5)
+
+def showStudents() :
+     for i in range(len(std)) :
+        namesText = ttk.Label(mainframe, text=std[i])
+        namesText.grid(column=4, row=i + 3)
+
+def findAvg() :
+    nameOfAvg = StringVar()
+    nameOfAvgText = ttk.Entry(mainframe, textvariable=nameOfAvg)
+    nameOfAvgText.grid(column=7, row=3)
+    someWords = ttk.Label(mainframe, text='Enter the name of the student')
+    someWords.grid(column=8, row=3)
+    def cal() :
+        for a in range(len(std)) :
+            if nameOfAvg.get() == std[a] :
+                avgLabel = ttk.Label(mainframe, text=avg[a][0])
+                avgLabel.grid(column=7, row=a+5)
+                
+    button6 = ttk.Button(mainframe, text='Calculate Average', command=cal)
+    button6.grid(column=7, row=4)
         
-        
-        for col in selected_columns:
-            cell_value = ws.cell(row=last_row, column=col).value
-        # Apply string truncation only to column 10
-            if col == 10 and isinstance(cell_value, (datetime)):
-                cell_value = cell_value.strftime("%Y-%m-%d")
-            values_list.append(cell_value)
+def save2File() :
+        f = open('grades.txt', 'a')
+        for b in range(len(std)) : 
+            f.write(f'{std[b]} : {grading[b]}')
 
-            
-        
-        
-        for i in range(len(values_list)):
-            # Second column: values
-            label2 = tk.Label(root, text=str(values_list[i]), font=("Arial", 10), anchor="w", width=25)
-            label2.grid(row=i+4, column=2, padx=10, pady=5)
+## program runnning in a loop
 
+root = Tk()
+root.title('Student Managment System')
+ttk.Style().theme_use("clam")
+root.geometry('1700x500')
 
-    else:
-        result = "Code not found!"
-        result_label = tk.Label(root, text=result)
-        result_label.grid(row=4 , column = 1, padx=10,pady=5)
-    
-        
-        
-keys_list=["PART NUM  :","DESCRIPTION  :","PROJECT  :","DATE  :","SIGN  :","GROUP  :"]
+mainframe = ttk.Frame(root, padding='3 3 12 12')
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
+main_Text = ttk.Label(mainframe, text="WELCOME!, Choose one of the following options")
+main_Text.grid(column=5, row=1)
 
-# Create a label
-label = tk.Label(root, text="Enter a 5-digit code:")
-label.grid(row=1 , column = 1, padx=10,pady=5)
+button1 = ttk.Button(mainframe, text='Add a Student', command=addNew)
+button1.grid(column=1, row=2)
 
-# Create a text entry field
-entry = tk.Entry(root)
-entry.grid(row=2 , column = 1, padx=10,pady=5)
+button2 = ttk.Button(mainframe, text='Show All Students', command=showStudents)
+button2.grid(column=4, row=2)
 
-# Create a button
-button = tk.Button(root, text="Search", command=search_code)
-button.grid(row=3 , column = 1, padx=10,pady=5)
+button3 = ttk.Button(mainframe, text='Find the Average Of the Student', command=findAvg)
+button3.grid(column=7, row=2)
 
+button4 = ttk.Button(mainframe, text='Save to a File (grades.txt)', command=save2File)
+button4.grid(column=10, row=2)
 
-
-for i in range(len(keys_list)):
-# First column: labels
-    label1 = tk.Label(root, text=keys_list[i], font=("Arial", 10), anchor="w", width=15)
-    label1.grid(row=i+4, column=1, padx=20, pady=5)
-
-# Run the application
 root.mainloop()
+
+
+        
+
