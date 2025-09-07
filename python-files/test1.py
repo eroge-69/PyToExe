b7771439af050a83830eefd0d4a90314553646d5
eroge-69 <erogexunamed@gmail.com>
2025-09-07 16:23:06 +0700

    Upload test1.py via PY to EXE tool

--- python-files/test1.py
+++ python-files/test1.py
@@ -1,20 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  4 19:14:23 2025
+num1 = int(input("Напиши первое число"))
 
-@author: straj
-"""
-from tkinter import Tk, Canvas
-from PIL import Image, ImageTk
-# Initialize the Tkinter window
-root = Tk()
-# Create a canvas object
-canvas = Canvas(root, width=400, height=400)
-canvas.pack()
-# Open an image using PIL
-image = Image.open('darow.png')
-photo = ImageTk.PhotoImage(image)
-# Draw the image on the canvas
-canvas.create_image(20, 20, anchor='nw', image=photo)
-# Start the Tkinter event loop
-root.mainloop()
+num2 = int(input("Напиши второе число"))
+
+print ("resuld",num1+num2)
+print ("resuld",num1-num2)
+print ("resuld",num1/num2)
+print ("resuld",num1*num2)
