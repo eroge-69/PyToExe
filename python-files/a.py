dd1807a8296ebb47b8639990a6d049f56fc82a51
eroge-69 <erogexunamed@gmail.com>
2025-09-07 17:34:32 +0700

    Upload a.py via PY to EXE tool

--- python-files/a.py
+++ python-files/a.py
@@ -1,26 +1,36 @@
-#!/usr/bin/env python3
-import subprocess
-import os
-import sys
+o = 0
+i = 0
+d = "-"
+print loading...
+import pyautogui
+import turtle
+import random
+turtle.Turtle
+turtle.right(50)
+turtle.forward(10)
+turtle.forward(10)
+turtle.forward(10)
+turtle.circle(20)
+turtle.bgcolor("black")
 
-def main():
-    # Caminho da raiz do jogo (onde está o TheBazaar.exe original)
-    user = os.getlogin()
-    root_game_path = fr"C:\Users\{user}\AppData\Roaming\Tempo Launcher - Beta\game\buildx64\TheBazaar.exe"
 
-    if not os.path.exists(root_game_path):
-        print(f"Erro: não encontrei o TheBazaar.exe em {root_game_path}")
-        sys.exit(1)
 
-    # Pega todos os argumentos passados para o wrapper
-    args = sys.argv[1:]
-
-    # Executa o EXE original passando os argumentos
-    try:
-        subprocess.run([root_game_path, *args], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Erro ao executar o jogo: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+for _ in range(10000000000000):
+    turtle.pencolor("red")
+    turtle.speed(10)
+    turtle.goto(random.randint(-400, 400), random.randint(-400, 400) )
+    asd = random.randint(0, 10)
+    if asd == 10:
+        i += 1
+        print(i)
+        turtle.clear()
+        turtle.pencolor("yellow")
+        turtle.circle(5)
+        turtle.write("1")
+        turtle.goto(0, 0)
+    else:
+        turtle.write(o)
+        o += 1
+        turtle.title(o)
+        print(i)
+        
