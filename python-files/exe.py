3be47aff115321b2e34a5f9eb5d683ebe22f17dc
eroge-69 <erogexunamed@gmail.com>
2025-09-08 08:15:45 +0700

    Upload exe.py via PY to EXE tool

--- python-files/exe.py
+++ python-files/exe.py
@@ -1,22 +1,3 @@
-from cx_Freeze import setup, Executable
-
-# List of included files (if any)
-included_files = []  # Add any data files here like ['data.txt', 'images/']
-
-# Base setup (use "Win32GUI" for GUI applications)
-base = None
-# Uncomment below if you're creating a GUI application
-# base = "Win32GUI"
-
-setup(
-    name="AI bot",
-    version="0.1",
-    description="Your Description",
-    options={
-        "build_exe": {
-            "includes": [],      # Add any special modules needed
-            "include_files": included_files
-        }
-    },
-    executables=[Executable("ai_bot.py", base=base)]
-)
+if __name__ == '__main__':
+    print("hi,python!")
+    
