5032fa737ed2cc89e46d6609fef75e9e7fe96fc9
eroge-69 <erogexunamed@gmail.com>
2025-09-12 04:21:54 +0700

    Upload game.py via PY to EXE tool

--- python-files/game.py
+++ python-files/game.py
@@ -1,220 +1,123 @@
-pip install auto-py-to-exe
-# imports
-from random import *
-from time import sleep
-import os
-# setting all the variable
-Health = 30
-Max_health = 30
-bandages = 5
-Xp = 5
-fight = 0
-Skeleton_health = 15
-Zombie_health = 20
-wins = 0
-skeleton = False
-zombie = False
-# entrance
-print("Welcome to my dungeon, in order to progress you can defeat enemies, once you die the game will end, once you die your achivments will be displayed")
+#ROCK PAPER SCISSORS USING PYTHON WITH OPENCV
 
-while Health > 0: # loop/ death check
-  fight = randint(1,Xp) #who wil you fight (increases the licklyhood of a boss as you go along) 
-  if fight in range (1, 6): #spawns a skeli and sets health based on lvl
-      Skeleton_health = 15
-      Skeleton_health = Skeleton_health + Xp
-      skeleton = True
-  else: #spawns a big skeli (called Zombie becuase i count find any ascii art for a Zombie) and sets health based on lvl
-      Zombie_health = 20
-      Zombie_health = Zombie_health + Xp
-      skeleton = False
-      zombie = True
-  while skeleton == True: #Skeli fight
-      sleep(2)
-      hit_increase = Xp * 1.25
-      Max_health = Xp * 6
-      if Health > Max_health:
-          Health = Max_health
-      os.system('cls')#shows health + bandages with a bossbar of sorts
-      print("Health " + str(Health))
-      print("bandages " + str(bandages))
-      print('█' * Skeleton_health )
-      print(r"""      .-.
-     (o.o)
-      |=|
-     __|__
-   //.=|=.\\
-  // .=|=. \\
-  \\ .=|=. //
-   \\(_=_)//
-    (:| |:)
-     || ||
-     () ()
-     || ||
-     || ||
-l42 ==' '==""")
-      print("1: Fight, 2: Heal") #what action fight or heal
-      action = input("what will you do ")
-      if action == "1": #checking if attack hits
-          hit = randint(1,20)
-          hit = hit + hit_increase
-          if hit >= 10: # rolling damage
-              damage = randint(1,Xp+10)
+#IMPORT PACKAGES
+import random
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+import time
 
-              Skeleton_health = Skeleton_health - damage
-              print(f"the attack hits dealing {damage} damage")
-          elif hit  <= 10:
-              print("the attack misses dealing no damage")
-      elif action == "2":
-        if bandages > 0:
-                bandages = bandages - 1
-                heal = randint(Xp, Xp+15)
-                Health = Health + heal
-                print(f"you heal for {heal} health")
-                if Health > Max_health: #to stop player from healing more than their health
-                  print(f"you heal to full health")
-                else:
-                  print(f"you are now at {Health} health")  
+#CAPTURE VIDEO USING OPENCV-->CV2
+
+cap=cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+detector=HandDetector(maxHands=1)   #Only one player can play with AI
+
+# Load the background image once
+imgBgOriginal = cv2.imread("Resources/BG.png")
+
+# Game State Variables
+stateResult=False
+startGame=False
+score=[0,0]  #Starting score will be 0
+result_display_end_time = 0 # Timer to reset the game after a round
+quit_gesture_start_time = 0 # Timer for the quit gesture
+
+
+while True:
+    # Create a fresh copy of the background for each frame
+    imgbg = imgBgOriginal.copy()
+    success,img=cap.read()
+
+    if not success:
+        print("Failed to capture image")
+        break
+
+    imgscale=cv2.resize(img,(0,0),None,0.875,0.875)
+    imgscale=imgscale[:,80:480]
+
+    #Find Hands
+    hands,img=detector.findHands(imgscale)
+
+    # Universal Quit Gesture with 2-second hold
+    if hands:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        if fingers == [0, 1, 1, 1, 0]: # User-defined Quit Gesture
+            if quit_gesture_start_time == 0:
+                quit_gesture_start_time = time.time() # Start timer
+            elif time.time() - quit_gesture_start_time > 2:
+                break # Held for 2s, so quit
         else:
-            print("no more bandages")
-      else: # to stop miss inputs
-        print("that won't work")
-      if Skeleton_health > 0:
-          print("THe Skeleton attacks")
-          Bad_attack = randint(1,20)
-          if Bad_attack > 10:
-               Bad_damage = randint(1,Xp*2)
-               Health = Health - Bad_damage
-               print(f"The Skeleton hits you dealing {Bad_damage} damage")
-          if Health <= 0:
-              skeleton = False
-          
-      else: #kills the skeli
-          print("the skeleton dies")
-          skeleton = False
-          gifts = randint(0,Xp)
-          gifts  = gifts + 2
-          bandages = bandages + gifts
-          print(f"you defeat the skeleton and recive {gifts} more bandages")
-          print("prepare for your next battle")
-          Health = Health + 5
-          wins = wins + 1
-          Xp = Xp + 1
-          sleep(0.5)
-
-
-
-
-
-  while zombie == True: #mostly the same as small skeli just with different numbers and art, gives more xp and bandages in return for a more difficult fight
-      sleep(2)
-      hit_increase = Xp * 1.25
-      Max_health = Xp * 6
-      if Health > Max_health:
-          Health = Max_health
-      os.system('cls')
-      print("Health " + str(Health))
-      print("bandages " + str(bandages))
-      print('█' * Zombie_health )
-      print(r"""
-                              _.--""-._
-  .                         ."         ".
- / \    ,^.         /(     Y             |      )\
-/   `---. |--'\    (  \__..'--   -   -- -'""-.-'  )
-|        :|    `>   '.     l_..-------.._l      .'
-|      __l;__ .'      "-.__.||_.-'v'-._||`"----"
- \  .-' | |  `              l._       _.'
-  \/    | |                   l`^^'^^'j
-        | |                _   \_____/     _
-        j |               l `--__)-'(__.--' |
-        | |               | /`---``-----'"1 |  ,-----.
-        | |               )/  `--' '---'   \'-'  ___  `-.
-        | |              //  `-'  '`----'  /  ,-'   I`.  \
-      _ L |_            //  `-.-.'`-----' /  /  |   |  `. \
-     '._' / \         _/(   `/   )- ---' ;  /__.J   L.__.\ :
-      `._;/7(-.......'  /        ) (     |  |            | |
-      `._;l _'--------_/        )-'/     :  |___.    _._./ ;
-        | |                 .__ )-'\  __  \  \  I   1   / /
-        `-'                /   `-\-(-'   \ \  `.|   | ,' /
-                           \__  `-'    __/  `-. `---'',-'
-                              )-._.-- (        `-----'
-                             )(  l\ o ('..-.
-                       _..--' _'-' '--'.-. |
-                __,,-'' _,,-''            \ \
-               f'. _,,-'                   \ \
-              ()--  |                       \ \
-                \.  |                       /  \
-                  \ \                      |._  |
-                   \ \                     |  ()|
-                    \ \                     \  /
-                     ) `-.                   | |
-                    // .__)                  | |
-                 _.//7'                      | |
-               '---'                         j_| `
-                                            (| |
-                                             |  \
-                                             |lllj
-                                             ||||| """)
-      print("1: Fight, 2: Heal")
-      action = input("what will you do ")
-      if action == "1":
-          hit = randint(4,20)
-          hit = hit + hit_increase
-          if hit >= 12: # note the higher ac 
-              damage = randint(1,Xp+10)
-              Zombie_health = Zombie_health - damage
-              print(f"the attack hits dealing {damage} damage")
-              print("")
-          elif hit  <= 10:
-              print("the attack misses dealing no damage")
-              print("")
-      elif action == "2":
-        if bandages > 0:
-                bandages = bandages - 1
-                heal = randint(Xp, Xp+15)
-                Health = Health + heal
-                print(f"you heal for {heal} health")
-                if Health > Max_health:
-                  print(f"you heal to full health")
-                else:
-                  print(f"you are now at {Health} health") 
-        else:
-            print("no more bandages")
-      else:
-        print("that won't work")
-      if Zombie_health > 0:
-          print("THe big skeleton attacks")
-          Bad_attack = randint(Xp,15)
-          if Bad_attack >= 10:
-               Bad_damage = randint(1,Xp*3)
-               Health = Health - Bad_damage
-               print(f"The big skeleton hits you dealing {Bad_damage} damage")
-          if Health <= 0:
-              zombie = False
-          
-      else:
-          print("the big skeleton dies")
-          zombie = False
-          gifts = randint(1,Xp)
-          gifts  = gifts + 4
-          bandages = bandages + gifts
-          print(f"you defeat the big skeleton and recive {gifts} more bandages")
-          print("prepare for your next battle")
-          Xp = Xp + 2
-          wins = wins + 1
-          Health = Health + 10
-          sleep(0.5)
-else: #lose screen, shows what you did and how much bandages left
-    sleep(1)
-    os.system('cls')
-    print(f"you died after killing {wins} enemies")
-    print(f"you were level {Xp-5}")
-    print(f"you had {bandages} bandages left")
-    if Xp >= 5:
-        print("That was not bad")
-    elif Xp <= 2:
-        print("that was terrible")
-    elif Xp >= 10:
-        print("very well done")
+            quit_gesture_start_time = 0 # Reset timer if gesture changes
     else:
-        print("i don't have a response to your skill")
-    print("please play again")
+        quit_gesture_start_time = 0 # Also reset if no hand is visible
+
+    if startGame:
+        if stateResult:
+            # State: Showing Result
+            if time.time() > result_display_end_time:
+                # Reset for the next round
+                startGame = False
+                stateResult = False
+            else:
+                cv2.putText(imgbg, "Round Over", (450, 500), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+        else:
+            # State: Playing
+            timer=time.time()-initialTime
+            # Display a 3, 2, 1 countdown
+            cv2.putText(imgbg,str(3 - int(timer)),(605,435),cv2.FONT_HERSHEY_PLAIN,6,(255,0,255),4)
+            
+            if timer>3:
+                stateResult=True
+                result_display_end_time = time.time() + 3
+                playerMove=0 #Default player move
+
+                # AI always makes a move
+                randomNumber=random.randint(1,3)
+                imgAi=cv2.imread(f'Resources/{randomNumber}.png',cv2.IMREAD_UNCHANGED)
+
+                if hands:
+                    if fingers==[0,0,0,0,0]: playerMove=1 #ROCK
+                    if fingers ==[1,1,1,1,1]: playerMove=2   #PAPER
+                    if fingers ==[0,1,1,0,0]: playerMove=3  #SCISSORS
+
+                #GAME RESULT
+                if (playerMove==1 and randomNumber==3) or (playerMove==2 and randomNumber==1) or (playerMove==3 and randomNumber==2): score[1]+=1
+                if (playerMove==3 and randomNumber==1) or (playerMove==1 and randomNumber==2) or (playerMove==2 and randomNumber==3): score[0]+=1
+    else:
+        # State: Waiting to Start
+        if hands:
+            if fingers == [0, 0, 1, 1, 1]:
+                startGame = True
+                initialTime = time.time()
+                stateResult = False # Explicitly reset state
+
+
+    imgbg[234:654,795:1195]=imgscale
+
+    if stateResult:
+        imgbg=cvzone.overlayPNG(imgbg,imgAi,(149,310))
+
+    cv2.putText(imgbg,str(score[0]),(410,215),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),6)
+    cv2.putText(imgbg,str(score[1]),(1112,215),cv2.FONT_HERSHEY_PLAIN,4,(255,255,255),6)
+
+    cv2.imshow("BG",imgbg)
+
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+#DECLARE THE RESULT
+
+if score[0]>score[1]:
+    print("AI WINS")
+elif score[1]>score[0]:
+    print("PLAYER WINS")
+else:
+    print("TIE")
