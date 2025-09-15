f5ccf3080c97be62bea08cae5b35332f839928ef
eroge-69 <erogexunamed@gmail.com>
2025-09-15 21:33:03 +0700

    Upload students_rank.py via PY to EXE tool

--- python-files/students_rank.py
+++ python-files/students_rank.py
@@ -1,6 +1,7 @@
-students={}
-n=int(input("Enter number of students:"))
-for i in range(n):
+def main():
+ students={}
+ n=int(input("Enter number of students:"))
+ for i in range(n):
     name=input(f"Enter name of student {i+1}:")
     marks=int(input(f"Enter marks of {name}:"))
     students[name]=marks
@@ -16,15 +17,17 @@ for i in range(n):
             grade="D"
         print(f"{name}'s grade={grade}")
 
-sorted_students=sorted(students.items(),key=lambda items:items[1],reverse=True)
+ sorted_students=sorted(students.items(),key=lambda items:items[1],reverse=True)
 
-print("\n---Students Ranks---")
-rank=1
-previous_marks=None
+ print("\n---Students Ranks---")
+ rank=1
+ previous_marks=None
 
-for i,(name,marks) in enumerate(sorted_students,start=1):
+ for i,(name,marks) in enumerate(sorted_students,start=1):
     if marks!=previous_marks:
         rank=i
 
     print(f"Rank {i}: {name} with {marks} marks")
     previous_marks=marks
+if __name__=="__main__": 
+ main()
