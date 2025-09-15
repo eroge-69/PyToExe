06df65e9c453d8e566a734aa8238126b69270646
eroge-69 <erogexunamed@gmail.com>
2025-09-15 23:19:46 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -1,6 +1,37 @@
-# Выводим приветственное сообщение, чтобы понять, что инъекция удалась
-print(">>> Hello from Python Payload! Injection successful! <<<")
-print("-" * 50)
+{% extends "base.html" %}
+{% block content %}
+<h2>{{rtype | capitalize}} Report</h2>
 
-# Ждем, пока пользователь нажмет Enter, чтобы окно не закрылось сразу
-input("Press Enter to exit...")
+{% if rtype == "inventory" %}
+<table class="table table-bordered">
+  <tr><th>SKU</th><th>Qty</th><th>Value</th></tr>
+  {% for r in data %}
+  <tr><td>{{r.sku}}</td><td>{{r.qty}}</td><td>{{r.value}}</td></tr>
+  {% endfor %}
+</table>
+
+{% elif rtype == "sales" %}
+<table class="table table-bordered">
+  <tr><th>SKU</th><th>Qty Sold</th><th>Revenue</th></tr>
+  {% for r in data %}
+  <tr><td>{{r.sku}}</td><td>{{r.qty}}</td><td>{{r.revenue}}</td></tr>
+  {% endfor %}
+</table>
+
+{% elif rtype == "expenses" %}
+<table class="table table-bordered">
+  <tr><th>Date</th><th>Amount</th><th>Description</th></tr>
+  {% for e in data %}
+  <tr><td>{{e.date}}</td><td>{{e.amount}}</td><td>{{e.description}}</td></tr>
+  {% endfor %}
+</table>
+
+{% elif rtype == "pl" %}
+<ul>
+  <li>Revenue: {{data.revenue}}</li>
+  <li>COGS: {{data.cogs}}</li>
+  <li>Expenses: {{data.expenses}}</li>
+  <li><b>Net Profit: {{data.net}}</b></li>
+</ul>
+{% endif %}
+{% endblock %}
