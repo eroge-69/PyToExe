f8014ae1de4e1472a0c18b5b007d42a3c5d642bd
eroge-69 <erogexunamed@gmail.com>
2025-09-16 10:23:06 +0700

    Upload 1.py via PY to EXE tool

--- python-files/1.py
+++ python-files/1.py
@@ -1,12 +1,14 @@
-import numpy as np
-import sounddevice as sd
-
-rate = 8000  # частота дискретизации
-t = np.arange(0, rate * 1000)  # 10 секунд
-
-# Формула bytebeat
-samples = ((t * 5 & t >> 7) | (t * 3 & t >> 10)) % 256
-samples = (samples.astype(np.float32) - 128) / 128  # нормализация в [-1,1]
-
-sd.play(samples, rate)
-sd.wait()
+from tkinter import *  
+from tkinter import messagebox  
+  
+  
+def clicked():  
+    messagebox.showinfo('Заголовок', 'и зачем?')  
+  
+  
+window = Tk()  
+window.title("Добро пожаловать в приложение PythonRu")  
+window.geometry('10x30')  
+btn = Button(window, text='Клик', command=clicked)  
+btn.grid(column=0, row=0)  
+window.mainloop()
