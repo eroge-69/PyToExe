cea3454a2d4fb85cbf135bc8fb842c3c6ebb85dd
eroge-69 <erogexunamed@gmail.com>
2025-09-16 11:04:52 +0700

    Upload 1.py via PY to EXE tool

--- python-files/1.py
+++ python-files/1.py
@@ -1,14 +1,23 @@
 from tkinter import *  
-from tkinter import messagebox  
-  
+from tkinter import messagebox
+import time as t
   
 def clicked():  
-    messagebox.showinfo('Заголовок', 'и зачем?')  
+    messagebox.showinfo('не жми куда попало', 'и зачем?')
+    messagebox.showinfo('и вот что ты будешь делать?','и вот что ты будешь делать?')
+    messagebox.showinfo('было только 1 задание', 'было только 1 задание')
+    messagebox.showinfo('и ты с ним не справился','и ты с ним не справился')
+    messagebox.showinfo('с 1 заданием', 'с 1 заданием')
+    messagebox.showinfo('тебе сказали не нажимать на что попало','тебе сказали не нажимать на что попало')
+    messagebox.showinfo( 'а ты что сделал?', 'а ты что сделал?')
+    messagebox.showinfo( 'а уже поздно', 'а уже поздно')
+    t.sleep(20)
+
   
-  
-window = Tk()  
+window = Tk()
+window.title("не нажимай")
 window.title("Добро пожаловать в приложение PythonRu")  
-window.geometry('10x30')  
+window.geometry('1900x1050')  
 btn = Button(window, text='Клик', command=clicked)  
-btn.grid(column=0, row=0)  
+btn.grid(column=10, row=10)  
 window.mainloop()
