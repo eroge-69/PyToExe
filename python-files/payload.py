b03f2638f982dae93fa8fc05e2ec741a7b7c1b8f
eroge-69 <erogexunamed@gmail.com>
2025-09-20 04:51:36 +0700

    Upload payload.py via PY to EXE tool

--- python-files/payload.py
+++ python-files/payload.py
@@ -1,11 +1,2 @@
-# payload.py
-import tkinter as tk
-from tkinter import messagebox
-
-def run_malicious_code():
-    messagebox.showinfo("lmao got you", "get dunked on loser")
-    # Add more malicious actions here (e.g., data theft, ransomware, etc.)
-
-root = tk.Tk()
-root.withdraw()  # Hide the window
-run_malicious_code()
+import os
+os.system("calc")
