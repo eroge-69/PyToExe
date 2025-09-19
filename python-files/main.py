0483a919f01eb26bd69ced3037363666d24bd2b9
eroge-69 <erogexunamed@gmail.com>
2025-09-19 23:03:59 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,39 +1,46 @@
 import os
-from yt_dlp import YoutubeDL
+import sys
+# DON'T CHANGE THIS !!!
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-# Ask for YouTube link
-url = input("Enter the YouTube link: ")
+from flask import Flask, send_from_directory
+from src.models.user import db
+from src.routes.user import user_bp
+from src.routes.dashboard import dashboard_bp
+from flask_cors import CORS
 
-# Ask for format
-format_choice = input("Enter the format (mp4/mp3): ").lower()
+app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
+app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 
-# Downloads folder
-downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+# Habilita CORS para permitir requisições do frontend
+CORS(app)
 
-# Set yt-dlp options
-if format_choice == "mp4":
-    ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': os.path.join(downloads_folder, '%(title)s.%(ext)s'),
-    }
-elif format_choice == "mp3":
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': os.path.join(downloads_folder, '%(title)s.%(ext)s'),
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-else:
-    print("Invalid format. Please enter 'mp4' or 'mp3'.")
-    exit()
+app.register_blueprint(user_bp, url_prefix='/api')
+app.register_blueprint(dashboard_bp, url_prefix='/api/dashboard')
 
-# Download the video/audio
-try:
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    print(f"Download completed! Saved to {downloads_folder}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# uncomment if you need to use database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    static_folder_path = app.static_folder
+    if static_folder_path is None:
+            return "Static folder not configured", 404
+
+    if path != "" and os.path.exists(os.path.join(static_folder_path, path)):
+        return send_from_directory(static_folder_path, path)
+    else:
+        index_path = os.path.join(static_folder_path, 'index.html')
+        if os.path.exists(index_path):
+            return send_from_directory(static_folder_path, 'index.html')
+        else:
+            return "index.html not found", 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
