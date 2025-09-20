71c679a722ee917012bcbd86036adf59eda064d6
eroge-69 <erogexunamed@gmail.com>
2025-09-20 22:28:10 +0700

    Upload nj.py via PY to EXE tool

---
+++ python-files/nj.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+# then make a url variable
+url = "https://newjerusalemonline.org/completeattendance/complete/"
+
+# then call the default open method described above
+webbrowser.open(url)
+
