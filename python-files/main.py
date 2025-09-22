e21258fd2f902357a1cf216b47b814f2574af5f9
eroge-69 <erogexunamed@gmail.com>
2025-09-23 00:48:47 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,98 +1,65 @@
-import random
-import math
-
-
-#displays the main menu
-def disp_menu():
-    print(" ")
-    print("Main Menu")
-    print("1. Start")
-    print("2. Exit")
-    option = input("pick an option: ")
-    if option == "1":
-        print("starting...\n")
-        start_game()
-    elif option == "2":
-        print("exiting...")
-        exit()
-    else:
-        print("invalid option, try again\n")
-        run_menu()
-
-#opens the difficulty selector
-def start_game():
-    mode()
-
-#runs the easier mode
-def easy_mode():
-    num = random.randint(1, 10)
-    choice = int(input("Pick a random number from 1-10: \n"))
-    if choice == num:
-        print("You got it right\n")
-        run_menu()
-    elif 11 > choice > 0:#if the user's choice was incorrect, this checks if it is in between the numbers specified. if so it tells the user they were wrong.
-        print("You picked wrong\n")
-        run_menu()
-    else:
-        print("invalid number, try again\n")
-        easy_mode()
-
-#runs the normal difficulty
-def normal_mode():
-    num = random.randint(1, 100)
-    choice = int(input("Pick a random number from 1-100: \n"))
-    if choice == num:
-        print("You got it right\n")
-        run_menu()
-    elif 101 > choice > 0:
-        print("You picked wrong\n")
-        run_menu()
-    else:
-        print("invalid number, try again\n")
-        normal_mode()
-
-#runs the harder difficulty
-def hard_mode():
-    num = random.randint(1, 1000)
-    choice = int(input("Pick a random number from 1-1000: \n"))
-    if choice == num:
-        print("You got it right\n")
-        run_menu()
-    elif 1001 > choice > 0:
-        print("You picked wrong\n")
-        run_menu()
-    else:
-        print("invalid number, try again\n")
-        hard_mode()
-
-#opens the difficulty selector
-def mode():
-    print("What difficulty do you want to play? \n")
-    print("1. Easy")
-    print("2. Normal")
-    print("3. hard")
-    print("4. go back")
-    diff = input("pick one: \n")
-    if diff == "1":
-        easy_mode()
-    elif diff == "2":
-        normal_mode()
-    elif diff == "3":
-        hard_mode()
-    elif diff == "4":
-        run_menu()
-    else:
-        print("Invalid option, try again")
-        mode()
-
-
-
-#runs the main menu
-def run_menu():
-    disp_menu()
-
-#runs the main menu when the program is opened
-if __name__ == "__main__":
-    run_menu()
-
-
+import pygame as pg
+import sys
+from settings import *
+from map import *
+from player import *
+from raycasting import *
+from object_renderer import *
+from sprite_object import *
+from object_handler import *
+from weapon import *
+from sound import *
+from pathfinding import *
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.mouse.set_visible(False)
+        self.screen = pg.display.set_mode(RES)
+        pg.event.set_grab(True)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
+        self.new_game()
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
+        self.raycasting = RayCasting(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
+        pg.mixer.music.play(-1)
+    def update(self):
+        self.player.update()
+        self.raycasting.update()
+        self.object_handler.update()
+        self.weapon.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+    def draw(self):
+        # self.screen.fill('black')
+        self.object_renderer.draw()
+        self.weapon.draw()
+        # self.map.draw()
+        # self.player.draw()
+    def check_events(self):
+        self.global_trigger = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
+            self.player.single_fire_event(event)
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
