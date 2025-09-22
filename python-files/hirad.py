ed6014152156630c8fdf90a1d012f445b5e1e860
eroge-69 <erogexunamed@gmail.com>
2025-09-22 21:25:55 +0700

    Upload hirad.py via PY to EXE tool

---
+++ python-files/hirad.py
@@ -0,0 +1,6 @@
+us = input('username : ')
+pas = input('password : ')
+if (us == 'admin' or us =='Admin') and pas == 'admin*1':
+    print('welcome admin')
+else:
+    print('invalid username or password !')    
