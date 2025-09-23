425ea6084d46cc350164d8f34c76d5c6d720bab2
eroge-69 <erogexunamed@gmail.com>
2025-09-23 20:09:13 +0700

    Upload setup.py via PY to EXE tool

--- python-files/setup.py
+++ python-files/setup.py
@@ -1,142 +1,108 @@
-#!/usr/bin/env python
-"""
-This module tries to install all the required software.
-"""
+from setuptools import setup
 
-from __future__ import print_function
-import sys
-import os
-from ctypes.util import find_library
-from setuptools import setup, find_packages, Command
-import wifiphisher.common.constants as constants
+dependencies = [
+    "aiter==0.13.20191203",  # Used for async generator tools
+    "blspy==1.0",  # Signature library
+    "chiavdf==1.0.1",  # timelord and vdf verification
+    "chiabip158==1.0",  # bip158-style wallet filters
+    "chiapos==0.9",  # proof of space
+    "clvm==0.9.4",
+    "clvm_rs==0.1.4",
+    "clvm_tools==0.4.3",
+    "aiohttp==3.7.4",  # HTTP server for full node rpc
+    "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
+    "bitstring==3.1.7",  # Binary data management library
+    "colorlog==4.7.2",  # Adds color to logs
+    "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
+    "cryptography==3.4.6",  # Python cryptography library for TLS - keyring conflict
+    "keyring==23.0",  # Store keys in MacOS Keychain, Windows Credential Locker
+    "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
+    #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
+    #  See https://github.com/frispete/keyrings.cryptfile/issues/15
+    "PyYAML==5.4.1",  # Used for config file format
+    "setproctitle==1.2.2",  # Gives the chia processes readable names
+    "sortedcontainers==2.3.0",  # For maintaining sorted mempools
+    "websockets==8.1.0",  # For use in wallet RPC and electron UI
+    "click@https://github.com/Chia-Network/click/tarball/master#egg=package-1.0",  # For the CLI
+]
+
+upnp_dependencies = [
+    "miniupnpc==2.1",  # Allows users to open ports on their router
+]
+dev_dependencies = [
+    "pytest",
+    "pytest-asyncio",
+    "flake8",
+    "mypy",
+    "black",
+]
+
+kwargs = dict(
+    name="chia-blockchain",
+    author="Mariano Sorgente",
+    author_email="mariano@chia.net",
+    description="Chia blockchain full node, farmer, timelord, and wallet.",
+    url="https://chia.net/",
+    license="Apache License",
+    python_requires=">=3.7, <4",
+    keywords="chia blockchain node",
+    install_requires=dependencies,
+    setup_requires=["setuptools_scm"],
+    extras_require=dict(
+        uvloop=["uvloop"],
+        dev=dev_dependencies,
+        upnp=upnp_dependencies,
+    ),
+    packages=[
+        "build_scripts",
+        "src",
+        "src.cmds",
+        "src.consensus",
+        "src.daemon",
+        "src.full_node",
+        "src.timelord",
+        "src.farmer",
+        "src.harvester",
+        "src.introducer",
+        "src.protocols",
+        "src.rpc",
+        "src.server",
+        "src.simulator",
+        "src.types",
+        "src.util",
+        "src.wallet",
+        "src.wallet.puzzles",
+        "src.wallet.rl_wallet",
+        "src.wallet.cc_wallet",
+        "src.wallet.util",
+        "src.wallet.trading",
+        "src.ssl",
+        "mozilla-ca",
+    ],
+    entry_points={
+        "console_scripts": [
+            "chia = src.cmds.chia:main",
+            "chia_wallet = src.server.start_wallet:main",
+            "chia_full_node = src.server.start_full_node:main",
+            "chia_harvester = src.server.start_harvester:main",
+            "chia_farmer = src.server.start_farmer:main",
+            "chia_introducer = src.server.start_introducer:main",
+            "chia_timelord = src.server.start_timelord:main",
+            "chia_timelord_launcher = src.timelord.timelord_launcher:main",
+            "chia_full_node_simulator = src.simulator.start_simulator:main",
+        ]
+    },
+    package_data={
+        "src.util": ["initial-*.yaml", "english.txt"],
+        "src.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
+        "mozilla-ca": ["cacert.pem"],
+    },
+    use_scm_version={"fallback_version": "unknown-no-.git-directory"},
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+)
 
 
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
-
-def get_dnsmasq():
-    """
-    Try to install dnsmasq on host machine if not present
-
-    :return: None
-    :rtype: None
-    """
-
-    if not os.path.isfile("/usr/sbin/dnsmasq"):
-        install = raw_input(("[" + constants.T + "*" + constants.W + "] dnsmasq not found " +
-                             "in /usr/sbin/dnsmasq, " + "install now? [y/n] "))
-
-        if install == "y":
-            if os.path.isfile("/usr/bin/pacman"):
-                os.system("pacman -S dnsmasq")
-            elif os.path.isfile("/usr/bin/yum"):
-                os.system("yum install dnsmasq")
-            else:
-                os.system("apt-get -y install dnsmasq")
-        else:
-            sys.exit(("[" + constants.R + "-" + constants.W + "] dnsmasq " +
-                      "not found in /usr/sbin/dnsmasq"))
-
-    if not os.path.isfile("/usr/sbin/dnsmasq"):
-        dnsmasq_message = ("\n[" + constants.R + "-" + constants.W +
-                           "] Unable to install the \'dnsmasq\' package!\n" + "[" + constants.T +
-                           "*" + constants.W + "] This process requires a persistent internet " +
-                           "connection!\nPlease follow the link below to configure your " +
-                           "sources.list\n" + constants.B + "http://docs.kali.org/general-use/" +
-                           "kali-linux-sources-list-repositories\n" + constants.W + "[" +
-                           constants.G + "+" + constants.W + "] Run apt-get update for changes " +
-                           "to take effect.\n" + "[" + constants.G + "+" + constants.W + "] " +
-                           "Rerun the script to install dnsmasq.\n[" + constants.R + "!" +
-                           constants.W + "] Closing")
-
-        sys.exit(dnsmasq_message)
-
-
-def get_hostapd():
-    """
-    Try to install hostapd on host system if not present
-
-    :return: None
-    :rtype: None
-    """
-
-    if not os.path.isfile("/usr/sbin/hostapd"):
-        install = raw_input(("[" + constants.T + "*" + constants.W + "] hostapd not found in " +
-                             "/usr/sbin/hostapd, install now? [y/n] "))
-
-        if install == "y":
-            if os.path.isfile("/usr/bin/pacman"):
-                os.system("pacman -S hostapd")
-            elif os.path.isfile("/usr/bin/yum"):
-                os.system("yum install hostapd")
-            else:
-                os.system("apt-get -y install hostapd")
-        else:
-            sys.exit(("[" + constants.R + "-" + constants.W + "] hostapd not found in " +
-                      "/usr/sbin/hostapd"))
-
-    if not os.path.isfile("/usr/sbin/hostapd"):
-        hostapd_message = ("\n[" + constants.R + "-" + constants.W + "] Unable to install the " +
-                           "\'hostapd\' package!\n[" + constants.T + "*" + constants.W + "] " +
-                           "This process requires a persistent internet connection!\nPlease " +
-                           "follow the link below to configure your sources.list\n" + constants.B +
-                           "http://docs.kali.org/general-use/kali-linux-sources-list-" +
-                           "repositories\n" + constants.W + "[" + constants.G + "+" + constants.W +
-                           "] Run apt-get update for changes to take effect.\n[" + constants.G +
-                           "+" + constants.W + "] Rerun the script to install hostapd.\n[" +
-                           constants.R + "!" + constants.W + "] Closing")
-
-        sys.exit(hostapd_message)
-
-
-# setup settings
-NAME = "wifiphisher"
-AUTHOR = "sophron"
-AUTHOR_EMAIL = "sophron@latthi.com"
-URL = "https://github.com/wifiphisher/wifiphisher"
-DESCRIPTION = "Automated phishing attacks against Wi-Fi networks"
-LICENSE = "GPL"
-KEYWORDS = ["wifiphisher", "evil", "twin", "phishing"]
-PACKAGES = find_packages(exclude=["docs", "tests"])
-INCLUDE_PACKAGE_DATA = True
-VERSION = "1.4"
-CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
-               "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-               "Natural Language :: English", "Operating System :: Unix",
-               "Programming Language :: Python :: 2", "Programming Language :: Python :: 2.7",
-               "Programming Language :: Python :: 2 :: Only", "Topic :: Security",
-               "Topic :: System :: Networking", "Intended Audience :: End Users/Desktop",
-               "Intended Audience :: System Administrators",
-               "Intended Audience :: Information Technology"]
-ENTRY_POINTS = {"console_scripts": ["wifiphisher = wifiphisher.pywifiphisher:run"]}
-INSTALL_REQUIRES = ["PyRIC", "tornado", "dbus-python",
-                    "pbkdf2", "roguehostapd", "scapy"]
-CMDCLASS = {"clean": CleanCommand,}
-
-# run setup
-setup(name=NAME, author=AUTHOR, author_email=AUTHOR_EMAIL, description=DESCRIPTION,
-      license=LICENSE, keywords=KEYWORDS, packages=PACKAGES,
-      include_package_data=INCLUDE_PACKAGE_DATA, version=VERSION, entry_points=ENTRY_POINTS,
-      install_requires=INSTALL_REQUIRES, classifiers=CLASSIFIERS, url=URL, cmdclass=CMDCLASS)
-
-# Get hostapd or dnsmasq if needed
-get_hostapd()
-get_dnsmasq()
-
-print()
-print("                     _  __ _       _     _     _               ")
-print("                    (_)/ _(_)     | |   (_)   | |              ")
-print("  ((.))    __      ___| |_ _ _ __ | |__  _ ___| |__   ___ _ __ ")
-print(r"    |      \ \ /\ / / |  _| | '_ \| '_ \| / __| '_ \ / _ \ '__|")
-print(r"   /_\      \ V  V /| | | | | |_) | | | | \__ \ | | |  __/ |   ")
-print(r"  /___\      \_/\_/ |_|_| |_| .__/|_| |_|_|___/_| |_|\___|_|   ")
-print(r" /     \                    | |                                ")
-print("                            |_|                                ")
-print("                                                               ")
+if __name__ == "__main__":
+    setup(**kwargs)
