855438feb063227df723d63b9c0e402831bcf6e5
eroge-69 <erogexunamed@gmail.com>
2025-09-23 17:55:41 +0700

    Upload rphost.py via PY to EXE tool

--- python-files/rphost.py
+++ python-files/rphost.py
@@ -1,12 +1,14 @@
 import time
+import os
 import sys
 
 def main():
+    # Бесконечный цикл с минимальной нагрузкой
     try:
         while True:
             time.sleep(3600)  # Спит 1 час
     except KeyboardInterrupt:
-        pass
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
