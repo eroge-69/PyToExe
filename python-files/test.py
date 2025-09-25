88e7abb08688f72bbf3eef08b4027cde2f3302ce
eroge-69 <erogexunamed@gmail.com>
2025-09-25 19:01:49 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -1,1 +1,10 @@
-print ('yippie! it works!')
+import time
+user_input = input('Дрочишь? (да/нет): ')
+
+if user_input.lower() == 'да':
+    print('Не отвлекайся')
+#elif user_input.lower() == 'no':
+#    print('Не пизди')
+else:
+    print('Не пизди')
+time.sleep(10)
