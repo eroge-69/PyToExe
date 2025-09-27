4e76b5e573e11cdeb95fed19ff553199937e3ea1
eroge-69 <erogexunamed@gmail.com>
2025-09-27 23:43:48 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,35 +1,55 @@
 import tkinter as tk
-import random
+from tkinter import filedialog, messagebox
 
-def move_no_button(event):
-    """Телепортируем кнопку 'Нет' в случайное место окна"""
-    new_x = random.randint(0, root.winfo_width() - no_button.winfo_width())
-    new_y = random.randint(50, root.winfo_height() - no_button.winfo_height())
-    no_button.place(x=new_x, y=new_y)
+from rembg import remove
+from PIL import Image, ImageTk
 
-def yes_answer():
-    """Меняем текст и убираем кнопки"""
-    question_label.config(text="Я так и знал 😏")
-    yes_button.place_forget()
-    no_button.place_forget()
+def remove_bg():
 
-# --- GUI ---
+    input_path = filedialog.askopenfilename(
+        title="Select Image",
+        filetypes=[("PNG, JPG, JPEG", "*.png;*.jpg;*.jpeg")]
+    )
+    if not input_path:
+        return
+
+    img = Image.open(input_path).convert("RGBA")
+
+    out_img = remove(img)
+
+    save_path = filedialog.asksaveasfilename(
+        title="Save Image As",
+        defaultextension=".png",
+        filetypes=[("PNG files", "*.png")]
+    )
+    if save_path:
+        out_img.save(save_path)
+        messagebox.showinfo("Saved", f"Image saved to {save_path}")
+
+
 root = tk.Tk()
-root.title("Вопросик")
-root.geometry("800x500")  # увеличенное окно
+root.title("EmadBG Remover")
+root.iconbitmap("icon.ico")
+root.geometry("400x500")
 root.resizable(False, False)
 
-question_label = tk.Label(root, text="Сосал?", font=("Arial", 20))
-question_label.pack(pady=40)
+img_logo = Image.open("logo.png").resize((400, 250)) 
+logo = ImageTk.PhotoImage(img_logo)
+label_logo = tk.Label(root, image=logo)
+label_logo.image = logo
+label_logo.pack(pady=10)
 
-# кнопки
-yes_button = tk.Button(root, text="Да", font=("Arial", 14), bg="green", fg="white", command=yes_answer)
-yes_button.place(x=300, y=200)
+btn_style = {
+    "bg": "#78b5ff",
+    "fg": "white",
+    "font": ("Arial", 12, "bold"),
+    "width": 20,
+    "height": 2,
+    "relief": "raised",
+    "bd": 4
+}
 
-no_button = tk.Button(root, text="Нет", font=("Arial", 14), bg="red", fg="white")
-no_button.place(x=400, y=200)
-
-# обработчик наведения
-no_button.bind("<Enter>", move_no_button)
+tk.Button(root, text="Remove Background", command=remove_bg, **btn_style).pack(pady=20)
+tk.Button(root, text="Exit", command=root.destroy, **btn_style).pack(pady=20)
 
 root.mainloop()
