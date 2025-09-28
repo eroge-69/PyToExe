03773fc35329091236312e07033e5ac028c5d668
eroge-69 <erogexunamed@gmail.com>
2025-09-28 18:30:25 +0700

    Upload Untitled-1.py via PY to EXE tool

--- python-files/Untitled-1.py
+++ python-files/Untitled-1.py
@@ -1,44 +1,22 @@
-# Note: The SysEx message below is an example and may not be exact.
-# You MUST consult the MODX Data List for the correct addresses and formats.
-# Make sure to install the library first: pip install mido python-rtmidi
+noth = input("Вы попали в калькулятор, для продолжения введите что угодно: ")
 
-import mido
-import time
+num1 = float(input("введите первое число: "))
+num2 = float(input("введите второе число: "))
 
-# The SysEx message to change the name of Slot 1 on Page 1
-# F0 (Start) 43 (Yamaha) 10 (Param Change) 7F 1C (MODX) ... (Address) ... (Data) ... F7 (End)
-# This is a simplified representation.
-slot_name = "My Python Pad"
-encoded_name = [ord(c) for c in slot_name] # Convert string to ASCII bytes
+func = input("вы будете {0} и {1} делить (/), умножать (*), вычитать (-), суммировать (+) или возводить в степень (**)? ".format(num1,num2))
+if(func == "/"):
+    print("Вы выбрали делить {0} на {1}, их частное {2}".format(num1,num2,num1/num2))
+elif(func == "*"):
+    print("Вы выбрали умножать {0} на {1}, их произведение {2}".format(num1,num2,num1*num2))
+elif(func == "-"):
+    print("Вы выбрали вычитать {0} из {1}, их разность {2}".format(num2,num1,num1-num2))
+elif(func == "+"):
+    print("Вы выбрали суммировать {0} и {1}, их сумма {2}".format(num1,num2,num1+num2))
+elif(func == "**"):
+    print("Вы выбрали возводить в степень {0} на {1}, их степень равна {2}".format(num1,num2,num1**num2))
+else:
+    print("Вы ввели несуществующую функцию")
 
-# Construct the SysEx message (addresses are hypothetical)
-# [Start, Manu ID, Group ID, Model ID, Addr High, Addr Mid, Addr Low, Data..., End]
-sys_ex_msg = [
-    0xF0, 0x43, 0x10, 0x7F, 0x1C,  # Standard Header
-    0x60, 0x02, 0x0C,              # Example address for Live Set Slot 1 Name
-    *encoded_name,                 # The actual name data
-    0xF7                           # End of Exclusive
-]
 
-try:
-    # 1. Find the MODX MIDI port
-    modx_port_name = None
-    for name in mido.get_output_names():
-        if 'MODX' in name:
-            modx_port_name = name
-            break
-    
-    if not modx_port_name:
-        raise IOError("MODX port not found. Is it connected and turned on?")
 
-    print(f"Found MODX on port: {modx_port_name}")
-
-    # 2. Open the port and send the message
-    with mido.open_output(modx_port_name) as out_port:
-        msg = mido.Message('sysex', data=sys_ex_msg[1:-1]) # mido adds F0 and F7
-        print(f"Sending SysEx message: {msg}")
-        out_port.send(msg)
-        print("Message sent! Check your MODX screen.")
-
-except Exception as e:
-    print(f"An error occurred: {e}")
+noth = input("Что бы закончить введите что угодно: ")
