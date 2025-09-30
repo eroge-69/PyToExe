5f5a04c24bcbeee81d49f9c1cbc55b86043a1a4b
eroge-69 <erogexunamed@gmail.com>
2025-09-30 19:20:44 +0700

    Upload aa.py via PY to EXE tool

--- python-files/aa.py
+++ python-files/aa.py
@@ -1,24 +1,14 @@
-from pypresence import Presence
-import time
 
-CLIENT_ID = "1412867850846208111"
-rpc = Presence(CLIENT_ID)
-rpc.connect()
+cache: list = []
 
-start_time = int(time.time())  # timestamp inizio
+def square(num):
+    return num*num
 
-rpc.update(
-    state="Idling...",
-    details="Welcome To Vice City!",
-    start=start_time,            # solo start, niente end
-    large_image="gta_vi_logo",
-    large_text="Grand Theft Auto VI",
-    small_image="rogue",
-    small_text="Rogue - Level 100",
-)
-
-print("Rich Presence aggiornato!")
-
-while True:
-    time.sleep(15)
-
+def step(number):
+    if number in cache:
+        return "sad"
+    cache.append(number)
+    digits = list(str(number))
+    digits = [int(digit) for digit in digits]
+    number = sum(map(square, digits))
+    step(number)
