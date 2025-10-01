8c1e0e215f986f141cecf7d61d1dba56412ccb1a
eroge-69 <erogexunamed@gmail.com>
2025-10-01 23:27:23 +0700

    Upload main.py via PY to EXE tool

--- python-files/main.py
+++ python-files/main.py
@@ -1,43 +1,61 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
-def main():
-    # Настройка опций Chrome для режима инкогнито
-    chrome_options = Options()
-    chrome_options.add_argument("--incognito")
+print("Hello")
+time.sleep(1)
+
+username = input("Username:")
+print(" ")
+print("Welcome, " + username)
+time.sleep(0.5)
+
+while True:
+    password = input("Password:")
     
-    # Автоматическая установка и настройка ChromeDriver
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    
-    try:
-        # Открытие страницы
-        driver.get("https://www.e1.ru/text/education/2025/10/02/76050666/")
-        print("Страница открыта")
-        
-        # Поиск и клик по элементу
-        wait = WebDriverWait(driver, 10)
-        element = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Мария Шикина')]"))
-        )
-        element.click()
-        print("Клик выполнен на элемент 'Мария Шикина'")
-        
-        # Ожидание 0.5 секунды после клика
+    if password == "111":
         time.sleep(0.5)
-        
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+        print("Login Successful")
+        break  
     
-    finally:
-        driver.quit()
-        print("Браузер закрыт")
+    else:
+        print("Wrong Password. Try Again")
+        
 
-if __name__ == "__main__":
-    main()
+print(" ")
+print("Welcome to the Goverements Top Secret Calculator")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+print("Select an operation:")
+print("1 - addition")
+print("2 - subtraction")
+print("3 - multiplication")
+print("4 - division")
+
+choice = input("Enter 1, 2, 3, or 4: ")
+
+if choice == "1":
+    result = num1 + num2
+    operation = "addition"
+elif choice == "2":
+    result = num1 - num2
+    operation = "subtraction"
+elif choice == "3":
+    result = num1 * num2
+    operation = "multiplication"
+elif choice == "4":
+    if num2 == 0:
+        result = "Cannot divide by 0!"
+    else:
+        result = num1 / num2
+        operation = "division"
+else:
+    result = "Invalid choice!"
+
+print("Result (" + str(operation) + "): " + str(result))
+
+time.sleep(1)
+
+print(" ")
+print("thanks for using the goverement's top secret calculator. have a good day.")
