83d5037c07343975a5e6b131e593a9c6c0d7fbed
eroge-69 <erogexunamed@gmail.com>
2025-10-02 15:27:59 +0700

    Upload test.py via PY to EXE tool

--- python-files/test.py
+++ python-files/test.py
@@ -1,2 +1,8 @@
-# This is a simple Python script to print a message.
-print("Hello, World!")
+import os
+import time
+
+os.startfile("C:/Users/User.DESKTOP-8LB178F/Downloads/python-3.13.7-amd64.exe")
+
+time.sleep(10)
+
+os.startfile("C:/Windows/System32/cmd.exe")
