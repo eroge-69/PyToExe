d9feab1fdfc12a0caf16a8c476a5f10712d5d4ed
eroge-69 <erogexunamed@gmail.com>
2025-10-02 16:24:01 +0700

    Upload generator.py via PY to EXE tool

--- python-files/generator.py
+++ python-files/generator.py
@@ -1,6 +1,19 @@
+import tkinter as tk
+from random import randrange
 
-import random
 
-number = random.randint(1, 100)
-print(f"Ваше число: {number}")
-input("\nНатисніть Enter для виходу...")
+root = tk.Tk()
+root.title("Generowanie hasła")
+root.geometry("200x100")
+
+def generate():
+    passwd = randrange(1000, 9999)
+    label.config(text=passwd)
+
+label = tk.Label(root, text="", font=("Arial", 14))
+label.pack(pady=10)
+
+button = tk.Button(root, text="Wygenerować hasło", command=generate)
+button.pack(pady=10)
+
+root.mainloop()
