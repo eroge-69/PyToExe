a9e523b9149344cb29d372e65622c8473e12adfb
eroge-69 <erogexunamed@gmail.com>
2025-10-04 22:42:26 +0700

    Upload program.py via PY to EXE tool

--- python-files/program.py
+++ python-files/program.py
@@ -1,14 +1,3 @@
-import random
-
-n = random.randint(1,10)
-g = int(input("enter number between 1 and 10: "))
-while g != n:
-  if g > n:
-    print("less")
-    g = int(input("enter number between 1 and 10: "))
-  elif g < n:
-    print("more")
-    g = int(input("enter number between 1 and 10: "))
-  else:
-      print('enter a number between 1 and 10')
-print("got it")
+# program.py
+print("Моя первая программа!")
+input("Нажми Enter...")
