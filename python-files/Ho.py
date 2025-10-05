61bd5178a47fd40d921e58565fc441cb4d4125f8
eroge-69 <erogexunamed@gmail.com>
2025-10-06 03:11:18 +0700

    Upload Ho.py via PY to EXE tool

--- python-files/Ho.py
+++ python-files/Ho.py
@@ -1,1 +1,4 @@
-print("Hola como estas?")
+Python 2.2 (#28, Dec 21 2001, 12:21:22) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> print("Hola como estas?")
