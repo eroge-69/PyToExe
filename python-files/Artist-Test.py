dccbcb89ebd065aa0ffefa949a696b8bf17f5c3d
eroge-69 <erogexunamed@gmail.com>
2025-10-06 02:04:12 +0700

    Upload Artist-Test.py via PY to EXE tool

---
+++ python-files/Artist-Test.py
@@ -0,0 +1,7 @@
+import time
+musician=input("Who is your favorite musician?")
+
+print("You typed" + " " + musician + "!")
+print("Processing...")
+time.sleep(5)
+print("Congragulations! Your music taste is ass!")
