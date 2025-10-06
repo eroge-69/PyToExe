60e682b2a781857b5eb0fb0076ec4155b7b3b149
eroge-69 <erogexunamed@gmail.com>
2025-10-06 14:28:17 +0700

    Upload setup.py via PY to EXE tool

--- python-files/setup.py
+++ python-files/setup.py
@@ -1,77 +1,141 @@
-# -*- coding:utf-8 -*-
-# /usr/bin/env python
-"""
-Date: 2024/11/20 15:20
-Desc: AKTools 的 PYPI 基本信息文件
-"""
-import re
-import ast
+#!/usr/bin/env python
+#                   ,*++++++*,                ,*++++++*,
+#                *++.        .+++          *++.        .++*
+#              *+*     ,++++*   *+*      *+*   ,++++,     *+*
+#             ,+,   .++++++++++* ,++,,,,*+, ,++++++++++.   *+,
+#             *+.  .++++++++++++..++    *+.,++++++++++++.  .+*
+#             .+*   ++++++++++++.*+,    .+*.++++++++++++   *+,
+#              .++   *++++++++* ++,      .++.*++++++++*   ++,
+#               ,+++*.    . .*++,          ,++*.      .*+++*
+#              *+,   .,*++**.                  .**++**.   ,+*
+#             .+*                                          *+,
+#             *+.                   Coqui                  .+*
+#             *+*              +++   TTS  +++              *+*
+#             .+++*.            .          .             *+++.
+#              ,+* *+++*...                       ...*+++* *+,
+#               .++.    .""""+++++++****+++++++"""".     ++.
+#                 ,++.                                .++,
+#                   .++*                            *++.
+#                       *+++,                  ,+++*
+#                           .,*++++::::::++++*,.
+#                                  ``````
 
-import setuptools
+import os
+import subprocess
+import sys
+from packaging.version import Version
+
+import numpy
+import setuptools.command.build_py
+import setuptools.command.develop
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
+
+python_version = sys.version.split()[0]
+if Version(python_version) < Version("3.9") or Version(python_version) >= Version("3.12"):
+    raise RuntimeError("TTS requires python >= 3.9 and < 3.12 " "but your Python version is {}".format(sys.version))
 
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+cwd = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(cwd, "TTS", "VERSION")) as fin:
+    version = fin.read().strip()
 
 
-def get_version_string() -> str:
-    """
-    get the aktools version
-    :return: version
-    :rtype: str
-    """
-    with open("aktools/__init__.py", "rb") as file:
-        version_line = re.search(
-            pattern=r"__version__\s+=\s+(.*)", string=file.read().decode("utf-8")
-        ).group(1)
-        return str(ast.literal_eval(version_line))
+class build_py(setuptools.command.build_py.build_py):  # pylint: disable=too-many-ancestors
+    def run(self):
+        setuptools.command.build_py.build_py.run(self)
 
 
-setuptools.setup(
-    name="aktools",
-    version=get_version_string(),
-    author="AKFamily",
-    author_email="albertandking@gmail.com",
-    license="MIT",
-    description="AKTools is a tool for AKShare HTTP API!",
-    long_description=long_description,
+class develop(setuptools.command.develop.develop):
+    def run(self):
+        setuptools.command.develop.develop.run(self)
+
+
+# The documentation for this feature is in server/README.md
+package_data = ["TTS/server/templates/*"]
+
+
+def pip_install(package_name):
+    subprocess.call([sys.executable, "-m", "pip", "install", package_name])
+
+
+requirements = open(os.path.join(cwd, "requirements.txt"), "r").readlines()
+with open(os.path.join(cwd, "requirements.notebooks.txt"), "r") as f:
+    requirements_notebooks = f.readlines()
+with open(os.path.join(cwd, "requirements.dev.txt"), "r") as f:
+    requirements_dev = f.readlines()
+with open(os.path.join(cwd, "requirements.ja.txt"), "r") as f:
+    requirements_ja = f.readlines()
+requirements_all = requirements_dev + requirements_notebooks + requirements_ja
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    README = readme_file.read()
+
+exts = [
+    Extension(
+        name="TTS.tts.utils.monotonic_align.core",
+        sources=["TTS/tts/utils/monotonic_align/core.pyx"],
+    )
+]
+setup(
+    name="TTS",
+    version=version,
+    url="https://github.com/coqui-ai/TTS",
+    author="Eren Gölge",
+    author_email="egolge@coqui.ai",
+    description="Deep learning for Text to Speech by Coqui.",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/akfamily/aktools",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "akshare>=1.16.25",
-        "fastapi>=0.115.5",
-        "uvicorn>=0.16.0",
-        "python-multipart>=0.0.9",
-        "jinja2>=3.1.2",
-        "typer[standard]>=0.6.1",
-    ],
-    package_data={"": ["*.py", "*.json", "*.pk", "*.woff", "*.html", "*.ico"]},
-    keywords=[
-        "stock",
-        "option",
-        "futures",
-        "bond",
-        "index",
-        "air",
-        "others",
-        "finance",
-        "spider",
-        "quant",
-        "quantitative",
-        "investment",
-        "trading",
-        "algotrading",
-    ],
+    license="MPL-2.0",
+    # cython
+    include_dirs=numpy.get_include(),
+    ext_modules=cythonize(exts, language_level=3),
+    # ext_modules=find_cython_extensions(),
+    # package
+    include_package_data=True,
+    packages=find_packages(include=["TTS"], exclude=["*.tests", "*tests.*", "tests.*", "*tests", "tests"]),
+    package_data={
+        "TTS": [
+            "VERSION",
+        ]
+    },
+    project_urls={
+        "Documentation": "https://github.com/coqui-ai/TTS/wiki",
+        "Tracker": "https://github.com/coqui-ai/TTS/issues",
+        "Repository": "https://github.com/coqui-ai/TTS",
+        "Discussions": "https://github.com/coqui-ai/TTS/discussions",
+    },
+    cmdclass={
+        "build_py": build_py,
+        "develop": develop,
+        # 'build_ext': build_ext
+    },
+    install_requires=requirements,
+    extras_require={
+        "all": requirements_all,
+        "dev": requirements_dev,
+        "notebooks": requirements_notebooks,
+        "ja": requirements_ja,
+    },
+    python_requires=">=3.9.0, <3.12",
+    entry_points={"console_scripts": ["tts=TTS.bin.synthesize:main", "tts-server = TTS.server.server:main"]},
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    python_requires=">=3.8",
+    zip_safe=False,
 )
