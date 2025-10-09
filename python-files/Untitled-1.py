a6c6360bbe07452cfc4dc4d3573b0aa77ef830c2
eroge-69 <erogexunamed@gmail.com>
2025-10-10 02:10:48 +0700

    Upload Untitled-1.py via PY to EXE tool

---
+++ python-files/Untitled-1.py
@@ -0,0 +1,17 @@
+def calculator():
+    num1 = float(input("enter first number: "))
+    op = input("enter operator (+, -, /, *,): ")
+    num2 = float(input("enter second number: "))
+
+    if op == '+':
+        print("result:", num1 + num2)
+    elif op == '-':
+        print("result:", num1 - num2)
+    elif op == '/':
+        print("result:", num1 / num2)
+    elif op == '*':
+        print("result:", num1 * num2)    
+    else:
+        print("invalid operator")
+
+calculator()
