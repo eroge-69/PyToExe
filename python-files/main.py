a5dff38122af3085ac76de9b63a634688104c7c6
eroge-69 <erogexunamed@gmail.com>
2025-10-11 14:31:14 +0700

    Upload main.py via PY to EXE tool

---
+++ python-files/main.py
@@ -0,0 +1,12 @@
+import pymem 
+import pymem.process
+import tkinter as tk 
+import time 
+
+ 
+root = tk.Tk()
+root.configure(bg="#47B315")
+button = tk.Button(bg="#FFD700", text="9999 suns", height=2, width=16)
+button.pack()
+root.geometry("600x600")
+root.mainloop()
